6cc01fd66e3551dedd1edae60bc7124f121ab127
gallayl <gallay.lajos@gmail.com>
2017-11-14 15:01:22 +0100

    [KFI]docs(GoogleAuthenticationOptions): added typedoc & fixed defaults

--- packages/sn-client-auth-google/src/GoogleAuthenticationOptions.ts
+++ packages/sn-client-auth-google/src/GoogleAuthenticationOptions.ts
@@ -1,11 +1,25 @@
+/**
+ * Options for Google OAuth Authentication
+ */
 export class GoogleAuthenticationOptions {
+    /**
+     * Defines the Redirect Uri. Will fall back to 'window.location.origin', if not provided
+     */
     public RedirectUri: string;
-    public Scope: string[] = ['userinfo.email', 'userinfo.profile'];
+    /**
+     * Scope settings for Google Oauth
+     * Visit the following link to read more about Google Scopes:
+     * https://developers.google.com/identity/protocols/googlescopes
+     */
+    public Scope: string[] = ['email', 'profile'];
+    /**
+     * Your application's ClientId, provided by Google
+     */
     public ClientId: string;
 
     constructor(options: Partial<GoogleAuthenticationOptions> & {ClientId: string}) {
         if (!options.RedirectUri) {
-            options.RedirectUri = window.location.origin;
+            options.RedirectUri = window.location.origin + '/';
         }
         Object.assign(this, options);
     }
