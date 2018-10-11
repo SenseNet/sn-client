420acdacc8cacf7f5589fb82dd22d2454a62f6fc
Aniko Litvanyi <herflis33@gmail.com>
2018-10-11 13:20:00 +0200

    [KFI]docs: Improve icon docs

--- packages/sn-react-component-docs/stories/Icon.stories.tsx
+++ packages/sn-react-component-docs/stories/Icon.stories.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { storiesOf, addDecorator } from "@storybook/react";
-import { Icon, iconType } from "../src/components/icons-react/src/components/Icon";
+import { Icon } from "../src/components/icons-react/src/components/Icon";
+import { FontAwesomeIcon } from '../src/components/icons-react/src/components/fontawesome/Icon';
+import { FlatIcon } from '../src/components/icons-react/src/components/flaticon/Icon';
+import { ImageIcon } from '../src/components/icons-react/src/components/image/Icon';
+import { MaterialIcon } from '../src/components/icons-react/src/components/materialui/Icon';
 import { muiTheme } from 'storybook-addon-material-ui';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
@@ -22,38 +26,43 @@ stories.add(
         />
     )),
 ).add(
-    "fontawesome",
+    "materialui",
     (() => (
-        <Icon
-            type={iconType.fontawesome}
+        <MaterialIcon
             iconName={text('Name of the icon', 'folder')}
             onClick={action('button-click')}
             color={select('Color', ["inherit", "primary", "secondary", "action", "error", "disabled"], 'primary')}
             fontSize={select('Size', ['inherit', 'default'], 'default')}
             className={text('Additional class name', 'myClass')}
+        />
+    )),
+).add(
+    "fontawesome",
+    (() => (
+        <FontAwesomeIcon
+            iconName={text('Name of the icon', 'folder')}
+            onClick={action('button-click')}
+            color={select('Color', ["inherit", "primary", "secondary", "action", "error", "disabled"], 'primary')}
+            fontSize={select('Size', ['inherit', 'default'], 'default')}
         />
     )),
 ).add(
     "flaticon",
     (() => (
-        <Icon
-            type={iconType.flaticon}
+        <FlatIcon
             iconName={text('Name of the icon', 'folder-symbol')}
             onClick={action('button-click')}
             color={select('Color', ["inherit", "primary", "secondary", "action", "error", "disabled"], 'primary')}
             fontSize={select('Size', ['inherit', 'default'], 'default')}
-            className={text('Additional class name', 'myClass')}
         />
     )),
 ).add(
     "image",
     (() => (
-        <Icon
-            type={iconType.image}
+        <ImageIcon
             iconName={text('Name of the icon', 'folder')}
             onClick={action('button-click')}
             size={select('Size', [16, 32], 16)}
-            className={text('Additional class name', 'myClass')}
         />
     )),
 );
