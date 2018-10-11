68fcf53abcc385e4ffb6fb47c6ad6659706e6188
Aniko Litvanyi <herflis33@gmail.com>
2018-10-11 10:43:45 +0200

    [KFI]fix: Remove content from src

--- packages/sn-react-component-docs/src/components/icons-react/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Icon, iconType } from './Icon'
-export { FontAwesomeIcon } from './fontawesome/Icon'
-export { FlatIcon } from './flaticon/Icon'
-export { MaterialIcon } from './materialui/Icon'
-export { ImageIcon } from './image/Icon'
