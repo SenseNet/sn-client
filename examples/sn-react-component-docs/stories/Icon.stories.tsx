9fb0a5b809dd1e6b934cd87269ed68eb7cfb695c
Zoltán Bedi <zoltan.bedi@gmail.com>
2018-12-05 15:41:31 +0100

    refactor(react-component-docs): styling + deps + fixes

--- examples/sn-react-component-docs/stories/Icon.stories.tsx
+++ examples/sn-react-component-docs/stories/Icon.stories.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { storiesOf, addDecorator } from "@storybook/react";
-import { Icon, FontAwesomeIcon, FlatIcon, ImageIcon, MaterialIcon } from "@sensenet/icons-react/src";
-import { muiTheme } from 'storybook-addon-material-ui';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, text, select } from '@storybook/addon-knobs';
-import { withMarkdownNotes } from '@storybook/addon-notes';
-import { withInfo } from "@storybook/addon-info";
+import { FlatIcon, FontAwesomeIcon, Icon, ImageIcon, MaterialIcon } from '@sensenet/icons-react/src'
 import { checkA11y } from '@storybook/addon-a11y'
+import { action } from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-info'
+import { select, text, withKnobs } from '@storybook/addon-knobs'
+import { withMarkdownNotes } from '@storybook/addon-notes'
+import { addDecorator, storiesOf } from '@storybook/react'
+import React from 'react'
+import { muiTheme } from 'storybook-addon-material-ui'
 
 addDecorator(muiTheme())
-const stories = storiesOf("Icon", module).addDecorator(withKnobs).addDecorator(withInfo()).addDecorator(checkA11y);
-
+const stories = storiesOf('Icon', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withInfo())
+  .addDecorator(checkA11y)
 
 const defaultNotes = require('../notes/icon/Default.md')
 const materialuiNotes = require('../notes/icon/Materialui.md')
@@ -18,56 +20,60 @@ const fontawesomeNotes = require('../notes/icon/FontAwesome.md')
 const flaticonNotes = require('../notes/icon/Flaticon.md')
 const imageNotes = require('../notes/icon/Image.md')
 
-stories.add(
-    "default", withMarkdownNotes(defaultNotes)
-        (() => (
-            <Icon
-                iconName={text('Name of the icon', 'folder')}
-                onClick={action('button-click')}
-                color={select('Color', ["inherit", "primary", "secondary", "action", "error", "disabled"], 'primary')}
-                fontSize={select('Size', ['inherit', 'default'], 'default')}
-                className={text('Additional class name', 'myClass')}
-            />
-        )),
-).add(
-    "materialui", withMarkdownNotes(materialuiNotes)
-        (() => (
-            <MaterialIcon
-                iconName={text('Name of the icon', 'folder')}
-                onClick={action('button-click')}
-                color={select('Color', ["inherit", "primary", "secondary", "action", "error", "disabled"], 'primary')}
-                fontSize={select('Size', ['inherit', 'default'], 'default')}
-                className={text('Additional class name', 'myClass')}
-            />
-        )),
-).add(
-    "fontawesome", withMarkdownNotes(fontawesomeNotes)
-        (() => (
-            <FontAwesomeIcon
-                iconName={text('Name of the icon', 'folder')}
-                onClick={action('button-click')}
-                color={select('Color', ["inherit", "primary", "secondary", "action", "error", "disabled"], 'primary')}
-                fontSize={select('Size', ['inherit', 'default'], 'default')}
-            />
-        )),
-).add(
-    "flaticon", withMarkdownNotes(flaticonNotes)
-        (() => (
-            <FlatIcon
-                iconName={text('Name of the icon', 'folder-symbol')}
-                onClick={action('button-click')}
-                color={select('Color', ["inherit", "primary", "secondary", "action", "error", "disabled"], 'primary')}
-                fontSize={select('Size', ['inherit', 'default'], 'default')}
-            />
-        )),
-).add(
-    "image", withMarkdownNotes(imageNotes)
-        (() => (
-            <ImageIcon
-                iconName={text('Name of the icon', 'folder')}
-                onClick={action('button-click')}
-                size={select('Size', [16, 32], 16)}
-            />
-        )),
-);
-
+stories
+  .add(
+    'default',
+    withMarkdownNotes(defaultNotes)(() => (
+      <Icon
+        iconName={text('Name of the icon', 'folder')}
+        onClick={action('button-click')}
+        color={select('Color', ['inherit', 'primary', 'secondary', 'action', 'error', 'disabled'], 'primary')}
+        fontSize={select('Size', ['inherit', 'default'], 'default')}
+        className={text('Additional class name', 'myClass')}
+      />
+    )),
+  )
+  .add(
+    'materialui',
+    withMarkdownNotes(materialuiNotes)(() => (
+      <MaterialIcon
+        iconName={text('Name of the icon', 'folder')}
+        onClick={action('button-click')}
+        color={select('Color', ['inherit', 'primary', 'secondary', 'action', 'error', 'disabled'], 'primary')}
+        fontSize={select('Size', ['inherit', 'default'], 'default')}
+        className={text('Additional class name', 'myClass')}
+      />
+    )),
+  )
+  .add(
+    'fontawesome',
+    withMarkdownNotes(fontawesomeNotes)(() => (
+      <FontAwesomeIcon
+        iconName={text('Name of the icon', 'folder')}
+        onClick={action('button-click')}
+        color={select('Color', ['inherit', 'primary', 'secondary', 'action', 'error', 'disabled'], 'primary')}
+        fontSize={select('Size', ['inherit', 'default'], 'default')}
+      />
+    )),
+  )
+  .add(
+    'flaticon',
+    withMarkdownNotes(flaticonNotes)(() => (
+      <FlatIcon
+        iconName={text('Name of the icon', 'folder-symbol')}
+        onClick={action('button-click')}
+        color={select('Color', ['inherit', 'primary', 'secondary', 'action', 'error', 'disabled'], 'primary')}
+        fontSize={select('Size', ['inherit', 'default'], 'default')}
+      />
+    )),
+  )
+  .add(
+    'image',
+    withMarkdownNotes(imageNotes)(() => (
+      <ImageIcon
+        iconName={text('Name of the icon', 'folder')}
+        onClick={action('button-click')}
+        size={select('Size', [16, 32], 16)}
+      />
+    )),
+  )
--- examples/sn-react-component-docs/stories/ListControl.stories.tsx
+++ examples/sn-react-component-docs/stories/ListControl.stories.tsx
@@ -1,99 +1,102 @@
-import React from "react";
-import { storiesOf, addDecorator } from "@storybook/react";
-import { ContentList } from "@sensenet/list-controls-react/src/ContentList";
-import { action } from '@storybook/addon-actions';
-import { muiTheme } from 'storybook-addon-material-ui';
-import { withKnobs, array, boolean, select, text } from '@storybook/addon-knobs';
-import { withMarkdownNotes } from '@storybook/addon-notes';
-import { withInfo } from "@storybook/addon-info";
+import { ContentList } from '@sensenet/list-controls-react/src/ContentList'
 import { checkA11y } from '@storybook/addon-a11y'
+import { action } from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-info'
+import { array, boolean, select, text, withKnobs } from '@storybook/addon-knobs'
+import { withMarkdownNotes } from '@storybook/addon-notes'
+import { addDecorator, storiesOf } from '@storybook/react'
+import React from 'react'
+import { muiTheme } from 'storybook-addon-material-ui'
 
-import { customSchema } from './ViewControl.stories'
+import { ActionModel } from '@sensenet/default-content-types'
 import { icons } from '../assets/icons'
-import { IActionModel } from "@sensenet/default-content-types";
+import { customSchema } from './ViewControl.stories'
 
 const orderDirectionOptions = {
-    asc: 'asc',
-    desc: 'desc',
-};
+  asc: 'asc',
+  desc: 'desc',
+}
 
 addDecorator(muiTheme())
 
 const contentListNotes = require('../notes/listcontrols/ContentList.md')
 
 const items = [
-    {
-        DisplayName: 'Sample folder',
-        Name: 'SampleFolder',
-        Locked: true,
-        ModificationDate: '2018-11-05T00:44:55.767Z',
-        Actions: [
-            {
-                DisplayName: "Rename",
-                Icon: "rename",
-                Name: "Rename",
-            } as IActionModel,
-            {
-                DisplayName: "Delete",
-                Icon: "delete",
-                Name: "Delete",
-            } as IActionModel
-        ],
-        Id: 1,
-        Path: '/Root/Profiles/Public/alba/Document_Library/Sample-folder',
-        Type: 'Folder',
-        Icon: "folder",
-    },
-    {
-        DisplayName: 'Sample-document.docx',
-        Name: 'Sample-document.docx',
-        Locked: false,
-        ModificationDate: '2018-11-05T00:44:55.843Z',
-        Actions: [
-            {
-                DisplayName: "Preview",
-                Icon: "preview",
-                Name: "Preview",
-            } as IActionModel,
-            {
-                DisplayName: "Download",
-                Icon: "download",
-                Name: "Browse",
-            } as IActionModel,
-            {
-                DisplayName: "Delete",
-                Icon: "delete",
-                Name: "Delete",
-            } as IActionModel
-        ],
-        Id: 2,
-        Path: '/Root/Profiles/Public/alba/Document_Library/Sample-document.docx',
-        Type: 'File',
-        Icon: 'word',
-    }
+  {
+    DisplayName: 'Sample folder',
+    Name: 'SampleFolder',
+    Locked: true,
+    ModificationDate: '2018-11-05T00:44:55.767Z',
+    Actions: [
+      {
+        DisplayName: 'Rename',
+        Icon: 'rename',
+        Name: 'Rename',
+      } as ActionModel,
+      {
+        DisplayName: 'Delete',
+        Icon: 'delete',
+        Name: 'Delete',
+      } as ActionModel,
+    ],
+    Id: 1,
+    Path: '/Root/Profiles/Public/alba/Document_Library/Sample-folder',
+    Type: 'Folder',
+    Icon: 'folder',
+  },
+  {
+    DisplayName: 'Sample-document.docx',
+    Name: 'Sample-document.docx',
+    Locked: false,
+    ModificationDate: '2018-11-05T00:44:55.843Z',
+    Actions: [
+      {
+        DisplayName: 'Preview',
+        Icon: 'preview',
+        Name: 'Preview',
+      } as ActionModel,
+      {
+        DisplayName: 'Download',
+        Icon: 'download',
+        Name: 'Browse',
+      } as ActionModel,
+      {
+        DisplayName: 'Delete',
+        Icon: 'delete',
+        Name: 'Delete',
+      } as ActionModel,
+    ],
+    Id: 2,
+    Path: '/Root/Profiles/Public/alba/Document_Library/Sample-document.docx',
+    Type: 'File',
+    Icon: 'word',
+  },
 ]
 
-storiesOf('ListControls', module).addDecorator(withKnobs).addDecorator(withInfo()).addDecorator(checkA11y)
-    .add("ContentList",
-        withMarkdownNotes(contentListNotes)
-            (() => (
-                <ContentList
-                    items={items}
-                    schema={customSchema.find((s) => s.ContentTypeName === 'GenericContent')}
-                    icons={icons}
-                    fieldsToDisplay={array('Fields to display', ['DisplayName', 'Locked', 'ModificationDate', 'Actions'])}
-                    orderBy={text('Order by', 'DisplayName')}
-                    orderDirection={select('Order direction', orderDirectionOptions, 'asc')}
-                    displayRowCheckbox={boolean('Display checkbox for selection', true)}
-                    onItemClick={action('item clicked')}
-                    onItemDoubleClick={action('item double-clicked')}
-                    onItemTap={action('item tapped')}
-                    onItemContextMenu={action('item right-clicked')}
-                    onRequestActionsMenu={action('actionmenu-button clicked')}
-                    onRequestOrderChange={action('ordering is changed')}
-                    onRequestSelectionChange={action('selection is changed')}
-                    onRequestActiveItemChange={action('active item is changed')}
-                    onAction={action('an action is requested')}
-                />
-            )),
-    )
+storiesOf('ListControls', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withInfo())
+  .addDecorator(checkA11y)
+  .add(
+    'ContentList',
+    withMarkdownNotes(contentListNotes)(() => (
+      <ContentList
+        items={items}
+        schema={customSchema.find(s => s.ContentTypeName === 'GenericContent')}
+        icons={icons}
+        fieldsToDisplay={array('Fields to display', ['DisplayName', 'Locked', 'ModificationDate', 'Actions'])}
+        orderBy={text('Order by', 'DisplayName')}
+        orderDirection={select('Order direction', orderDirectionOptions, 'asc')}
+        displayRowCheckbox={boolean('Display checkbox for selection', true)}
+        onItemClick={action('item clicked')}
+        onItemDoubleClick={action('item double-clicked')}
+        onItemTap={action('item tapped')}
+        onItemContextMenu={action('item right-clicked')}
+        onRequestActionsMenu={action('actionmenu-button clicked')}
+        onRequestOrderChange={action('ordering is changed')}
+        onRequestSelectionChange={action('selection is changed')}
+        onRequestActiveItemChange={action('active item is changed')}
+        onAction={action('an action is requested')}
+      />
+    )),
+  )
