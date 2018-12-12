84acfd235ffd1ae3886345d892071706429c1c3b
Gallay Lajos <gallay.lajos@LIVE.COM>
2018-12-12 14:35:05 +0100

    chore(authentication-jwt): lint fixes

--- packages/sn-authentication-jwt/test/MockOauthProvider.ts
+++ packages/sn-authentication-jwt/test/MockOauthProvider.ts
@@ -1,11 +1,13 @@
 import { OauthProvider } from '../src/OauthProvider'
 
 export class MockOauthProvider implements OauthProvider {
-  public dispose: () => void | Promise<void>
+  public async dispose() {
+    /** */
+  }
   public getToken(): Promise<string> {
     throw new Error('Method not implemented.')
   }
-  public login(token: string): Promise<any> {
+  public login(_token: string): Promise<any> {
     throw new Error('Method not implemented.')
   }
 }
