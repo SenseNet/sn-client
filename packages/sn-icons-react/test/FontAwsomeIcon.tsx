e031c729cb821754ce93cf7a7e985fd4c0db1509
Aniko Litvanyi <herflis33@gmail.com>
2018-12-13 16:39:25 +0100

    [KFI]test(icons-react): mocha => jest, react-test-renderer => enzyme

---
+++ packages/sn-icons-react/test/FontAwsomeIcon.tsx
@@ -0,0 +1,33 @@
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import 'jest'
+import React from 'react'
+import { FontAwesomeIcon } from '../src/components/fontawesome/Icon'
+import { Icon, iconType } from '../src/components/Icon'
+/**
+ * FontAwesome Icon Component tests
+ */
+describe('Icon component', () => {
+  configure({ adapter: new Adapter() })
+  it('Should render without crashing', () => {
+    expect(shallow(<FontAwesomeIcon iconName="folder" />).hasClass('fa-folder')).toBe(true)
+  })
+  it('Should render without crashing with fontSize', () => {
+    expect(shallow(<FontAwesomeIcon fontSize="default" iconName="folder" />).hasClass('fa-folder')).toBe(true)
+  })
+  it('Should render without crashing with color', () => {
+    expect(shallow(<FontAwesomeIcon color="primary" iconName="folder" />).hasClass('fa-folder')).toBe(true)
+  })
+  it('Should render without crashing with classes', () => {
+    expect(shallow(<FontAwesomeIcon classes={{}} iconName="folder" />).hasClass('fa-folder')).toBe(true)
+  })
+  it('Should render without crashing with styles', () => {
+    expect(shallow(<FontAwesomeIcon style={{}} iconName="folder" />).hasClass('fa-folder')).toBe(true)
+  })
+  it('Should render without crashing with onClick', () => {
+    expect(shallow(<FontAwesomeIcon onClick={(e: React.MouseEvent<HTMLElement>) => console.log(e.target)} iconName="folder" />).hasClass('fa-folder')).toBe(true)
+  })
+  it('Should render without crashing with children component', () => {
+    expect(shallow(<FontAwesomeIcon iconName="folder"><Icon type={iconType.materialui} iconName="forward" /></FontAwesomeIcon>).hasClass('fa-folder')).toBe(true)
+  })
+})
--- packages/sn-icons-react/test/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Icon'
-
-export * from './materialui'
-export * from './fontawesome'
-export * from './flaticon'
-export * from './image'
