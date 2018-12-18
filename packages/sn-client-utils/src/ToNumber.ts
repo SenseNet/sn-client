0b1d2b41a80c9f12f93c80f79fa1d52411b07834
Zoltán Bedi <zoltan.bedi@gmail.com>
2018-12-18 14:37:09 +0100

    docs(client-utils): toNumber doc update

--- packages/sn-client-utils/src/ToNumber.ts
+++ packages/sn-client-utils/src/ToNumber.ts
@@ -4,12 +4,12 @@
  * @param defaultValue The default value that is used when the specified value is either null or undefined values.
  * @returns Number | NaN | undefined
  * @example
- *    undefined = asNumber(undefined)
- *    undefined = asNumber(null)
- *    10        = asNumber(10)
- *    10        = asNumber('10')
- *    10        = asNumber({ toString() { return '10'; } })
- *    NaN       = asNumber('Hello')
+ *    undefined = toNumber(undefined)
+ *    undefined = toNumber(null)
+ *    10        = toNumber(10)
+ *    10        = toNumber('10')
+ *    10        = toNumber({ toString() { return '10'; } })
+ *    NaN       = toNumber('Hello')
  */
 export function toNumber(value: any, defaultValue?: number): number | undefined {
   const num = value === null || value === undefined || value === '' ? defaultValue : +value
