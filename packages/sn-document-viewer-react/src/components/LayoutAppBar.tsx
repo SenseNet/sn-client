8d10dc8b920d69f9bf04e5b9a374c3ab43240bdc
Zoltán Bedi <zoltan.bedi@gmail.com>
2018-12-20 18:57:38 +0100

    refactor(document-viewer): layout appbar to sfc + snapshot test

--- packages/sn-document-viewer-react/src/components/LayoutAppBar.tsx
+++ packages/sn-document-viewer-react/src/components/LayoutAppBar.tsx
@@ -1,42 +1,9 @@
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import React, { CSSProperties } from 'react'
 
-import React = require('react')
-import { connect } from 'react-redux'
-
-import { CSSProperties } from 'react'
-import { componentType } from '../services'
-
-/**
- * maps state fields from the store to component props
- */
-const mapStateToProps = () => {
-  return {}
-}
-
-/**
- * maps state actions from the store to component props
- * @param state the redux state
- */
-const mapDispatchToProps = {}
-
-class LayoutAppBar extends React.Component<
-  componentType<typeof mapStateToProps, typeof mapDispatchToProps, { style?: CSSProperties }>
-> {
-  /**
-   * renders the component
-   */
-  public render() {
-    return (
-      <AppBar position="sticky" style={{ position: 'relative', zIndex: 1, ...this.props.style }}>
-        <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>{this.props.children}</Toolbar>
-      </AppBar>
-    )
-  }
-}
-
-const connectedComponent = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(LayoutAppBar)
-export { connectedComponent as LayoutAppBar }
+export const LayoutAppBar: React.FunctionComponent<{ style?: CSSProperties }> = props => (
+  <AppBar position="sticky" style={{ position: 'relative', zIndex: 1, ...props.style }}>
+    <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>{props.children}</Toolbar>
+  </AppBar>
+)
