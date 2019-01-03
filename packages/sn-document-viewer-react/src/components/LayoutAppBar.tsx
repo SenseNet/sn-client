1f043314fefa2e9cb7ba7e6775f2967ea7104460
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-01-03 12:34:38 +0100

    docs(document-viewer): missing comment after refactor

--- packages/sn-document-viewer-react/src/components/LayoutAppBar.tsx
+++ packages/sn-document-viewer-react/src/components/LayoutAppBar.tsx
@@ -2,6 +2,9 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import React, { CSSProperties } from 'react'
 
+/**
+ * Represents a Toolbar component in an AppBar
+ */
 export const LayoutAppBar: React.FunctionComponent<{ style?: CSSProperties }> = props => (
   <AppBar position="sticky" style={{ position: 'relative', zIndex: 1, ...props.style }}>
     <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>{props.children}</Toolbar>
