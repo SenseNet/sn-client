17e120fa54eb3a4fe194690d07a22240aaff9dd6
gallayl <gallay.lajos@gmail.com>
2019-01-10 14:55:09 +0100

    [KFI]fix(sn-dms-demo): fixed SearchDocuments and added missing minor features

--- examples/sn-dms-demo/src/assets/picker.ts
+++ examples/sn-dms-demo/src/assets/picker.ts
@@ -131,18 +131,18 @@ export const pickerTheme = createMuiTheme({
           boxShadow: 'none',
         },
       },
-      '&$disabled': {
-        background: '#fff',
-        color: '#016D9E',
-        boxShadow: 'none',
-        '&.disabled-mobile-button': {
-          [theme.breakpoints.down('md')]: {
-            background: '#fff',
-            color: '#016D9E',
-            opacity: 0.25,
-          },
-        },
-      },
+      // '&$disabled': {
+      //   background: '#fff',
+      //   color: '#016D9E',
+      //   boxShadow: 'none',
+      //   // '&.disabled-mobile-button': {
+      //   //   [theme.breakpoints.down('md')]: {
+      //   //     background: '#fff',
+      //   //     color: '#016D9E',
+      //   //     opacity: 0.25,
+      //   //   },
+      //   // },
+      // },
     } as any,
   },
 })
