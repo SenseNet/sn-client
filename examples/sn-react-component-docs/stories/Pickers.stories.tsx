ed0572c2878083997b933400df0507f9034a48be
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-02-04 21:24:11 +0100

    [KFI]docs(storybook): pickers-react update

--- examples/sn-react-component-docs/stories/Pickers.stories.tsx
+++ examples/sn-react-component-docs/stories/Pickers.stories.tsx
@@ -1,6 +1,48 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 
-import { ListPickerComponent } from '@sensenet/pickers-react/src/ListPicker'
+import { GenericContent } from '@sensenet/default-content-types'
+import { Item, ListPickerComponent } from '@sensenet/pickers-react/src/ListPicker'
 
-storiesOf('ListPicker', module).add('default', () => <ListPickerComponent />)
+export const genericContentItems: Array<Item<GenericContent>> = [
+  {
+    id: 1,
+    nodeData: {
+      Id: 1,
+      Type: 'Folder',
+      Path: 'path',
+      Name: 'Csiga',
+    },
+  },
+  {
+    id: 2,
+    nodeData: {
+      Id: 2,
+      Type: 'Folder',
+      Path: 'path',
+      Name: 'Biga',
+    },
+  },
+  {
+    id: 3,
+    nodeData: {
+      Id: 3,
+      Type: 'Folder',
+      Path: 'path',
+      Name: 'Sapka',
+    },
+  },
+  {
+    id: 4,
+    nodeData: {
+      Id: 4,
+      Type: 'Folder',
+      Path: 'path',
+      Name: 'Kabát',
+    },
+  },
+]
+
+const loadItems = () => Promise.resolve(genericContentItems)
+
+storiesOf('ListPicker', module).add('default', () => <ListPickerComponent loadItems={loadItems} />)
