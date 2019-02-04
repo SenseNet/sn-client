5e3208163dca096ca8a541cff8a2e44b2da5a206
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-02-04 13:41:45 +0100

    [KFI]docs(storybook): list picker component initial version

---
+++ examples/sn-react-component-docs/stories/Pickers.stories.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react'
+import React from 'react'
+
+import { ListPickerComponent } from '@sensenet/pickers-react/src/ListPicker'
+
+storiesOf('ListPicker', module).add('default', () => <ListPickerComponent />)
