c22b71005c1b4cd35b9585e220c25efeb4003e73
Gallay Lajos <gallay.lajos@gmail.com>
2019-02-12 14:13:12 +0100

    [KFI]refactor(sn-dms-demo): store/actionlog
    refactored with action helpers

--- examples/sn-dms-demo/src/store/actionlog/reducers.ts
+++ examples/sn-dms-demo/src/store/actionlog/reducers.ts
@@ -1,6 +1,7 @@
+import { isFromAction } from '@sensenet/redux'
 import { Reducer } from 'redux'
 import { v1 } from 'uuid'
-import { addLogEntry, LogEntry, readLogEntries } from './actions'
+import { addLogEntry, initLog, LogEntry, readLogEntries } from './actions'
 
 export interface LogStateType {
   isInitialized: boolean
@@ -26,40 +27,39 @@ export const defaultLogState: LogStateType = {
 }
 
 export const logReducer: Reducer<LogStateType> = (state = defaultLogState, action) => {
-  switch (action.type) {
-    case 'SN_DMS_INIT_LOG':
-      return {
-        ...state,
-        isInitialized: true,
-      }
-    case 'SN_DMS_ADD_LOG_ENTRY':
-      return {
-        ...state,
-        entries: [
-          ...state.entries,
-          {
-            ...(action as ReturnType<typeof addLogEntry>).entry,
-            created: new Date(),
-            unread: true,
-            uuid: v1(),
-          },
-        ],
-      }
-    case 'SN_DMS_READ_LOG_ENTRIES':
-      return {
-        ...state,
-        entries: state.entries.map(e => {
-          const a = action as ReturnType<typeof readLogEntries>
-          if (a.entries.find(actionEntry => actionEntry.uuid === e.uuid)) {
-            return {
-              ...e,
-              unread: false,
-            }
-          }
-          return e
-        }),
-      }
-    default:
-      return state
+  if (isFromAction(action, initLog)) {
+    return {
+      ...state,
+      isInitialized: true,
+    }
   }
+  if (isFromAction(action, addLogEntry)) {
+    return {
+      ...state,
+      entries: [
+        ...state.entries,
+        {
+          ...action.entry,
+          created: new Date(),
+          unread: true,
+          uuid: v1(),
+        },
+      ],
+    }
+  }
+  if (isFromAction(action, readLogEntries)) {
+    return {
+      ...state,
+      entries: state.entries.map(e => {
+        if (action.entries.find(actionEntry => actionEntry.uuid === e.uuid)) {
+          return {
+            ...e,
+            unread: false,
+          }
+        }
+        return e
+      }),
+    }
+  }
+  return state
 }
