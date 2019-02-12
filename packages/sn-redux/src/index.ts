61be2cd203b8e0276be82afe2459e24b8366516f
Gallay Lajos <gallay.lajos@gmail.com>
2019-02-12 13:04:07 +0100

    [KFI]feat(redux): Added createAction() and isFromAction() helpers

--- packages/sn-redux/src/index.ts
+++ packages/sn-redux/src/index.ts
@@ -1,5 +1,7 @@
 import * as Actions from './Actions'
 import * as Reducers from './reducers'
 import * as Store from './Store'
+export * from './createAction'
+export * from './isFromAction'
 
 export { Actions, Reducers, Store }
