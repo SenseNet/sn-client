64d20f91bad38c09c0a53444051582f8a08af1a7
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-02-20 13:48:12 +0100

    [KFI]refactor(component-docs): react-docgen-loader to plugin

---
+++ examples/sn-react-component-docs/.storybook/docgen-plugin.js
@@ -0,0 +1,51 @@
+const docGen = require('react-docgen-typescript')
+const docGenLoader = require('react-docgen-typescript-loader/dist/generateDocgenCodeBlock.js')
+const ts = require('typescript')
+
+class DocgenPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('DocgenPlugin', compilation => {
+      compilation.hooks.seal.tap('DocgenPlugin', modules => {
+        const modulesToProcess = []
+        compilation.modules.forEach(module => {
+          // Skip ignored / external modules
+          if (!module.built || module.external || !module.rawRequest) {
+            return
+          }
+          if (/\/src.+.tsx/.test(module.request)) {
+            modulesToProcess.push(module)
+          }
+        })
+        const tsProgram = ts.createProgram(modulesToProcess.map(v => v.userRequest), {
+          jsx: ts.JsxEmit.React,
+          module: ts.ModuleKind.CommonJS,
+          target: ts.ScriptTarget.Latest,
+        })
+        modulesToProcess.forEach(m => processModule(m, tsProgram))
+      })
+    })
+  }
+}
+
+function processModule(module, tsProgram) {
+  if (!module) {
+    return
+  }
+  const componentDocs = docGen.withDefaultConfig().parseWithProgramProvider(module.userRequest, () => tsProgram)
+  if (!componentDocs.length) return
+  let source = module._source._value
+  source +=
+    '\n' +
+    docGenLoader
+      .default({
+        filename: module.userRequest,
+        source: module.userRequest,
+        componentDocs,
+        docgenCollectionName: 'STORYBOOK_REACT_CLASSES',
+        setDisplayName: true,
+      })
+      .substring(module.userRequest.length) +
+    '\n'
+  module._source._value = source
+}
+module.exports = DocgenPlugin
--- examples/sn-react-component-docs/.storybook/webpack.config.js
+++ examples/sn-react-component-docs/.storybook/webpack.config.js
@@ -2,35 +2,10 @@ const path = require('path')
 
 // tslint:disable-next-line: variable-name
 const TsConfigWebpackPlugin = require('ts-config-webpack-plugin')
+const DocgenPlugin = require('./docgen-plugin')
 
 const config = {
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [['react-app', { flow: false, typescript: true }]],
-            },
-          },
-          {
-            loader: require.resolve('react-docgen-typescript-loader'),
-          },
-        ],
-        include: [path.resolve(__dirname, '../stories')],
-      },
-      {
-        test: /\.stories\.tsx?$/,
-        loaders: [
-          { loader: require.resolve('@storybook/addon-storysource/loader'), options: { parser: 'typescript' } },
-        ],
-        enforce: 'pre',
-      },
-    ],
-  },
-  plugins: [new TsConfigWebpackPlugin()],
+  plugins: [new TsConfigWebpackPlugin(), new DocgenPlugin()],
   resolve: {
     symlinks: true,
     extensions: ['.ts', '.tsx'],
