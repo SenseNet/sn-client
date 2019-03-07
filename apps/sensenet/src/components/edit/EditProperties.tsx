b2a426eea7c6e5180a114a55334af07e978071ac
Gallay Lajos <gallay.lajos@gmail.com>
2019-03-07 18:07:43 +0100

    fix(admin-ui):early initialization bug fix for EditProperties

--- apps/sensenet/src/components/edit/EditProperties.tsx
+++ apps/sensenet/src/components/edit/EditProperties.tsx
@@ -1,7 +1,7 @@
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { Repository } from '@sensenet/client-core'
 import { EditView } from '@sensenet/controls-react'
-import { GenericContent } from '@sensenet/default-content-types'
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { connect } from 'react-redux'
 import { RouteComponentProps, withRouter } from 'react-router'
 import { ContentContextProvider } from '../../services/ContentContextProvider'
@@ -31,12 +31,9 @@ const GenericContentEditor: React.FunctionComponent<
 
   const injector = useContext(InjectorContext)
   const repo = injector.GetInstance(Repository)
-  const schema = repo.schemas.getSchemaByName(props.content.Type)
-
-  const [savedContent, setSavedContent] = useState({ ...props.content })
 
   return (
-    <div style={{ width: '100%', height: '100%', padding: '3em', overflow: 'auto' }}>
+    <div style={{ width: '100%', height: '100%', padding: '1em', overflow: 'auto' }}>
       <Breadcrumbs
         onItemClick={(_ev, item) => {
           props.history.push(item.url)
@@ -57,28 +54,21 @@ const GenericContentEditor: React.FunctionComponent<
           content: props.content,
         }}
       />
-      <EditView
-        schema={schema}
-        content={props.content}
-        repository={repo}
-        contentTypeName={props.content.Type}
-        onSubmit={async (id, content) => {
-          const change = {} as Partial<GenericContent>
-          const changeKeys = Object.keys(content)
-
-          for (const key of changeKeys) {
-            if (savedContent[key as keyof typeof props.content] !== content[key as keyof typeof content]) {
-              change[key as keyof typeof change] = content[key as keyof typeof content]
-            }
-          }
-
-          const result = await repo.patch({
-            idOrPath: id,
-            content: change,
-          })
-          setSavedContent({ ...result.d })
-        }}
-      />
+      {props.content.Id ? (
+        <EditView
+          content={props.content}
+          repository={repo}
+          contentTypeName={props.content.Type}
+          onSubmit={async (id, content) => {
+            repo.patch({
+              idOrPath: id,
+              content,
+            })
+          }}
+        />
+      ) : (
+        <CircularProgress />
+      )}
     </div>
   )
 }
