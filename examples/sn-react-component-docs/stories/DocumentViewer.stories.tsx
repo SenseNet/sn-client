5926831835a3a37b91c3066428466adf05147cab
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-03-19 16:42:45 +0100

    [KFI]refactor(component-docs): viewer example app story

--- examples/sn-react-component-docs/stories/DocumentViewer.stories.tsx
+++ examples/sn-react-component-docs/stories/DocumentViewer.stories.tsx
@@ -1,3 +1,4 @@
+import { ViewerExampleApp } from '@sensenet/document-viewer-react/example/example'
 import { CommentComponent } from '@sensenet/document-viewer-react/src/components'
 import { CreatedByUser } from '@sensenet/document-viewer-react/src/models'
 import { defaultLocalization } from '@sensenet/document-viewer-react/src/store/Localization'
@@ -14,39 +15,49 @@ const createdBy: CreatedByUser = {
   userName: 'some/name',
 }
 
+const centered = story => (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ flexBasis: '300px' }}>{story()}</div>
+  </div>
+)
+
 storiesOf('DocumentViewer', module)
   .addDecorator(withKnobs)
-  .addDecorator(story => (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ flexBasis: '300px' }}>{story()}</div>
-    </div>
-  ))
-  .add('a single comment with long text', () => (
-    <CommentComponent
-      createdBy={object('Created by', createdBy)}
-      page={1}
-      x={10}
-      y={10}
-      deleteComment={action('delete')}
-      id={text('id', 'randomId')}
-      localization={defaultLocalization}
-      text={text(
-        'commentBody',
-        `maros mester kis tanulóköréből Krisztián, Edgár és Böröndi Dani sikeresen letették a 480-as vizsgát! Gratulálunk nekik és köszönet marosnak a mentorálásért!
+  .add('showcase app', () => <ViewerExampleApp />, { info: { disable: true }, options: { showAddonPanel: false } })
+  .add(
+    'a single comment with long text',
+    () => (
+      <CommentComponent
+        createdBy={object('Created by', createdBy)}
+        page={1}
+        x={10}
+        y={10}
+        deleteComment={action('delete')}
+        id={text('id', 'randomId')}
+        localization={defaultLocalization}
+        text={text(
+          'commentBody',
+          `maros mester kis tanulóköréből Krisztián, Edgár és Böröndi Dani sikeresen letették a 480-as vizsgát! Gratulálunk nekik és köszönet marosnak a mentorálásért!
         A tanulókör készül a következő 486-os vizsgára. Cél az MCSD App Builder minősítés megszerzése, amelyhez a 486-os vizsga után már csak 1 vizsga kell.
         `,
-      )}
-    />
-  ))
-  .add('a single comment with short text', () => (
-    <CommentComponent
-      createdBy={createdBy}
-      page={1}
-      x={10}
-      y={10}
-      deleteComment={action('delete')}
-      id={text('id', 'randomId')}
-      localization={defaultLocalization}
-      text={text('commentBody', 'Lorem Ipsum is simply dummy text')}
-    />
-  ))
+        )}
+      />
+    ),
+    { decorators: [centered] },
+  )
+  .add(
+    'a single comment with short text',
+    () => (
+      <CommentComponent
+        createdBy={createdBy}
+        page={1}
+        x={10}
+        y={10}
+        deleteComment={action('delete')}
+        id={text('id', 'randomId')}
+        localization={defaultLocalization}
+        text={text('commentBody', 'Lorem Ipsum is simply dummy text')}
+      />
+    ),
+    { decorators: [centered] },
+  )
