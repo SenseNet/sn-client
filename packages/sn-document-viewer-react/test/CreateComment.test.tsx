b596b250350d5fdd3ce10249ef9c33a8d17b5c98
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-03-19 11:57:07 +0100

    [KFI]refactor(document-viewer): create comment marker placing

--- packages/sn-document-viewer-react/test/CreateComment.test.tsx
+++ packages/sn-document-viewer-react/test/CreateComment.test.tsx
@@ -1,5 +1,8 @@
 import Button from '@material-ui/core/Button'
 import FormControl from '@material-ui/core/FormControl'
+import IconButton from '@material-ui/core/IconButton'
+import Input from '@material-ui/core/Input'
+import SvgIcon from '@material-ui/core/SvgIcon'
 import { mount, shallow } from 'enzyme'
 import React from 'react'
 import { CreateComment, CreateCommentProps } from '../src/components'
@@ -12,6 +15,8 @@ describe('Create comment component', () => {
       commentInputPlaceholder: 'commentInputPlaceholder',
       submit: 'submit',
     },
+    handlePlaceMarkerClick: jest.fn(),
+    isPlacingMarker: false,
   }
 
   it('should show add comment button', () => {
@@ -32,5 +37,30 @@ describe('Create comment component', () => {
     wrapper.find(Button).simulate('click')
     wrapper.find(Button).simulate('click')
     expect(createComment).toBeCalled()
+  })
+
+  it('should clear input value after submitted', () => {
+    const createComment = jest.fn()
+    const wrapper = mount(<CreateComment {...defaultProps} createComment={createComment} />)
+    wrapper.find(Button).simulate('click')
+    wrapper.find(Input).simulate('change', { target: { value: 'Hello' } })
+    wrapper.find(Button).simulate('click')
+    wrapper.find(Button).simulate('click')
+    expect(wrapper.find(Input).prop('value')).toBe('')
+  })
+
+  it('should handle marker placement', () => {
+    const handlePlaceMarkerClick = jest.fn()
+    const wrapper = mount(<CreateComment {...defaultProps} handlePlaceMarkerClick={handlePlaceMarkerClick} />)
+    wrapper.find(Button).simulate('click')
+    wrapper.find(IconButton).simulate('click')
+    expect(handlePlaceMarkerClick).toBeCalled()
+  })
+
+  it("'s comment marker should look different when placing is happening", () => {
+    const wrapper = mount(<CreateComment {...defaultProps} isPlacingMarker={true} />)
+    wrapper.find(Button).simulate('click')
+    const iconStyle = wrapper.find(SvgIcon).prop('style')
+    expect(iconStyle).toEqual({ transform: 'rotate(90deg)', color: 'blue' })
   })
 })
