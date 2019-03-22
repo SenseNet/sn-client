3873a642c44d32394e134a1e599fb88da9d487d1
Gallay Lajos <gallay.lajos@gmail.com>
2019-03-22 16:33:39 +0100

    feat(admin-ui):redux cleanup

--- apps/sensenet/src/store/index.ts
+++ apps/sensenet/src/store/index.ts
@@ -4,14 +4,12 @@ import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import { ReduxDiMiddleware } from 'redux-di-middleware'
 import { commandPalette } from './CommandPalette'
 import { editContent } from './EditContent'
-import { persistedState } from './PersistedState'
 
 const sensenetDocumentViewer = sensenetDocumentViewerReducer
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const rootReducer = combineReducers({
-  persistedState,
   commandPalette,
   editContent,
   sensenetDocumentViewer,
