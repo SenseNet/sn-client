bff45c77f4ee2a044839c999f4f788cbe05a59cf
Aniko Litvanyi <herflis33@gmail.com>
2019-03-25 15:30:46 +0100

    Fix broken text

--- apps/sensenet/src/services/MonacoModels/PersonalSettingsModel.ts
+++ apps/sensenet/src/services/MonacoModels/PersonalSettingsModel.ts
@@ -41,7 +41,7 @@ languages.json.jsonDefaults.setDiagnosticsOptions({
               },
               displayName: {
                 type: 'string',
-                description: 'An optional user friendly name to display the repository when you're connected to it',
+                description: `An optional user friendly name to display the repository when you're connected to it`,
               },
             },
           },
