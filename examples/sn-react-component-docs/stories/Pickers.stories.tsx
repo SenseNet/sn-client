401c6f3dbcbb8d3d4cf223cb72f60bc8399e2685
gallayl <gallay.lajos@gmail.com>
2019-04-03 15:51:19 +0200

    fix(react-component-docs):fixed breaks(partial)

--- examples/sn-react-component-docs/stories/Pickers.stories.tsx
+++ examples/sn-react-component-docs/stories/Pickers.stories.tsx
@@ -1,11 +1,44 @@
 import ListItem from '@material-ui/core/ListItem/ListItem'
 import ListItemText from '@material-ui/core/ListItemText/ListItemText'
-import { genericContentItems, mockContent } from '@sensenet/pickers-react/__tests__/mocks/items'
+import { GenericContent } from '@sensenet/default-content-types'
 import { App } from '@sensenet/pickers-react/example/ExampleApp'
 import { ListPickerComponent } from '@sensenet/pickers-react/src/ListPicker'
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 const showCaseAppNotes = require('../notes/pickers/ConnectedToDms.md')
+
+export const mockContent = { Id: 1, Type: 'Folder', Path: '', Name: 'MockFolder' }
+
+export const genericContentItems: GenericContent[] = [
+  {
+    Id: 1,
+    Type: 'Folder',
+    Path: 'path',
+    Name: 'Content1',
+    DisplayName: 'Content 1',
+  },
+  {
+    Id: 2,
+    Type: 'Folder',
+    Path: 'path',
+    Name: 'Content2',
+    DisplayName: 'Content 2',
+  },
+  {
+    Id: 3,
+    Type: 'Folder',
+    Path: 'path',
+    Name: 'Content3',
+    DisplayName: 'Content 3',
+  },
+  {
+    Id: 4,
+    Type: 'Folder',
+    Path: 'path',
+    Name: 'Content4',
+    DisplayName: 'Content 4',
+  },
+]
 
 storiesOf('ListPicker', module)
   .add('Connected to repository', () => <App />, { showCaseAppNotes })
