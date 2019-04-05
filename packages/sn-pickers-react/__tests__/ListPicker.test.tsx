feeccbaab0548d26e5590310b46f0b94d7441eef
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-04-05 16:13:28 +0200

    [KFI]test(pickers-react): list picker tests

--- packages/sn-pickers-react/__tests__/ListPicker.test.tsx
+++ packages/sn-pickers-react/__tests__/ListPicker.test.tsx
@@ -1,16 +1,38 @@
+import { Repository } from '@sensenet/client-core'
 import { shallow } from 'enzyme'
 import React from 'react'
-import useAsync from 'react-use/lib/useAsync'
-import { ItemComponent } from '../src/ListPicker/Item'
-import { ListPickerComponent } from '../src/ListPicker/ListPicker'
-import { genericContentItems } from './mocks/items'
+import { useAsync } from 'react-async'
+import { ItemComponent, ListPickerComponent } from '../src/ListPicker'
+import { genericContentItems, mockContent } from './mocks/items'
 
-jest.mock('react-use/lib/useAsync')
+jest.mock('react-async')
 
 describe('List picker component', () => {
-  it('render list items', () => {
-    ;(useAsync as any).mockReturnValue({ value: undefined }).mockReturnValueOnce({ value: genericContentItems })
-    const wrapper = shallow(<ListPickerComponent loadParent={jest.fn()} loadItems={jest.fn()} />)
+  it('should render list items without parent', () => {
+    ;(useAsync as any).mockReturnValue({ data: undefined }).mockReturnValueOnce({ data: genericContentItems })
+    const wrapper = shallow(<ListPickerComponent repository={new Repository()} />)
+    expect(wrapper.find(ItemComponent).exists()).toBeTruthy()
     expect(wrapper.find(ItemComponent).length).toBe(4)
+  })
+
+  it('should render list items with parent', () => {
+    ;(useAsync as any).mockReturnValue({ data: mockContent }).mockReturnValueOnce({ data: genericContentItems })
+    const wrapper = shallow(<ListPickerComponent repository={new Repository()} />)
+    expect(wrapper.find(ItemComponent).exists()).toBeTruthy()
+    expect(wrapper.find(ItemComponent).length).toBe(5)
+  })
+
+  it('should render loading when parent is loading', () => {
+    ;(useAsync as any).mockReturnValue({ data: undefined, isLoading: true })
+    const loadingRenderer = jest.fn()
+    const wrapper = shallow(<ListPickerComponent renderLoading={loadingRenderer} repository={new Repository()} />)
+    expect(wrapper.find(ItemComponent).exists()).toBeFalsy()
+    expect(loadingRenderer).toBeCalled()
+  })
+
+  it('should not render loading when items are loading and render loading is not defined', () => {
+    ;(useAsync as any).mockReturnValue({ data: undefined }).mockReturnValueOnce({ data: undefined, isLoading: true })
+    const wrapper = shallow(<ListPickerComponent repository={new Repository()} />)
+    expect(wrapper.find(ItemComponent).exists()).toBeFalsy()
   })
 })
