c246e5c386dba3b05d205be4f77f2ff6c86ee79c
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-04-11 17:51:25 +0200

    [KFI]refactor(dms-demo): use name instead of id for form fields

--- examples/sn-dms-demo/src/pages/Login.tsx
+++ examples/sn-dms-demo/src/pages/Login.tsx
@@ -227,7 +227,7 @@ class Login extends React.Component<
               required={true}
               style={styles.formControl}>
               <TextField
-                id="email"
+                name="email"
                 onBlur={event => this.handleEmailBlur(event)}
                 onChange={event => this.handleEmailChange(event)}
                 fullWidth={true}
@@ -244,7 +244,7 @@ class Login extends React.Component<
               style={styles.formControl}>
               <TextField
                 type="password"
-                id="password"
+                name="password"
                 onBlur={event => this.handlePasswordBlur(event)}
                 onChange={event => this.handlePasswordChange(event)}
                 fullWidth={true}
