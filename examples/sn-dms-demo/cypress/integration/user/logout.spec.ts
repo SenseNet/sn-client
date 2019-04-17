9699fde9c62cd05c511f976da5b1236bf0ff463d
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-04-17 14:26:14 +0200

    [KFI]test(dms-demo): logout wait for element to load

--- examples/sn-dms-demo/cypress/integration/user/logout.spec.ts
+++ examples/sn-dms-demo/cypress/integration/user/logout.spec.ts
@@ -4,6 +4,7 @@ context('Logout', () => {
   })
 
   it('should navigate to login page', () => {
+    cy.contains('Document library').should('exist')
     cy.get('[aria-owns="actionmenu"] > .material-icons').click()
     cy.get('[title="Log out"]').click()
     cy.url().should('include', '/login')
