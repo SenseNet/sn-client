c0a9468d5d72084f3fa55ec2721f4b75fe017b8d
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-04-17 11:40:18 +0200

    [KFI]refactor(dms-demo): registerUser cypress command

--- examples/sn-dms-demo/cypress/integration/typings.d.ts
+++ examples/sn-dms-demo/cypress/integration/typings.d.ts
@@ -7,6 +7,7 @@ declare global {
   namespace Cypress {
     interface Chainable {
       login: (email: string, password: string) => void
+      registerUser: (email: string, password: string) => void
     }
   }
 }
--- examples/sn-dms-demo/cypress/support/commands.ts
+++ examples/sn-dms-demo/cypress/support/commands.ts
@@ -4,3 +4,18 @@ Cypress.Commands.add('login', (email, password) => {
     await win.repository.authentication.login(email, password)
   })
 })
+
+Cypress.Commands.add('registerUser', (email, password) => {
+  cy.visit('')
+  cy.window().then(async win => {
+    await win.repository.executeAction({
+      name: 'RegisterUser',
+      idOrPath: `/Root/IMS('Public')`,
+      body: {
+        email,
+        password,
+      },
+      method: 'POST',
+    })
+  })
+})
