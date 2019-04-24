802f90fcc58a6b313b072a6e06007499e336a932
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-04-24 13:02:46 +0200

    [KFI]test(dms-demo): create new document test with new user

--- examples/sn-dms-demo/cypress/integration/documents.spec.ts
+++ examples/sn-dms-demo/cypress/integration/documents.spec.ts
@@ -1,6 +1,25 @@
+import Chance = require('chance')
+
 context('The documents page', () => {
+  /**
+   * Change this to registered user once the test run for the first time to prevent new registration.
+   * @example ```js
+   * { email: 'miwor@sensenet.com', password: 'aY]w9UJ2j' }```
+   */
+  let currentUser = { email: '', password: '' }
+  before(async () => {
+    if (process.env.CI) {
+      currentUser = registerNewUser()
+    }
+  })
+
   beforeEach(() => {
-    cy.login('businesscat@sensenet.com', 'businesscat')
+    cy.login(currentUser.email, currentUser.password).then(isSuccesful => {
+      if (!isSuccesful) {
+        currentUser = registerNewUser()
+        cy.login(currentUser.email, currentUser.password)
+      }
+    })
   })
 
   it('header contains the logged in users avatar', () => {
@@ -8,6 +27,30 @@ context('The documents page', () => {
 
     cy.url().should('include', '/documents')
 
-    cy.get('div[aria-label="Business Cat"]').should('exist')
+    cy.get(`div[aria-label="${currentUser.email}"]`).should('exist')
+  })
+
+  it('creates new document', () => {
+    openNew('document')
+    const displayName = Chance().word()
+    cy.get('#DisplayName').type(displayName)
+    cy.get('#Watermark').type(Chance().word() + '{enter}')
+    cy.contains(displayName + ' is successfully created').should('exist')
+    cy.contains(displayName + '.').should('exist')
   })
 })
+
+const openNew = (action: string) => {
+  cy.visit('#/documents')
+  cy.contains('Document library').should('exist')
+  cy.contains('Documents').click()
+  cy.contains('New').click()
+  cy.get(`[title="New ${action}"]`).click()
+}
+
+const registerNewUser = () => {
+  const chance = new Chance()
+  const currentUser = { email: chance.email({ domain: 'sensenet.com' }), password: chance.string() }
+  cy.registerUser(currentUser.email, currentUser.password)
+  return currentUser
+}
--- examples/sn-dms-demo/cypress/support/commands.ts
+++ examples/sn-dms-demo/cypress/support/commands.ts
@@ -1,14 +1,23 @@
+import { User } from '@sensenet/default-content-types'
+
 Cypress.Commands.add('login', (email, password) => {
   cy.visit('')
+  Cypress.log({
+    name: 'login',
+    consoleProps: () => {
+      return { email, password }
+    },
+    message: [`${email} | ${password}`],
+  })
   cy.window().then(async win => {
-    await win.repository.authentication.login(email, password)
+    return await win.repository.authentication.login(email, password)
   })
 })
 
 Cypress.Commands.add('registerUser', (email, password) => {
   cy.visit('')
   cy.window().then(async win => {
-    await win.repository.executeAction({
+    return await win.repository.executeAction<{ email: string; password: string }, User>({
       name: 'RegisterUser',
       idOrPath: `/Root/IMS('Public')`,
       body: {
