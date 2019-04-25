c3c497e80da3d244cd496b67faeb4c5a5c35a23a
Aniko Litvanyi <herflis33@gmail.com>
2019-04-25 09:45:41 +0200

    Remove redux and related from the react-controls package (#177)
    * [KFI]refactor(controls-react): Remove redux related stuff from newview
    
    * [KFI]chore(controls-react): Remove redux related dependencies
    
    * [KFI]fix(controls-react): Fix package.json
    
    * [KFI]fix(dms-demo): Remove the unnecessary changeAction
    
    * [KFI]fix(react-component-docs): Fix the docs
    
    * [KFI]fix(controls-react): Fix loading fieldvalues
    
    * [KFI]fix(dms-demo): Add index, watermark and relateddoc fields to the select list
    
    * [KFI]fix(react-component-docs): Fix newview docs
    
    * [KFI]fix(controls-react): Fix extension in newview for files
    
    * [KFI]fix(dms-demo): Fix file extension values for adding files
    
    * [KFI]fix(dms-demo): Set documents as default menuitem
    
    * [KFI]chore(controls-react): Remove rootStateType and reducers from views index.ts
    
    * [KFI]refactor(controls-react): Rename NewViewComponent to NewView
    
    * [KFI]fix(controls-react): Remove rootStateType and reducers from views index.ts
    
    * [KFI]fix(sensenet): Rename NewViewComponent to NewView

--- packages/sn-controls-react/src/viewcontrols/index.ts
+++ packages/sn-controls-react/src/viewcontrols/index.ts
@@ -4,14 +4,6 @@
  * @description This module contains controls that displays a full content in a specific way
  */
 
-import { Reducers } from '@sensenet/redux'
 export { EditView } from './EditView'
-export { NewView, NewViewComponent } from './NewView'
+export { NewView } from './NewView'
 export { BrowseView } from './BrowseView'
-
-/**
- * Interface for root state type
- */
-export interface RootStateType {
-  sensenet: ReturnType<typeof Reducers.sensenet>
-}
