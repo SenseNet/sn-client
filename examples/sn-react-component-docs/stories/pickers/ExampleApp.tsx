75a07e912d8ddaae398bb174e98d605aecc4df52
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-05-02 12:05:52 +0200

    [KFI]docs(component-docs): list picker hook example

--- examples/sn-react-component-docs/stories/pickers/ExampleApp.tsx
+++ examples/sn-react-component-docs/stories/pickers/ExampleApp.tsx
@@ -1,6 +1,11 @@
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
 import { Repository } from '@sensenet/client-core'
 import { SchemaStore } from '@sensenet/default-content-types'
-import { ListPickerComponent } from '@sensenet/pickers-react'
+import { Icon, iconType } from '@sensenet/icons-react'
+import { ListPickerComponent, useListPicker } from '@sensenet/pickers-react'
 import React from 'react'
 
 const testRepository = new Repository({
@@ -26,4 +31,40 @@ export const ExampleApp = () => {
     return <p>{message}</p>
   }
   return <ListPickerComponent renderError={renderError} repository={testRepository} />
+}
+
+export const ExampleAppWithHook = () => {
+  const { parent, items, getListItemProps, selectedItem, path } = useListPicker(testRepository)
+  console.log({ selectedItem, path })
+
+  return (
+    <List>
+      {parent !== undefined ? (
+        <ListItem
+          button={true}
+          selected={selectedItem && parent.Id === selectedItem.Id}
+          onClick={e => getListItemProps().onClick(e, parent)}
+          onDoubleClick={e => getListItemProps().onDoubleClick(e, parent)}>
+          <ListItemIcon>
+            <Icon type={iconType.materialui} iconName="folder" style={{ color: 'yellow' }} />
+          </ListItemIcon>
+          <ListItemText primary={parent.DisplayName} />
+        </ListItem>
+      ) : null}
+      {items &&
+        items.map(node => (
+          <ListItem
+            button={true}
+            selected={selectedItem && node.Id === selectedItem.Id}
+            onClick={e => getListItemProps().onClick(e, node)}
+            onDoubleClick={e => getListItemProps().onDoubleClick(e, node)}
+            key={node.Id}>
+            <ListItemIcon>
+              <Icon type={iconType.materialui} iconName="folder" />
+            </ListItemIcon>
+            <ListItemText primary={node.DisplayName} />
+          </ListItem>
+        ))}
+    </List>
+  )
 }
