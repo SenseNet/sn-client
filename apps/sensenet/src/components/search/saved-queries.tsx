523b16db298b9b8c9f2b9e3cef6f28ba5525c279
gallayl <gallay.lajos@gmail.com>
2019-05-07 15:38:59 +0200

    feat(admin-ui):Saved queries list

--- apps/sensenet/src/components/search/saved-queries.tsx
+++ apps/sensenet/src/components/search/saved-queries.tsx
@@ -1,19 +1,55 @@
 import Checkbox from '@material-ui/core/Checkbox'
+import Fab from '@material-ui/core/Fab'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Tooltip from '@material-ui/core/Tooltip'
 import Typography from '@material-ui/core/Typography'
+
+import SearchIcon from '@material-ui/icons/Search'
 import { ConstantContent } from '@sensenet/client-core'
+import { debounce } from '@sensenet/client-utils'
 import { Query } from '@sensenet/default-content-types'
 import React, { useContext, useEffect, useState } from 'react'
-import { LocalizationContext, RepositoryContext } from '../../context'
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom'
+import {
+  ContentRoutingContext,
+  CurrentAncestorsContext,
+  CurrentChildrenContext,
+  CurrentContentContext,
+  InjectorContext,
+  LoadSettingsContext,
+  LocalizationContext,
+  RepositoryContext,
+} from '../../context'
+import { CollectionComponent } from '../ContentListPanel'
 
-const Search: React.FunctionComponent = () => {
+const Search: React.FunctionComponent<RouteComponentProps> = props => {
   const repo = useContext(RepositoryContext)
   const localization = useContext(LocalizationContext).values.search
+  const ctx = useContext(ContentRoutingContext)
+  const injector = useContext(InjectorContext)
 
   const [onlyPublic, setOnlyPublic] = useState(false)
   const [queries, setQueries] = useState<Query[]>([])
+  const [repoToken, setRepoToken] = useState(btoa(repo.configuration.repositoryUrl))
 
-  console.log(queries)
+  const [reloadToken, setReloadToken] = useState(Math.random())
+  const [requestReload] = useState(() => debounce(() => setReloadToken(Math.random()), 250))
+  const loadSettingsContext = useContext(LoadSettingsContext)
+
+  useEffect(() => {
+    setRepoToken(btoa(repo.configuration.repositoryUrl))
+  }, [repo.configuration.repositoryUrl])
+
+  useEffect(() => {
+    const eventHub = injector.getEventHub(repo.configuration.repositoryUrl)
+    const subscriptions = [
+      eventHub.onContentModified.subscribe(() => requestReload()),
+      eventHub.onContentCopied.subscribe(() => requestReload()),
+      eventHub.onContentCreated.subscribe(() => requestReload()),
+      eventHub.onContentDeleted.subscribe(() => requestReload()),
+    ]
+    return () => subscriptions.forEach(s => s.dispose())
+  }, [repo])
 
   useEffect(() => {
     repo
@@ -22,31 +58,76 @@ const Search: React.FunctionComponent = () => {
         name: 'GetQueries',
         method: 'GET',
         oDataOptions: {
+          ...loadSettingsContext.loadChildrenSettings,
           select: ['Query', 'Icon'],
           onlyPublic,
         } as any,
         body: undefined,
       })
       .then(result => setQueries(result.d.results))
-  }, [onlyPublic])
+  }, [reloadToken, loadSettingsContext.loadChildrenSettings])
   return (
-    <div style={{ padding: '1em', margin: '1em', overflow: 'auto' }}>
+    <div style={{ padding: '1em', margin: '1em', overflow: 'hidden' }}>
       <div>
-        <Typography variant="h3">{localization.title}</Typography>
+        <Typography variant="h3">{localization.savedQueries}</Typography>
         <FormControlLabel
-          style={{ marginLeft: '1em' }}
           label={localization.onlyPublic}
-          control={<Checkbox onChange={ev => setOnlyPublic(ev.target.checked)} />}
+          control={
+            <Checkbox
+              onChange={ev => {
+                setOnlyPublic(ev.target.checked)
+                requestReload()
+              }}
+            />
+          }
         />
+        <Tooltip title={localization.newSearch}>
+          <Link
+            style={{ textDecoration: 'none', position: 'fixed', bottom: '2em', right: '2em' }}
+            to={`/${repoToken}/search`}>
+            <Fab color="primary" title={localization.newSearch}>
+              <SearchIcon />
+            </Fab>
+          </Link>
+        </Tooltip>
       </div>
-      {/* 
-      {props.match.params.query ? (
-        `Queryke: ${props.match.params.query}`
-      ) : (
-        <Link to={generatePath(props.match.path, { query: 'alma', repo: repo.configuration.repositoryUrl })}>alma</Link>
-      )} */}
+      <div>
+        {queries.length > 0 ? (
+          <CurrentContentContext.Provider value={ConstantContent.PORTAL_ROOT}>
+            <CurrentChildrenContext.Provider value={queries}>
+              <CurrentAncestorsContext.Provider value={[]}>
+                <CollectionComponent
+                  style={{
+                    height: 'calc(100% - 75px)',
+                    overflow: 'auto',
+                  }}
+                  enableBreadcrumbs={false}
+                  parentId={0}
+                  onParentChange={() => {
+                    // ignore, only queries will be listed
+                  }}
+                  onActivateItem={p => {
+                    props.history.push(
+                      `/${btoa(repo.configuration.repositoryUrl)}/search/${encodeURIComponent(
+                        (p as Query).Query || '',
+                      )}`,
+                    )
+                  }}
+                  onTabRequest={() => {
+                    /** */
+                  }}
+                />
+              </CurrentAncestorsContext.Provider>
+            </CurrentChildrenContext.Provider>
+          </CurrentContentContext.Provider>
+        ) : (
+          <Typography variant="subtitle1" style={{ marginTop: '3em' }}>
+            {localization.noSavedQuery}
+          </Typography>
+        )}
+      </div>
     </div>
   )
 }
 
-export default Search
+export default withRouter(Search)
