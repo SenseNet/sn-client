91a07692d4d1d44cb2cc4ac96565aef13b42cc06
Gallay Lajos <gallay.lajos@gmail.com>
2019-05-09 17:31:29 +0200

    feat(admin-ui):Setup stub and localization

--- apps/sensenet/src/components/setup/index.tsx
+++ apps/sensenet/src/components/setup/index.tsx
@@ -1,13 +1,47 @@
-import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import React from 'react'
-import { Bullie } from '../Bullie'
+import { ConstantContent } from '@sensenet/client-core'
+import { Settings } from '@sensenet/default-content-types'
+import React, { useContext, useEffect, useState } from 'react'
+import { Query } from '../../../../../packages/sn-query/dist'
+import { LocalizationContext, RepositoryContext } from '../../context'
 
-const Setup: React.StatelessComponent = () => (
-  <Paper style={{ padding: '1em', margin: '1em', overflow: 'auto' }}>
-    <Typography variant="h3">Setup</Typography>
-    <Bullie />
-  </Paper>
-)
+const Setup: React.StatelessComponent = () => {
+  const repo = useContext(RepositoryContext)
+  const localization = useContext(LocalizationContext).values.settings
+  const [wellKnownSettings, setWellKnownSettings] = useState<Settings[]>([])
+  const [settings, setSettings] = useState<Settings[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await repo.loadCollection({
+        path: ConstantContent.PORTAL_ROOT.Path,
+        oDataOptions: {
+          query: new Query(q => q.typeIs(Settings)).toString() + ' .AUTOFILTERS:OFF',
+        },
+      })
+
+      setWellKnownSettings(
+        response.d.results.filter(setting => Object.keys(localization.descriptions).includes(setting.Path)),
+      )
+
+      setSettings(response.d.results.filter(setting => !Object.keys(localization.descriptions).includes(setting.Path)))
+    })()
+  }, [repo])
+
+  return (
+    <div style={{ padding: '1em', margin: '1em', overflow: 'auto' }}>
+      <Typography variant="h5">Setup</Typography>
+      WellKnownSettings:
+      {wellKnownSettings.map(s => (
+        <span key={s.Id}>{s.DisplayName || s.Name}</span>
+      ))}
+      <br />
+      Settings:{' '}
+      {settings.map(s => (
+        <span key={s.Id}>{s.DisplayName || s.Name}</span>
+      ))}
+    </div>
+  )
+}
 
 export default Setup
