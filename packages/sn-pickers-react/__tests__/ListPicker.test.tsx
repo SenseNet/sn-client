d3e3683a7972b0fea27351c1634442044a340f4e
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-05-22 13:25:15 +0200

    [KFI]test(pickers-react): more tests

--- packages/sn-pickers-react/__tests__/ListPicker.test.tsx
+++ packages/sn-pickers-react/__tests__/ListPicker.test.tsx
@@ -9,19 +9,39 @@ import { genericContentItems } from './mocks/items'
 jest.mock('react-async')
 
 describe('List picker component', () => {
-  it('should render list items without parent', () => {
+  it('should render list items', () => {
     ;(useAsync as any).mockReturnValue({ data: genericContentItems })
     const wrapper = shallow(<ListPickerComponent repository={new Repository()} />)
     expect(wrapper.find(ListItem).exists()).toBeTruthy()
     expect(wrapper.find(ListItem).length).toBe(4)
   })
 
-  it('should render loading when parent is loading', () => {
+  it('should call renderLoading when loading is true', () => {
     ;(useAsync as any).mockReturnValue({ data: undefined, isLoading: true })
     const loadingRenderer = jest.fn()
     const wrapper = shallow(<ListPickerComponent renderLoading={loadingRenderer} repository={new Repository()} />)
     expect(wrapper.find(ListItem).exists()).toBeFalsy()
     expect(loadingRenderer).toBeCalled()
+  })
+
+  it('should render nothing when no renderLoading and loading is true', () => {
+    ;(useAsync as any).mockReturnValue({ data: undefined, isLoading: true })
+    const wrapper = shallow(<ListPickerComponent repository={new Repository()} />)
+    expect(wrapper.find(ListItem).exists()).toBeFalsy()
+  })
+
+  it('should render an error message when error', () => {
+    ;(useAsync as any).mockReturnValue({ data: undefined, error: { message: 'Error' } })
+    const errorRenderer = jest.fn()
+    const wrapper = shallow(<ListPickerComponent renderError={errorRenderer} repository={new Repository()} />)
+    expect(wrapper.find(ListItem).exists()).toBeFalsy()
+    expect(errorRenderer).toBeCalled()
+  })
+
+  it('should render nothing when no renderError and error', () => {
+    ;(useAsync as any).mockReturnValue({ data: undefined, error: { message: 'Error' } })
+    const wrapper = shallow(<ListPickerComponent repository={new Repository()} />)
+    expect(wrapper.find(ListItem).exists()).toBeFalsy()
   })
 
   it('should handle navigation', () => {
@@ -35,9 +55,14 @@ describe('List picker component', () => {
     expect(onNavigation).toBeCalled()
   })
 
-  it('should not render loading when items are loading and render loading is not defined', () => {
-    ;(useAsync as any).mockReturnValue({ data: undefined }).mockReturnValueOnce({ data: undefined, isLoading: true })
-    const wrapper = shallow(<ListPickerComponent repository={new Repository()} />)
-    expect(wrapper.find(ListItem).exists()).toBeFalsy()
+  it('should handle selection', () => {
+    ;(useAsync as any).mockReturnValue({ data: undefined }).mockReturnValueOnce({ data: genericContentItems })
+    const onSelectionChanged = jest.fn()
+    const wrapper = mount(<ListPickerComponent onSelectionChanged={onSelectionChanged} repository={new Repository()} />)
+    wrapper
+      .find(ListItem)
+      .first()
+      .simulate('click')
+    expect(onSelectionChanged).toBeCalled()
   })
 })
--- packages/sn-pickers-react/__tests__/loaders.test.ts
+++ packages/sn-pickers-react/__tests__/loaders.test.ts
@@ -1,4 +1,5 @@
 import { defaultLoadItemsODataOptions, loadItems } from '../src/ListPicker/loaders'
+import { mockContent } from './mocks/items'
 
 const repository = {
   loadCollection: jest.fn(() => {
@@ -15,7 +16,7 @@ const repository = {
   load: jest.fn(() => {
     return { d: {} }
   }),
-}
+} as any
 
 const mockOdataOptions = {
   select: ['DisplayName'],
@@ -26,12 +27,22 @@ const mockOdataOptions = {
 
 describe('loadItems function', () => {
   it('should load with default OData options', async () => {
-    await loadItems({ repository, path: '' } as any)
+    await loadItems({ repository, path: '' })
     expect(repository.loadCollection).toBeCalledWith({ oDataOptions: defaultLoadItemsODataOptions, path: '' })
   })
 
   it('should load with provided OData options', async () => {
     await loadItems({ repository, path: '', itemsODataOptions: mockOdataOptions } as any)
     expect(repository.loadCollection).toBeCalledWith({ oDataOptions: mockOdataOptions, path: '' })
+  })
+
+  it('should load with parent', async () => {
+    repository.load = jest.fn(() => {
+      return { d: mockContent }
+    })
+    const items = await loadItems({ repository, path: '' })
+    expect(repository.load).toBeCalled()
+    expect(items).toHaveLength(2)
+    expect(items[0]).toHaveProperty('isParent', true)
   })
 })
