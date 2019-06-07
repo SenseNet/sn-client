5cfe211d470c961b31ce1ec21ea4cce4222855fe
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-06-07 17:09:40 +0200

    [KFI]chore: add cypress to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:cypress/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  env: { browser: true, node: true, es6: true, jest: true },
+  plugins: ['@typescript-eslint', 'prettier', 'cypress'],
+  env: { browser: true, node: true, es6: true, jest: true, 'cypress/globals': true },
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
-    project: './tsconfig-base.json',
-    warnOnUnsupportedTypeScriptVersion: false,
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   settings: {
     react: {
@@ -21,5 +29,6 @@ module.exports = {
     'react/prop-types': 'skipUndeclared',
     '@typescript-eslint/no-unused-vars': 'off', // Use Typescript own check for this
     'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    'prettier/prettier': 'error',
   },
 }
