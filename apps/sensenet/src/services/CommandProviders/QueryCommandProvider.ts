eef3e11dc3a16ad9c897e9b7dd3eeea9f0d50c28
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-06-13 15:10:45 +0200

    [KFI]style(snapp): 💅🏻 eslint

--- apps/sensenet/src/services/CommandProviders/QueryCommandProvider.ts
+++ apps/sensenet/src/services/CommandProviders/QueryCommandProvider.ts
@@ -33,21 +33,18 @@ export class QueryCommandProvider implements CommandProvider {
       },
     })
     return [
-      ...result.d.results.map(
-        content =>
-          ({
-            primaryText: content.DisplayName || content.Name,
-            secondaryText: content.Path,
-            url: ctx.getPrimaryActionUrl(content),
-            content,
-            icon: content.Icon,
-            hits: query
-              .substr(1)
-              .replace(/\*/g, ' ')
-              .replace(/\?/g, ' ')
-              .split(' '),
-          } as CommandPaletteItem),
-      ),
+      ...result.d.results.map(content => ({
+        primaryText: content.DisplayName || content.Name,
+        secondaryText: content.Path,
+        url: ctx.getPrimaryActionUrl(content),
+        content,
+        icon: content.Icon,
+        hits: query
+          .substr(1)
+          .replace(/\*/g, ' ')
+          .replace(/\?/g, ' ')
+          .split(' '),
+      })),
       {
         primaryText: this.localization.currentValues.getValue().search.openInSearchTitle,
         secondaryText: this.localization.currentValues.getValue().search.openInSearchDescription,
