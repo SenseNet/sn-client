a193862de07a10bb10f576c7db4f29a5d1253335
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-06-13 11:23:36 +0200

    [KFI]style(eslint): turn on plugin:@typescript-eslint/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:cypress/recommended',
     'prettier',
@@ -8,7 +9,7 @@ module.exports = {
     'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier', 'cypress', 'jsdoc', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'cypress', 'jsdoc', 'react-hooks'],
   env: { browser: true, node: true, es6: true, jest: true, 'cypress/globals': true },
   parserOptions: {
     ecmaVersion: 6,
@@ -27,12 +28,40 @@ module.exports = {
     'arrow-parens': ['error', 'as-needed'],
     'no-unused-vars': 'off',
     'no-console': 'off',
-    'react/prop-types': 'skipUndeclared',
+    'react/prop-types': 0,
     '@typescript-eslint/no-unused-vars': 'off', // Use Typescript own check for this
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          accessors: 'explicit',
+          constructors: 'no-public',
+          methods: 'explicit',
+          properties: 'off',
+          parameterProperties: 'explicit',
+        },
+      },
+    ],
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/array-type': ['error', 'array-simple'],
     'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
     'prettier/prettier': 'error',
     'require-jsdoc': 1,
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
   },
+  overrides: [
+    {
+      files: ['packages/**/test/**/*.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-object-literal-type-assertion': 'off',
+      },
+    },
+  ],
 }
