15529304907f94293b07fc0c8454bdaddce9a1f3
Gallay Lajos <gallay.lajos@gmail.com>
2019-06-17 12:40:14 +0200

    feat(admin-ui):RequestCounterService

---
+++ apps/sensenet/src/services/request-counter-service.ts
@@ -0,0 +1,74 @@
+import { Injectable, Injector } from '@furystack/inject'
+import { IDisposable } from 'monaco-editor'
+import { ScopedLogger } from '@furystack/logging'
+
+interface RequestCounterRecord {
+  all: number
+  sites: {
+    [key: string]: number
+  }
+}
+
+@Injectable({ lifetime: 'singleton' })
+export class RequestCounterService implements IDisposable {
+  private readonly logger: ScopedLogger
+  private readonly writeInterval: number
+  public dispose() {
+    clearInterval(this.writeInterval)
+    this.write()
+  }
+
+  private readonly defaultState: RequestCounterRecord = {
+    all: 0,
+    sites: {},
+  }
+
+  private hasChanged: boolean = false
+
+  private currentState: RequestCounterRecord = { ...this.defaultState }
+
+  public reset() {
+    this.currentState = { ...this.defaultState }
+    this.hasChanged = true
+    this.logger.information({ message: 'Request counter reseted.' })
+  }
+
+  private getStorageKey() {
+    // SN-RequestCounterService-2019-06-17
+    return `SN-${this.constructor.name}-${new Date().toISOString().split('T')[0]}`
+  }
+
+  public countRequest(site: string) {
+    this.currentState.all += 1
+    this.currentState.sites[site] ? (this.currentState.sites[site] += 1) : (this.currentState.sites[site] = 1)
+    this.hasChanged = true
+  }
+
+  private read() {
+    try {
+      const stored = localStorage.getItem(this.getStorageKey())
+      if (stored) {
+        this.currentState = JSON.parse(stored)
+      }
+    } catch (e) {
+      // ignore
+      this.logger.warning({ message: 'Error reading / parsing request count from local storage' })
+    }
+  }
+
+  private write() {
+    if (this.hasChanged) {
+      localStorage.setItem(this.getStorageKey(), JSON.stringify(this.currentState))
+      this.hasChanged = false
+      this.logger.verbose({ message: `Request count changes persisted. Total: ${this.currentState.all}` })
+    }
+  }
+
+  constructor(injector: Injector) {
+    this.logger = injector.logger.withScope(this.constructor.name)
+    this.read()
+    this.writeInterval = setInterval(() => {
+      this.write()
+    }, 5000)
+  }
+}
