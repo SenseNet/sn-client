dccd6be4b7c649867b2df5319692c19ce1729763
Gallay Lajos <gallay.lajos@gmail.com>
2019-06-20 21:13:59 +0200

    feat(admin-ui):Markdown widget replaces

--- apps/sensenet/src/components/dashboard/markdown-widget.tsx
+++ apps/sensenet/src/components/dashboard/markdown-widget.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Typography } from '@material-ui/core'
 import ReactMarkdown from 'react-markdown'
 import { MarkdownWidget as MarkdownWidgetModel } from '../../services/PersonalSettings'
+import { SessionContext } from '../../context'
+import { useRepository, usePersonalSettings } from '../../hooks'
 
 export const MarkdownWidget: React.FunctionComponent<MarkdownWidgetModel> = props => {
+  const [replacedContent, setReplacedContent] = useState('')
+  const session = useContext(SessionContext)
+  const repo = useRepository()
+  const personalSettings = usePersonalSettings()
+
+  useEffect(() => {
+    const currentRepo = personalSettings.repositories.find(r => r.url === repo.configuration.repositoryUrl)
+
+    const newReplacedContent = props.settings.content
+      .replace(
+        '{currentUserName}',
+        session.currentUser.FullName || session.currentUser.DisplayName || session.currentUser.Name,
+      )
+      .replace(
+        '{currentRepositoryName}',
+        currentRepo && currentRepo.displayName
+          ? currentRepo.displayName
+          : repo.configuration.repositoryUrl || repo.configuration.repositoryUrl,
+      )
+      .replace('{currentRepositoryUrl}', repo.configuration.repositoryUrl)
+
+    setReplacedContent(newReplacedContent)
+  }, [
+    personalSettings.repositories,
+    props.settings.content,
+    repo.configuration.repositoryUrl,
+    session.currentUser.DisplayName,
+    session.currentUser.FullName,
+    session.currentUser.Name,
+  ])
+
   return (
     <div>
       <Typography
@@ -14,7 +47,7 @@ export const MarkdownWidget: React.FunctionComponent<MarkdownWidgetModel> = prop
         {props.title}
       </Typography>
       <div style={{ overflow: 'auto' }}>
-        <ReactMarkdown escapeHtml={false} source={props.settings.content} />
+        <ReactMarkdown escapeHtml={false} source={replacedContent} />
       </div>
     </div>
   )
