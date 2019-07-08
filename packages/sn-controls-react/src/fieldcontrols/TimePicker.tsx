737cd315406d49e1d2e73c29ec4960e24d100c21
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-07-08 12:56:46 +0200

    [KFI]test(controls): timepicker to function + tests

--- packages/sn-controls-react/src/fieldcontrols/TimePicker.tsx
+++ packages/sn-controls-react/src/fieldcontrols/TimePicker.tsx
@@ -4,66 +4,55 @@
 import MomentUtils from '@date-io/moment'
 import { MaterialUiPickersDate, MuiPickersUtilsProvider, TimePicker as MUITimePicker } from '@material-ui/pickers'
 import moment from 'moment'
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
 import { DateTimeFieldSetting } from '@sensenet/default-content-types'
+import Typography from '@material-ui/core/Typography'
 import { ReactClientFieldSetting } from './ClientFieldSetting'
 
 /**
- * Interface for TimePicker state
- */
-export interface TimePickerState {
-  value: string
-}
-/**
  * Field control that represents a DateTime field. Available values will be populated from the FieldSettings.
  */
-export class TimePicker extends React.Component<ReactClientFieldSetting<DateTimeFieldSetting>, TimePickerState> {
-  state: TimePickerState = {
-    value: this.props.fieldValue || this.props.settings.DefaultValue || moment().toISOString(),
-  }
+export function TimePicker(props: ReactClientFieldSetting<DateTimeFieldSetting>) {
+  const initialState = props.fieldValue || props.settings.DefaultValue || moment().toISOString()
+  const [value, setValue] = useState(initialState)
 
-  /**
-   * handle changes
-   * @param {MaterialUiPickersDate} date
-   */
-  public handleDateChange = (date: MaterialUiPickersDate) => {
+  const handleDateChange = (date: MaterialUiPickersDate) => {
     if (!date) {
       return
     }
-    this.setState({
-      value: moment.utc(date).toString(),
-    })
-    this.props.fieldOnChange && this.props.fieldOnChange(this.props.settings.Name, moment.utc(date))
+    setValue(moment.utc(date).toString())
+    props.fieldOnChange && props.fieldOnChange(props.settings.Name, moment.utc(date))
   }
 
-  public render() {
-    const { value } = this.state
-    switch (this.props.actionName) {
-      case 'edit':
-      case 'new':
-        return (
-          <MuiPickersUtilsProvider utils={MomentUtils}>
-            <Fragment>
-              <MUITimePicker
-                value={value}
-                onChange={this.handleDateChange}
-                label={this.props.settings.DisplayName}
-                id={this.props.settings.Name as string}
-                disabled={this.props.settings.ReadOnly}
-                placeholder={this.props.settings.DisplayName}
-                required={this.props.settings.Compulsory}
-                fullWidth={true}
-              />
-            </Fragment>
-          </MuiPickersUtilsProvider>
-        )
-      default:
-        return this.props.fieldValue ? (
-          <div>
-            <label>{this.props.settings.DisplayName}</label>
-            <p>{moment(this.props.fieldValue).format('HH:mm:ss')}</p>
-          </div>
-        ) : null
-    }
+  switch (props.actionName) {
+    case 'edit':
+    case 'new':
+      return (
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <MUITimePicker
+            value={value}
+            name={props.settings.Name}
+            onChange={handleDateChange}
+            label={props.settings.DisplayName}
+            id={props.settings.Name}
+            disabled={props.settings.ReadOnly}
+            placeholder={props.settings.DisplayName}
+            required={props.settings.Compulsory}
+            fullWidth={true}
+          />
+        </MuiPickersUtilsProvider>
+      )
+    case 'browse':
+    default:
+      return props.fieldValue ? (
+        <div>
+          <Typography variant="caption" gutterBottom={true}>
+            {props.settings.DisplayName}
+          </Typography>
+          <Typography variant="body1" gutterBottom={true}>
+            {moment(props.fieldValue).format('HH:mm:ss')}
+          </Typography>
+        </div>
+      ) : null
   }
 }
