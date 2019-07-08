fa43e32ef2fabe3f9196549c8ffc1a09eb5f2f3f
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-07-08 15:50:55 +0200

    [KFI]test(controls): password to function + tests 🧪

---
+++ packages/sn-controls-react/test/Password.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import Typography from '@material-ui/core/Typography'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import IconButton from '@material-ui/core/IconButton'
+import { Password } from '../src/fieldcontrols/Password'
+
+describe('Password field control', () => {
+  const defaultSettings = {
+    Type: 'PasswordFieldSetting',
+    Name: 'Password',
+    FieldClassName: 'SenseNet.ContentRepository.Fields.PasswordField',
+    DisplayName: 'Password',
+    Description: 'User password.',
+  }
+
+  it('should render the display name of the control in browse mode', () => {
+    const wrapper = shallow(<Password actionName="browse" settings={defaultSettings} />)
+    expect(wrapper.find(Typography).text()).toBe(defaultSettings.DisplayName)
+  })
+
+  it('should set all the props', () => {
+    const wrapper = shallow(
+      <Password
+        actionName="edit"
+        settings={{
+          ...defaultSettings,
+          ReadOnly: true,
+          Compulsory: true,
+        }}
+      />,
+    )
+    expect(wrapper.find(InputLabel).text()).toBe(defaultSettings.DisplayName)
+    expect(wrapper.find(Input).prop('name')).toBe(defaultSettings.Name)
+    expect(wrapper.find(Input).prop('id')).toBe(defaultSettings.Name)
+    expect(wrapper.find(Input).prop('placeholder')).toBe(defaultSettings.DisplayName)
+    expect(wrapper.find(Input).prop('required')).toBeTruthy()
+    expect(wrapper.find(Input).prop('disabled')).toBeTruthy()
+    expect(wrapper.find(FormHelperText).text()).toBe(defaultSettings.Description)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should be able to show the password as text', () => {
+    const wrapper = mount(<Password actionName="new" settings={defaultSettings} />)
+    wrapper.find(IconButton).simulate('click')
+    expect(wrapper.find(Input).prop('type')).toBe('text')
+  })
+})
