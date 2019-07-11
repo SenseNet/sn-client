86e2240c7c9f94c040e825fe541109ac6bd8c60f
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-07-11 12:18:58 +0200

    [KFI]refactor(controls): empty field control to function

--- packages/sn-controls-react/src/fieldcontrols/EmptyFieldControl.tsx
+++ packages/sn-controls-react/src/fieldcontrols/EmptyFieldControl.tsx
@@ -1,19 +1,10 @@
 /**
  * @module FieldControls
  */
-import { Component } from 'react'
-import { ReactClientFieldSetting } from './ClientFieldSetting'
 
 /**
  * Field control that represents obsolete fieldcontrols.
  */
-
-export class EmptyFieldControl extends Component<ReactClientFieldSetting, {}> {
-  /**
-   * render
-   * @return {ReactElement} markup
-   */
-  public render() {
-    return null
-  }
+export function EmptyFieldControl() {
+  return null
 }
