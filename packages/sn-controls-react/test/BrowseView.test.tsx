16904c648acc932bab0c53add60e42624c5d5139
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-07-17 17:51:00 +0200

    [KFI]test(controls): browse view 🧪

---
+++ packages/sn-controls-react/test/BrowseView.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Repository } from '@sensenet/client-core'
+import { GenericContent, VersioningMode } from '@sensenet/default-content-types'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import { BrowseView } from '../src/viewcontrols/BrowseView'
+import { schema } from './Mocks/schema'
+
+export const testRepository = new Repository({
+  repositoryUrl: 'https://devservice.demo.sensenet.com',
+  requiredSelect: ['Id', 'Path', 'Name', 'Type', 'ParentId', 'DisplayName'],
+  schemas: schema,
+  sessionLifetime: 'expiration',
+})
+
+export const testFile: GenericContent = {
+  Id: 1,
+  Name: 'Sample-document.docx',
+  DisplayName: 'Sample-document.docx',
+  Path: '/Root/Profiles/Public/alba/Document_Library/Sample-document.docx',
+  Type: 'File',
+  Index: 42,
+  VersioningMode: [VersioningMode.Option0],
+  AllowedChildTypes: [1, 2],
+  Description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec iaculis lectus, sed blandit urna. Nullam in auctor odio, eu eleifend diam. Curabitur rutrum ullamcorper nunc, sit amet consectetur turpis elementum ac. Aenean lorem lorem, feugiat sit amet sem at, accumsan cursus leo.',
+}
+
+describe('Browse view component', () => {
+  it('should render all the controls for Generic content', () => {
+    const wrapper = shallow(<BrowseView content={testFile} repository={testRepository}></BrowseView>)
+    // Do not render the repository, because of the schema it grows big
+    const json = toJson(wrapper, {
+      mode: 'shallow',
+      noKey: false,
+      map: info => {
+        if (!info.props) {
+          return info
+        }
+        if (info.props.repository) {
+          info.props.repository = '[repository]'
+        }
+
+        return info
+      },
+    })
+    expect(json).toMatchSnapshot({})
+  })
+})
