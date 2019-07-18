faca7cd2175d5e290f7cb39934dcb1c03bdf3bb9
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-07-18 16:12:02 +0200

    [KFI]refactor(controls): remove snapshot tests from view controls

--- packages/sn-controls-react/test/BrowseView.test.tsx
+++ packages/sn-controls-react/test/BrowseView.test.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 import { Repository } from '@sensenet/client-core'
 import { GenericContent, VersioningMode } from '@sensenet/default-content-types'
 import { shallow } from 'enzyme'
+import Typography from '@material-ui/core/Typography'
 import { BrowseView } from '../src/viewcontrols/BrowseView'
+import {
+  AllowedChildTypes,
+  Avatar,
+  BooleanComponent,
+  CheckboxGroup,
+  ColorPicker,
+  DatePicker,
+  DropDownList,
+  EmptyFieldControl,
+  FileName,
+  Name,
+  NumberComponent,
+  Password,
+  RadioButtonGroup,
+  ReferenceGrid,
+  RichTextEditor,
+  ShortText,
+  TagsInput,
+  Textarea,
+} from '../src/fieldcontrols'
 import { schema } from './__mocks__/schema'
-import { json } from './__mocks__/snapshotSerializer'
 
 export const testRepository = new Repository({
   repositoryUrl: 'https://devservice.demo.sensenet.com',
@@ -29,6 +49,24 @@ export const testFile: GenericContent = {
 describe('Browse view component', () => {
   it('should render all the controls for Generic content', () => {
     const wrapper = shallow(<BrowseView content={testFile} repository={testRepository}></BrowseView>)
-    expect(json(wrapper)).toMatchSnapshot()
+    expect(wrapper.find(Typography).text()).toBe(testFile.DisplayName)
+    expect(wrapper.find(FileName)).toHaveLength(1)
+    expect(wrapper.find(TagsInput)).toHaveLength(1)
+    expect(wrapper.find(ReferenceGrid)).toHaveLength(1)
+    expect(wrapper.find(Name)).toHaveLength(1)
+    expect(wrapper.find(ShortText)).toHaveLength(2)
+    expect(wrapper.find(EmptyFieldControl)).toHaveLength(1)
+    expect(wrapper.find(RichTextEditor)).toHaveLength(2)
+    expect(wrapper.find(Password)).toHaveLength(1)
+    expect(wrapper.find(BooleanComponent)).toHaveLength(2)
+    expect(wrapper.find(NumberComponent)).toHaveLength(3)
+    expect(wrapper.find(AllowedChildTypes)).toHaveLength(1)
+    expect(wrapper.find(CheckboxGroup)).toHaveLength(2)
+    expect(wrapper.find(DatePicker)).toHaveLength(1)
+    expect(wrapper.find(DropDownList)).toHaveLength(2)
+    expect(wrapper.find(RadioButtonGroup)).toHaveLength(1)
+    expect(wrapper.find(Textarea)).toHaveLength(2)
+    expect(wrapper.find(Avatar)).toHaveLength(1)
+    expect(wrapper.find(ColorPicker)).toHaveLength(2)
   })
 })
