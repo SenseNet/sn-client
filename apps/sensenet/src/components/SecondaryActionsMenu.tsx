beb31a75212808f2743f02e83421113029eb585a
Gallay Lajos <gallay.lajos@gmail.com>
2019-07-22 14:24:07 +0200

    chore(admin-ui):Secondary actions menu unneccessary content cleanup

--- apps/sensenet/src/components/SecondaryActionsMenu.tsx
+++ apps/sensenet/src/components/SecondaryActionsMenu.tsx
@@ -1,13 +1,11 @@
 import IconButton from '@material-ui/core/IconButton'
 import MoreHoriz from '@material-ui/icons/MoreHoriz'
-import React, { useContext, useState } from 'react'
-import { CurrentContentContext } from '../context'
+import React, { useState } from 'react'
 import { ContentContextMenu } from './ContentContextMenu'
 
 export const SecondaryActionsMenu: React.FunctionComponent<{
   style?: React.CSSProperties
 }> = props => {
-  const content = useContext(CurrentContentContext)
   const [isOpened, setIsOpened] = useState(false)
   const [ref, setRef] = useState<HTMLElement | null>(null)
 
@@ -23,22 +21,20 @@ export const SecondaryActionsMenu: React.FunctionComponent<{
         }}>
         <MoreHoriz />
       </IconButton>
-      <CurrentContentContext.Provider value={content}>
-        <ContentContextMenu
-          isOpened={isOpened}
-          onOpen={() => setIsOpened(true)}
-          onClose={() => setIsOpened(false)}
-          menuProps={{
-            anchorEl: ref,
-            disablePortal: true,
-            BackdropProps: {
-              onClick: () => setIsOpened(false),
-              onContextMenu: ev => ev.preventDefault(),
-            },
-          }}
-          drawerProps={{}}
-        />
-      </CurrentContentContext.Provider>
+      <ContentContextMenu
+        isOpened={isOpened}
+        onOpen={() => setIsOpened(true)}
+        onClose={() => setIsOpened(false)}
+        menuProps={{
+          anchorEl: ref,
+          disablePortal: true,
+          BackdropProps: {
+            onClick: () => setIsOpened(false),
+            onContextMenu: ev => ev.preventDefault(),
+          },
+        }}
+        drawerProps={{}}
+      />
     </div>
   )
 }
