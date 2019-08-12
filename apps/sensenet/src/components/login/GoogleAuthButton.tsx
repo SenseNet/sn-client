92c948d3afd1ffbdd5dc05ae76a1f3cbbed130c5
Gallay Lajos <gallay.lajos@gmail.com>
2019-08-12 14:31:57 +0200

    fix(admin-ui):Login and logout bugs

--- apps/sensenet/src/components/login/GoogleAuthButton.tsx
+++ apps/sensenet/src/components/login/GoogleAuthButton.tsx
@@ -1,26 +1,24 @@
-import React, { useEffect, useRef } from 'react'
-import { JwtService } from '@sensenet/authentication-jwt'
-import { addGoogleAuth, GoogleOauthProvider } from '@sensenet/authentication-google'
-import { useRepository } from '../../hooks'
+import React from 'react'
 import { OAuthButton } from './OAuthButton'
 
 export const GoogleAuthButton = () => {
-  const repository = useRepository()
-  const googleOauthProvider = useRef<GoogleOauthProvider>()
-  useEffect(() => {
-    const jwt = new JwtService(repository)
-    googleOauthProvider.current = addGoogleAuth(jwt, {
-      clientId: '', // We are going to add this later
-    })
-    return () => {
-      jwt.dispose()
-      googleOauthProvider.current && googleOauthProvider.current.dispose()
-    }
-  }, [repository])
+  // Mixing JWT with Forms is not safe at the moment.
 
-  const onClickHandler = () => {
-    googleOauthProvider.current && googleOauthProvider.current.login()
-  }
-
-  return <OAuthButton buttonProps={{ onClick: onClickHandler, disabled: true }} buttonText="Google" iconName="google" />
+  // const repository = useRepository()
+  // const googleOauthProvider = useRef<GoogleOauthProvider>()
+  // useEffect(() => {
+  //   const jwt = new JwtService(repository)
+  //   googleOauthProvider.current = addGoogleAuth(jwt, {
+  //     clientId: '', // We are going to add this later
+  //   })
+  //   return () => {
+  //     jwt.dispose()
+  //     googleOauthProvider.current && googleOauthProvider.current.dispose()
+  //   }
+  // }, [repository])
+  // const onClickHandler = () => {
+  //   googleOauthProvider.current && googleOauthProvider.current.login()
+  // }
+  // return <OAuthButton buttonProps={{ onClick: onClickHandler, disabled: true }} buttonText="Google" iconName="google" />
+  return <OAuthButton buttonProps={{ disabled: true }} buttonText="Google" iconName="google" />
 }
