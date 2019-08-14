892cad3363798dfd8f5b5d9ca03b91e48d2797a0
Anikó Litványi <herflis33@gmail.com>
2019-08-14 15:20:31 +0200

    Display empty modifiedby cell when somebody its the value

--- apps/sensenet/src/components/content-list/reference-field.tsx
+++ apps/sensenet/src/components/content-list/reference-field.tsx
@@ -6,10 +6,12 @@ import { Icon } from '../Icon'
 export const ReferenceField: React.FC<{ content: GenericContent }> = ({ content }) => {
   return (
     <TableCell padding={'none'}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <Icon item={content} />
-        <div style={{ marginLeft: '1em' }}>{content.DisplayName || content.Name}</div>
-      </div>
+      {content.DisplayName !== 'Somebody' ? (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Icon item={content} />
+          <div style={{ marginLeft: '1em' }}>{content.DisplayName || content.Name}</div>
+        </div>
+      ) : null}
     </TableCell>
   )
 }
