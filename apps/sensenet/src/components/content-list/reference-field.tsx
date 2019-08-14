43a6931f7beb7858763a6cc121e8fcf253377ea5
Aniko Litvanyi <herflis33@gmail.com>
2019-08-14 16:48:05 +0200

    Change displayname to name in the somebody -check

--- apps/sensenet/src/components/content-list/reference-field.tsx
+++ apps/sensenet/src/components/content-list/reference-field.tsx
@@ -6,7 +6,7 @@ import { Icon } from '../Icon'
 export const ReferenceField: React.FC<{ content: GenericContent }> = ({ content }) => {
   return (
     <TableCell padding={'none'}>
-      {content.DisplayName !== 'Somebody' ? (
+      {content.Name !== 'Somebody' ? (
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <Icon item={content} />
           <div style={{ marginLeft: '1em' }}>{content.DisplayName || content.Name}</div>
