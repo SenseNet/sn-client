9ac0328387495b230701978dc41300b35b825296
Gallay Lajos <gallay.lajos@gmail.com>
2019-08-22 13:07:01 +0200

    feat(document-viewer): Regenerate com.to FC, added progress indicator

--- packages/sn-document-viewer-react/src/components/DocumentViewerRegeneratePreviews.tsx
+++ packages/sn-document-viewer-react/src/components/DocumentViewerRegeneratePreviews.tsx
@@ -1,7 +1,7 @@
 import Typography from '@material-ui/core/Typography'
-import React = require('react')
 import { connect } from 'react-redux'
-import { Button } from '@material-ui/core'
+import { Button, CircularProgress } from '@material-ui/core'
+import React, { useState } from 'react'
 import { componentType } from '../services/TypeHelpers'
 import { regeneratePreviews, RootReducerType } from '../store'
 import { LayoutAppBar } from './LayoutAppBar'
@@ -25,44 +25,51 @@ const mapDispatchToProps = {
   regeneratePreviews,
 }
 
-class DocumentViewerRegeneratePreviewsComponent extends React.Component<
+const DocumentViewerRegeneratePreviewsComponent: React.FC<
   componentType<typeof mapStateToProps, typeof mapDispatchToProps>
-> {
+> = props => {
+  const [isRegenerating, setIsRegenerating] = useState(false)
+
   /**
    * renders the component
    */
-  public render() {
-    return (
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}>
+      <LayoutAppBar style={{ position: 'fixed', top: 0 }}>
+        <span />
+      </LayoutAppBar>
       <div
         style={{
           display: 'flex',
-          justifyContent: 'space-around',
-          alignContent: 'center',
+          justifyContent: 'space-between',
           alignItems: 'center',
-          height: '100%',
+          flexDirection: 'column',
+          maxWidth: 500,
+          margin: '.5em 0 .6em 0',
         }}>
-        <LayoutAppBar style={{ position: 'fixed', top: 0 }}>
-          <span />
-        </LayoutAppBar>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            flexDirection: 'column',
-            maxWidth: 500,
-            margin: '.5em 0 .6em 0',
+        <Typography variant="subtitle1" color="textSecondary" align="center" gutterBottom>
+          {props.regeneratePreviewsText}
+        </Typography>
+        <Button
+          variant="contained"
+          disabled={isRegenerating}
+          onClick={() => {
+            setIsRegenerating(true)
+            props.regeneratePreviews()
           }}>
-          <Typography variant="subtitle1" color="textSecondary" align="center" gutterBottom>
-            {this.props.regeneratePreviewsText}
-          </Typography>
-          <Button variant="contained" onClick={this.props.regeneratePreviews}>
-            {this.props.regenerateButton}
-          </Button>
-        </div>
+          {isRegenerating && <CircularProgress size={24} style={{ marginRight: '1em' }} />}
+          {props.regenerateButton}
+        </Button>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const connectedComponent = connect(
