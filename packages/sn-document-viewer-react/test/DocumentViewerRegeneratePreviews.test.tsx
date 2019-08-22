83aa7aceb5da3c96dde9c0bb66cf7a0cc792c955
Gallay Lajos <gallay.lajos@gmail.com>
2019-08-22 14:28:48 +0200

    test(document-viewer):regeneratePreviews action

--- packages/sn-document-viewer-react/test/DocumentViewerRegeneratePreviews.test.tsx
+++ packages/sn-document-viewer-react/test/DocumentViewerRegeneratePreviews.test.tsx
@@ -2,6 +2,9 @@ import { shallow } from 'enzyme'
 import React from 'react'
 import { Button } from '@material-ui/core'
 import { DocumentViewerRegeneratePreviewsComponent } from '../src/components/DocumentViewerRegeneratePreviews'
+import { defaultState, regeneratePreviews } from '../src/store'
+import { DocumentViewerSettings } from '../src/models'
+import { defaultSettings } from './__Mocks__/viewercontext'
 
 describe('Document Regenerate Previews component', () => {
   it('should render without crashing', () => {
@@ -34,5 +37,24 @@ describe('Document Regenerate Previews component', () => {
     expect(regenerate).toBeCalled()
 
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('Should send an API Request', async () => {
+    const dispatch = jest.fn()
+    const regenerateFn = jest.fn()
+    const getInjectable = jest.fn(
+      () => ({ ...defaultSettings, regeneratePreviews: regenerateFn } as DocumentViewerSettings),
+    )
+    const getState = jest.fn(() => ({
+      sensenetDocumentViewer: { documentState: defaultState },
+    }))
+    const result = regeneratePreviews()
+    expect(result.type).toEqual('SN_DOCVIEWER_REGENERATE_PREVIEWS_INJECTABLE_ACTION')
+    await result.inject({
+      dispatch,
+      getInjectable: getInjectable as any,
+      getState: getState as any,
+    })
+    expect(regenerateFn).toBeCalled()
   })
 })
