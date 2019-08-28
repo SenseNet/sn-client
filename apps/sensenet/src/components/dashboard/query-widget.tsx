340fcd87f1a3a1c9fdba9d3eb200e1002c1de027
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-08-28 15:28:56 +0200

    refactor(snapp): query-widget to use useQuery

--- apps/sensenet/src/components/dashboard/query-widget.tsx
+++ apps/sensenet/src/components/dashboard/query-widget.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { IconButton, Tooltip, Typography } from '@material-ui/core'
 import Refresh from '@material-ui/icons/RefreshTwoTone'
 import OpenInNewTwoTone from '@material-ui/icons/OpenInNewTwoTone'
 import { GenericContent } from '@sensenet/default-content-types'
-import { ConstantContent, ODataParams } from '@sensenet/client-core'
+import { ConstantContent } from '@sensenet/client-core'
 import { RouteComponentProps, withRouter } from 'react-router'
 import { QueryWidget as QueryWidgetModel } from '../../services/PersonalSettings'
 import { useContentRouting, useLocalization, useRepository, useSelectionService, useStringReplace } from '../../hooks'
-import { CollectionComponent, isReferenceField } from '../content-list'
+import { CollectionComponent } from '../content-list'
 import {
   CurrentAncestorsContext,
   CurrentChildrenContext,
@@ -15,54 +15,15 @@ import {
   LoadSettingsContext,
 } from '../../context'
 import { encodeQueryData } from '../search'
+import { useQuery } from '../../hooks/use-query'
 
 const QueryWidget: React.FunctionComponent<QueryWidgetModel<GenericContent> & RouteComponentProps> = props => {
-  const [items, setItems] = useState<GenericContent[]>([])
-  const [loadChildrenSettings, setLoadChildrenSettings] = useState<ODataParams<GenericContent>>({})
-  const [error, setError] = useState('')
-  const [refreshToken, setRefreshToken] = useState(Math.random())
-  const [count, setCount] = useState(0)
+  const { error, loadChildrenSettings, setLoadChildrenSettings, count, items, refresh } = useQuery(props.settings)
   const repo = useRepository()
   const contentRouter = useContentRouting()
   const replacedTitle = useStringReplace(props.title)
   const localization = useLocalization().dashboard
   const selectionService = useSelectionService()
-
-  useEffect(() => {
-    setLoadChildrenSettings({
-      query: props.settings.query,
-      top: props.settings.countOnly ? 1 : props.settings.top,
-      inlinecount: 'allpages',
-      select: ['Actions', ...props.settings.columns],
-      expand: ['Actions', ...props.settings.columns.filter(f => isReferenceField(f, repo))],
-    })
-  }, [props.settings.columns, props.settings.countOnly, props.settings.query, props.settings.top, repo])
-
-  useEffect(() => {
-    const ac = new AbortController()
-    if (loadChildrenSettings.query) {
-      ;(async () => {
-        /** */
-        try {
-          setError('')
-          const result = await repo.loadCollection({
-            path: ConstantContent.PORTAL_ROOT.Path,
-            oDataOptions: loadChildrenSettings,
-            requestInit: {
-              signal: ac.signal,
-            },
-          })
-          setCount(result.d.__count)
-          setItems(result.d.results)
-        } catch (e) {
-          if (!ac.signal.aborted) {
-            setError(e.toString())
-          }
-        }
-      })()
-      return () => ac.abort()
-    }
-  }, [repo, loadChildrenSettings, refreshToken])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
@@ -82,7 +43,7 @@ const QueryWidget: React.FunctionComponent<QueryWidgetModel<GenericContent> & Ro
         <div style={{ flex: 1 }} />
         {props.settings.showRefresh ? (
           <Tooltip title={localization.refresh}>
-            <IconButton onClick={() => setRefreshToken(Math.random())} style={{ padding: '0', margin: '0 0 0 1em' }}>
+            <IconButton onClick={refresh} style={{ padding: '0', margin: '0 0 0 1em' }}>
               <Refresh />
             </IconButton>
           </Tooltip>
