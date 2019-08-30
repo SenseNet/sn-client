682a683648f2eefcfea3862fe2563332b53df2a9
Gallay <gallay.lajos@gmail.com>
2019-08-30 16:49:32 +0200

    test(hooks):Session context

--- packages/sn-hooks-react/test/session.test.tsx
+++ packages/sn-hooks-react/test/session.test.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import { SessionContextProvider } from '../src/context/session'
+import { useRepository } from '../src/hooks'
 
 describe('Session', () => {
   it('matches snapshot', () => {
     const p = shallow(<SessionContextProvider />)
     expect(p).toMatchSnapshot()
+  })
+
+  it('Should update the current user state on repository auth.state change', () => {
+    const MockElement: React.FC = () => {
+      const repo = useRepository()
+      repo.authentication.currentUser.setValue({
+        Type: 'User',
+        Id: 12345,
+        Name: 'DevDog',
+        Path: 'Root/IMS/DevDog',
+      })
+      return <div />
+    }
+
+    const m = mount(
+      <SessionContextProvider>
+        <MockElement />
+      </SessionContextProvider>,
+    )
+
+    m.unmount()
   })
 })
