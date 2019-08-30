3e4b13b05a36cf1fb40b74b11d66b26376ded3df
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-08-30 22:37:01 +0200

    refactor(Trash): use simple list

--- apps/sensenet/src/components/Trash.tsx
+++ apps/sensenet/src/components/Trash.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react'
 import { createStyles, Grid, IconButton, makeStyles, Theme, Typography } from '@material-ui/core'
 import { GenericContent, TrashBin } from '@sensenet/default-content-types'
-import { RouteComponentProps, withRouter } from 'react-router'
-import { ConstantContent } from '@sensenet/client-core'
 import { Settings } from '@material-ui/icons'
-import { useQuery } from '../hooks/use-query'
-import { CurrentAncestorsContext, CurrentChildrenContext, CurrentContentContext, LoadSettingsContext } from '../context'
-import { useContentRouting, useLocalization, useSelectionService } from '../hooks'
+import { useLocalization } from '../hooks'
 import { useLoadContent } from '../hooks/use-loadContent'
-import { CollectionComponent } from './content-list'
 import { EditPropertiesDialog } from './dialogs'
+import { SimpleList } from './content/Simple'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,14 +17,8 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const columns: Array<keyof GenericContent> = ['DisplayName', 'ModificationDate', 'ModifiedBy']
 
-const Trash: React.FC<RouteComponentProps> = props => {
-  const { loadChildrenSettings, setLoadChildrenSettings, items, error } = useQuery({
-    columns,
-    query: '+TypeIs: TrashBag',
-  })
+const Trash: React.FC = () => {
   const [isEditPropertiesOpened, setIsEditPropertiesOpened] = useState(false)
-  const contentRouter = useContentRouting()
-  const selectionService = useSelectionService()
   const localization = useLocalization().trash
   const trash = useLoadContent<TrashBin>({ idOrPath: '/Root/Trash', oDataOptions: { select: 'all' } })
   const classes = useStyles()
@@ -58,54 +48,10 @@ const Trash: React.FC<RouteComponentProps> = props => {
             ))
           : null}
       </Grid>
-      <CurrentContentContext.Provider value={ConstantContent.PORTAL_ROOT}>
-        <CurrentChildrenContext.Provider value={items}>
-          <CurrentAncestorsContext.Provider value={[]}>
-            <LoadSettingsContext.Provider
-              value={{
-                loadAncestorsSettings: {},
-                loadSettings: {},
-                loadChildrenSettings,
-                setLoadChildrenSettings: newSettings => {
-                  setLoadChildrenSettings({
-                    ...loadChildrenSettings,
-                    orderby: newSettings.orderby,
-                  })
-                },
-                setLoadSettings: () => ({}),
-                setLoadAncestorsSettings: () => ({}),
-              }}>
-              <CollectionComponent
-                disableSelection={false}
-                hideHeader={false}
-                fieldsToDisplay={columns}
-                style={{
-                  overflow: 'auto',
-                  height: items.length < 1 ? 0 : '100%',
-                }}
-                enableBreadcrumbs={false}
-                parentIdOrPath={0}
-                onParentChange={() => {
-                  // props.history.push(contentRouter.getPrimaryActionUrl(p))
-                }}
-                onActivateItem={p => {
-                  props.history.push(contentRouter.getPrimaryActionUrl(p))
-                }}
-                onTabRequest={() => {
-                  /** */
-                }}
-                onSelectionChange={sel => {
-                  selectionService.selection.setValue(sel)
-                }}
-                onActiveItemChange={item => {
-                  selectionService.activeContent.setValue(item)
-                }}
-              />
-              {error ? <Typography color="error">{error}</Typography> : null}
-            </LoadSettingsContext.Provider>
-          </CurrentAncestorsContext.Provider>
-        </CurrentChildrenContext.Provider>
-      </CurrentContentContext.Provider>
+      <SimpleList
+        parent="/Root/Trash"
+        collectionComponentProps={{ enableBreadcrumbs: false, fieldsToDisplay: columns }}
+      />
       {trash.content ? (
         <EditPropertiesDialog
           content={trash.content}
@@ -120,4 +66,4 @@ const Trash: React.FC<RouteComponentProps> = props => {
   )
 }
 
-export default withRouter(Trash)
+export default Trash
