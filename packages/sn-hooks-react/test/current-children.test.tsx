303b7eb8cf00ef8586f208f90ba1c26df6830474
Gallay Lajos <gallay.lajos@gmail.com>
2019-09-02 14:05:13 +0200

    test(hooks):current content & children

--- packages/sn-hooks-react/test/current-children.test.tsx
+++ packages/sn-hooks-react/test/current-children.test.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
+import { act } from 'react-dom/test-utils'
+import { Repository } from '@sensenet/client-core'
 import { CurrentChildrenProvider } from '../src/context/current-children'
+import { RepositoryContext } from '../src/context'
 
 describe('CurrentChildren', () => {
   it('matches snapshot', () => {
     const p = shallow(<CurrentChildrenProvider />)
     expect(p).toMatchSnapshot()
+  })
+
+  it('Can be mounted', async () => {
+    await act(async () => {
+      const repo = new Repository(
+        {},
+        async () =>
+          ({
+            ok: true,
+            json: async () => ({
+              d: {
+                results: [{ Id: 1 }],
+              },
+            }),
+          } as any),
+      )
+      mount(
+        <RepositoryContext.Provider value={repo}>
+          <CurrentChildrenProvider />
+        </RepositoryContext.Provider>,
+      )
+    })
   })
 })
