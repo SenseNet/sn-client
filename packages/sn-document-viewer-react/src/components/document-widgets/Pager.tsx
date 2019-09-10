3eaac969c9a8cc5e0333913493a8ae6b8e4d793f
Gallay Lajos <gallay.lajos@gmail.com>
2019-09-10 10:48:41 +0200

    refactor(viewer):pager

--- packages/sn-document-viewer-react/src/components/document-widgets/Pager.tsx
+++ packages/sn-document-viewer-react/src/components/document-widgets/Pager.tsx
@@ -4,34 +4,9 @@ import FirstPage from '@material-ui/icons/FirstPage'
 import LastPage from '@material-ui/icons/LastPage'
 import NavigateBefore from '@material-ui/icons/NavigateBefore'
 import NavigateNext from '@material-ui/icons/NavigateNext'
-import React from 'react'
-import { connect } from 'react-redux'
-import { debounce } from '@sensenet/client-utils'
-import { RootReducerType, setActivePages } from '../../store'
-
-/**
- * maps state fields from the store to component props
- * @param state the redux state
- */
-export const mapStateToProps = (state: RootReducerType) => {
-  return {
-    activePages: state.sensenetDocumentViewer.viewer.activePages,
-    pageCount: state.sensenetDocumentViewer.documentState.document.pageCount,
-    firstPage: state.sensenetDocumentViewer.localization.firstPage,
-    previousPage: state.sensenetDocumentViewer.localization.previousPage,
-    gotoPage: state.sensenetDocumentViewer.localization.gotoPage,
-    nextPage: state.sensenetDocumentViewer.localization.nextPage,
-    lastPage: state.sensenetDocumentViewer.localization.lastPage,
-  }
-}
-
-/**
- * maps state actions from the store to component props
- * @param state the redux state
- */
-export const mapDispatchToProps = {
-  setActivePages,
-}
+import React, { useCallback, useEffect, useState } from 'react'
+import { useViewerState } from '../../hooks/useViewerState'
+import { useDocumentData, useLocalization } from '../../hooks'
 
 /**
  * Defines the own props for the PagerState component
@@ -41,103 +16,72 @@ export interface PagerState {
   lastPage: number
 }
 
-/**
- * Document widget component for paging
- */
-export class PagerComponent extends React.Component<
-  ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps,
-  PagerState
-> {
-  /** the component state */
-  public state = { currentPage: this.props.activePages[0], lastPage: this.props.pageCount }
+export const PagerWidget: React.FC = () => {
+  const viewerState = useViewerState()
+  const localization = useLocalization()
+  const document = useDocumentData()
 
-  private setPage = debounce(() => {
-    this.props.setActivePages([this.state.currentPage])
-  }, 200).bind(this)
+  const [currentPage, setCurrentPage] = useState(viewerState.activePages[0] || 1)
+  const [lastPage, setLastPage] = useState(document.pageCount || 1)
 
-  /** creates a derived state from props */
-  public static getDerivedStateFromProps(
-    nextProps: PagerComponent['props'],
-    lastState: Partial<PagerComponent['state']>,
-  ) {
-    return {
-      ...lastState,
-      currentPage: lastState.currentPage || nextProps.activePages[0],
-      lastPage: nextProps.pageCount,
-    }
-  }
+  useEffect(() => {
+    setCurrentPage(viewerState.activePages[0] || 1)
+  }, [viewerState.activePages])
 
-  private gotoPage(page: string | number) {
-    let pageInt = typeof page === 'string' ? parseInt(page, 10) : page
-    if (!isNaN(pageInt)) {
-      pageInt = Math.max(pageInt, 1)
-      pageInt = Math.min(pageInt, this.props.pageCount)
-      this.setState({
-        currentPage: pageInt,
-      })
-      this.setPage()
-    }
-  }
+  useEffect(() => {
+    setLastPage(document.pageCount || 1)
+  }, [document.pageCount])
 
-  /**
-   * renders the component
-   */
-  public render() {
-    return (
-      <div style={{ display: 'inline-block' }}>
-        <IconButton
-          disabled={this.state.currentPage <= 1}
-          title={this.props.firstPage}
-          onClick={() => this.gotoPage(1)}
-          id="FirstPage">
-          <FirstPage />
-        </IconButton>
+  const gotoPage = useCallback(
+    (pageNo: number) => {
+      viewerState.updateState({ activePages: [pageNo] })
+    },
+    [viewerState],
+  )
 
-        <IconButton
-          disabled={this.state.currentPage <= 1}
-          title={this.props.previousPage}
-          onClick={() => this.gotoPage(this.props.activePages[0] - 1)}
-          id="NavigateBefore">
-          <NavigateBefore />
-        </IconButton>
+  return (
+    <div style={{ display: 'inline-block' }}>
+      <IconButton disabled={currentPage <= 1} title={localization.firstPage} onClick={() => gotoPage(1)} id="FirstPage">
+        <FirstPage />
+      </IconButton>
 
-        <TextField
-          title={this.props.gotoPage}
-          value={this.state.currentPage}
-          onChange={ev => this.gotoPage(ev.currentTarget.value)}
-          type="number"
-          required={true}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          inputProps={{ min: 1, max: this.state.lastPage }}
-          margin="dense"
-        />
+      <IconButton
+        disabled={currentPage <= 1}
+        title={localization.previousPage}
+        onClick={() => gotoPage(viewerState.activePages[0] - 1)}
+        id="NavigateBefore">
+        <NavigateBefore />
+      </IconButton>
 
-        <IconButton
-          disabled={this.state.currentPage >= this.state.lastPage}
-          title={this.props.nextPage}
-          color={'primary'}
-          onClick={() => this.gotoPage(this.props.activePages[0] + 1)}
-          id="NavigateNext">
-          <NavigateNext />
-        </IconButton>
+      <TextField
+        title={localization.gotoPage}
+        value={currentPage}
+        onChange={ev => gotoPage(parseInt(ev.currentTarget.value, 10))}
+        type="number"
+        required={true}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{ min: 1, max: lastPage }}
+        margin="dense"
+      />
 
-        <IconButton
-          disabled={this.state.currentPage >= this.state.lastPage}
-          title={this.props.lastPage}
-          onClick={() => this.gotoPage(this.state.lastPage)}
-          id="LastPage">
-          <LastPage />
-        </IconButton>
-      </div>
-    )
-  }
+      <IconButton
+        disabled={currentPage >= lastPage}
+        title={localization.nextPage}
+        color={'primary'}
+        onClick={() => gotoPage(viewerState.activePages[0] + 1)}
+        id="NavigateNext">
+        <NavigateNext />
+      </IconButton>
+
+      <IconButton
+        disabled={currentPage >= lastPage}
+        title={localization.lastPage}
+        onClick={() => gotoPage(lastPage)}
+        id="LastPage">
+        <LastPage />
+      </IconButton>
+    </div>
+  )
 }
-
-const connectedComponent = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(PagerComponent)
-
-export { connectedComponent as PagerWidget }
