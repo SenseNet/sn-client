ba353589edd3f7c8ed5db6117c9f436f6792cd64
Gallay <gallay.lajos@gmail.com>
2019-09-13 14:54:49 +0200

    test(viewer):Refactored CreateComment tests

--- packages/sn-document-viewer-react/test/CreateComment.test.tsx
+++ packages/sn-document-viewer-react/test/CreateComment.test.tsx
@@ -1,9 +1,9 @@
 import Button from '@material-ui/core/Button'
 import FormHelperText from '@material-ui/core/FormHelperText'
-import IconButton from '@material-ui/core/IconButton'
 import { mount, shallow } from 'enzyme'
 import React from 'react'
 import { CreateComment, CreateCommentProps } from '../src/components'
+import { CommentStateContext, CommentStateProvider } from '../src/context/comment-states'
 
 describe('Create comment component', () => {
   const defaultProps: CreateCommentProps = {
@@ -17,11 +17,8 @@ describe('Create comment component', () => {
       markerTooltip: 'markerTooltip',
       cancelButton: 'cancel',
     },
-    handlePlaceMarkerClick: jest.fn(),
-    isPlacingMarker: false,
     isActive: true,
     handleIsActive: jest.fn(),
-    draftCommentMarker: { x: 10, y: 10, id: 'id' },
     handleInputValueChange: jest.fn(),
     inputValue: '',
   }
@@ -83,13 +80,6 @@ describe('Create comment component', () => {
     expect(wrapper.prop('isPlacingMarker')).toBeFalsy()
   })
 
-  it('should handle marker placement', () => {
-    const handlePlaceMarkerClick = jest.fn()
-    const wrapper = mount(<CreateComment {...defaultProps} handlePlaceMarkerClick={handlePlaceMarkerClick} />)
-    wrapper.find(IconButton).simulate('click')
-    expect(handlePlaceMarkerClick).toBeCalled()
-  })
-
   it('should handle input change', () => {
     const wrapper = mount(<CreateComment {...defaultProps} />)
     wrapper
@@ -101,13 +91,22 @@ describe('Create comment component', () => {
   })
 
   it('should give an error message when input is empty', () => {
-    const wrapper = mount(<CreateComment {...defaultProps} draftCommentMarker={undefined} />)
+    const wrapper = mount(
+      <CommentStateProvider>
+        <CreateComment {...defaultProps} />
+      </CommentStateProvider>,
+    )
     wrapper.find('form').simulate('submit')
     expect(wrapper.find(FormHelperText).text()).toEqual(defaultProps.localization!.inputRequiredError)
   })
 
   it('should give an error message when input is filled but draftCommentMarker is undefined', () => {
-    const wrapper = mount(<CreateComment {...defaultProps} draftCommentMarker={undefined} inputValue="Hello" />)
+    const wrapper = mount(
+      <CommentStateContext.Provider value={{ setActiveComment: () => undefined, setDraft: () => undefined }}>
+        <CreateComment {...defaultProps} />
+      </CommentStateContext.Provider>,
+    )
+    wrapper.find('input').simulate('change', { target: { value: 'Hello There' } })
     wrapper.find('form').simulate('submit')
     expect(wrapper.find(FormHelperText).text()).toEqual(defaultProps.localization!.markerRequiredError)
   })
