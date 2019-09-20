b4dc697a7836f6563aef40ae518e875a88e4fc73
Gallay Lajos <gallay.lajos@gmail.com>
2019-09-20 16:57:28 +0200

    docs(utils):fixed code doc for AsyncReturnValue

--- packages/sn-client-utils/src/AsyncReturnValue.ts
+++ packages/sn-client-utils/src/AsyncReturnValue.ts
@@ -1,5 +1,5 @@
 /**
  * Type helper that extracts the return value from an async method
- * E.g. PromiseReturnValue<Promise<T>> will be evaluated as T
+ * E.g. AsyncReturnValue<()=>Promise<T>> will be evaluated as T
  */
 export type AsyncReturnValue<T> = T extends (...args: any[]) => PromiseLike<infer U> ? U : never
