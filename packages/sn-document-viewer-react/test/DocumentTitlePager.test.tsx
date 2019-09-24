c959efd6b6e2527f071deb309e5bc66ebc4aafa0
Gallay Lajos <gallay.lajos@gmail.com>
2019-09-24 10:50:37 +0200

    test(viewer): DocumentTitlePager test fixes

--- packages/sn-document-viewer-react/test/DocumentTitlePager.test.tsx
+++ packages/sn-document-viewer-react/test/DocumentTitlePager.test.tsx
@@ -1,54 +1,39 @@
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import { sleepAsync } from '@sensenet/client-utils'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import React from 'react'
-import { DocumentTitlePagerComponent } from '../src/components/document-widgets/DocumentTitlePager'
+import { DocumentTitlePager } from '../src/components/document-widgets/DocumentTitlePager'
+import { defaultViewerState, ViewerStateContext } from '../src/context/viewer-state'
 
 describe('DocumentTitlePager component', () => {
   it('Should render the unfocused state without crashing', () => {
-    const wrapper = shallow(
-      <DocumentTitlePagerComponent
-        activePages={[1]}
-        documentName="TestDocument"
-        gotoPage="goto"
-        pageCount={10}
-        setActivePages={jest.fn()}
-      />,
-    )
+    const wrapper = shallow(<DocumentTitlePager />)
     expect(wrapper).toMatchSnapshot()
   })
 
   it('Should render the focused state without crashing', async () => {
-    const wrapper = shallow(
-      <DocumentTitlePagerComponent
-        activePages={[1]}
-        documentName="TestDocument"
-        gotoPage="goto"
-        pageCount={10}
-        setActivePages={jest.fn()}
-      />,
-    )
+    const wrapper = shallow(<DocumentTitlePager />)
     wrapper.find(Typography).simulate('click')
     expect(wrapper.find(TextField)).toBeTruthy()
     expect(wrapper).toMatchSnapshot()
   })
 
   it('Should set the default value of the textfield when changed', async () => {
-    const activePages = jest.fn()
-    const wrapper = shallow(
-      <DocumentTitlePagerComponent
-        activePages={[1]}
-        documentName="TestDocument"
-        gotoPage="goto"
-        pageCount={10}
-        setActivePages={activePages}
-      />,
+    const updateState = jest.fn()
+    const wrapper = mount(
+      <ViewerStateContext.Provider
+        value={{
+          ...defaultViewerState,
+          updateState,
+        }}>
+        <DocumentTitlePager />
+      </ViewerStateContext.Provider>,
     )
     wrapper.find(Typography).simulate('click')
     const onChange = wrapper.find(TextField).prop('onChange')
     onChange && onChange({ currentTarget: { value: '3' } } as any)
     await sleepAsync()
-    expect(activePages).toBeCalled()
+    expect(updateState).toBeCalledWith([3])
   })
 })
