a3f601d15fe0a1b1fa29afa6b4fde243fbc7571d
Gallay Lajos <gallay.lajos@gmail.com>
2019-09-24 13:06:32 +0200

    refactor(viewer): extracted comment State type & defaults

--- packages/sn-document-viewer-react/src/context/comment-states.tsx
+++ packages/sn-document-viewer-react/src/context/comment-states.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
 import { DraftCommentMarker } from '../models'
 
-export const CommentStateContext = React.createContext<{
+export interface CommentState {
   draft?: DraftCommentMarker
   setDraft: (d?: DraftCommentMarker) => void
   activeCommentId?: string
   setActiveComment: (commentId?: string) => void
-}>({
+}
+
+export const defaultCommentState: CommentState = {
   setDraft: () => undefined,
   activeCommentId: undefined,
   setActiveComment: () => undefined,
-})
+}
+
+export const CommentStateContext = React.createContext<CommentState>(defaultCommentState)
 
 export const CommentStateProvider: React.FC = ({ children }) => {
   const [draft, setDraft] = useState<DraftCommentMarker | undefined>(undefined)
