1b6fc380ffa0a9776a61f2407b13b976f9116dbf
Gallay Lajos <gallay.lajos@gmail.com>
2019-09-24 09:32:15 +0200

    chore(utils):removed PromiseReturnValue type

--- packages/sn-client-utils/src/PromiseReturnValue.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Type helper that extracts the return value from a PromiseLike type
- * E.g. PromiseReturnValue<Promise<T>> will be evaluated as T
- */
-export type PromiseReturnValue<T> = T extends PromiseLike<infer U> ? U : never
