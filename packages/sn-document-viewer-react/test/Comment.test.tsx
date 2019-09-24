e6fa116557f7d4668eb0a2e9758bd79472b4acc7
Gallay Lajos <gallay.lajos@gmail.com>
2019-09-24 10:36:03 +0200

    test(viewer):refactored comments tests

--- packages/sn-document-viewer-react/test/Comment.test.tsx
+++ packages/sn-document-viewer-react/test/Comment.test.tsx
@@ -4,7 +4,10 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import { mount } from 'enzyme'
 import React from 'react'
+import { RepositoryContext } from '@sensenet/hooks-react'
+import { Repository } from '@sensenet/client-core'
 import { Comment, CommentProps } from '../src/components/comment'
+import { CommentStateProvider } from '../src/context/comment-states'
 import { examplePreviewComment } from './__Mocks__/viewercontext'
 
 const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -13,10 +16,6 @@ const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
 
 const defaultProps: CommentProps = {
   ...examplePreviewComment,
-  deleteComment: jest.fn(),
-  setSelectedCommentId: jest.fn(),
-  selectedCommentId: '',
-  localization: {} as any,
   comment: examplePreviewComment,
 }
 
@@ -32,7 +31,7 @@ describe('Comment component', () => {
     const button = wrapper.find(Button)
     expect(button.exists()).toBeTruthy()
     expect(button.length).toBe(1) // Should not show delete button
-    expect(button.text()).toBe('Show more')
+    expect(button.text()).toBe('+ Show more')
   })
 
   it('should show show less button when too long text is opened', () => {
@@ -40,28 +39,34 @@ describe('Comment component', () => {
     const button = wrapper.find(Button).first()
     button.simulate('click')
     expect(wrapper.find(Button).length).toBe(2) // Should show delete button as well
-    expect(button.text()).toBe('Show less')
+    expect(button.text()).toBe('+ Show less')
   })
   it('should show delete button when text is short', () => {
     const wrapper = mount(<Comment {...defaultProps} />)
-    expect(wrapper.find(Button).text()).toBe('delete')
+    expect(wrapper.find(Button).text()).toBe('Delete')
   })
 
   it('should be in selected state when clicked', () => {
-    const wrapper = mount(<Comment {...defaultProps} />)
+    const wrapper = mount(
+      <CommentStateProvider>
+        <Comment {...defaultProps} />
+      </CommentStateProvider>,
+    )
     wrapper.find(Card).simulate('click')
     expect(wrapper.find(Card).prop('raised')).toBe(true)
   })
 
   it('should display an avatar without src when host is the same', () => {
     const wrapper = mount(
-      <Comment
-        {...defaultProps}
-        comment={{
-          ...defaultProps.comment,
-          createdBy: { ...defaultProps.comment.createdBy, avatarUrl: 'https://cdn.images.express.co.uk' },
-        }}
-      />,
+      <RepositoryContext.Provider value={new Repository({ repositoryUrl: 'https://cdn.images.express.co.uk' })}>
+        <Comment
+          {...defaultProps}
+          comment={{
+            ...defaultProps.comment,
+            createdBy: { ...defaultProps.comment.createdBy, avatarUrl: 'https://cdn.images.express.co.uk' },
+          }}
+        />
+      </RepositoryContext.Provider>,
     )
     expect(wrapper.find(Avatar).prop('src')).toBeUndefined()
   })
