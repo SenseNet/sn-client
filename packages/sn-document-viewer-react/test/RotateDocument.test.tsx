56566f13bf720d4fcadbee4db72fa114fa329cb7
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-10-10 20:51:56 +0200

    test(viewer): rotate document fix

--- packages/sn-document-viewer-react/test/RotateDocument.test.tsx
+++ packages/sn-document-viewer-react/test/RotateDocument.test.tsx
@@ -1,4 +1,4 @@
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import React from 'react'
 import { RotateDocumentWidget } from '../src/components/document-widgets/RotateDocument'
 import { PreviewImageDataContext } from '../src/context/preview-image-data'
@@ -17,23 +17,29 @@ describe('RotateDocument component', () => {
 
   it('RotateLeft should trigger a rotate to left', () => {
     const rotateImages = jest.fn()
-    const wrapper = shallow(
+    const wrapper = mount(
       <PreviewImageDataContext.Provider value={{ imageData: [examplePreviewImageData], rotateImages }}>
         <RotateDocumentWidget />
       </PreviewImageDataContext.Provider>,
     )
-    wrapper.find('#RotateLeft').simulate('click')
+    wrapper
+      .find('#RotateLeft')
+      .first()
+      .simulate('click')
     expect(rotateImages).toBeCalledWith([1], -90)
   })
 
   it('RotateRight should trigger a rotate to right', () => {
     const rotateImages = jest.fn()
-    const wrapper = shallow(
+    const wrapper = mount(
       <PreviewImageDataContext.Provider value={{ imageData: [examplePreviewImageData], rotateImages }}>
         <RotateDocumentWidget />
       </PreviewImageDataContext.Provider>,
     )
-    wrapper.find('#RotateRight').simulate('click')
+    wrapper
+      .find('#RotateRight')
+      .first()
+      .simulate('click')
     expect(rotateImages).toBeCalledWith([1], 90)
   })
 })
