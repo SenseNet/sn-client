88b49f8bf4bb450208b563d0472b19521f0b00ac
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-10-10 20:26:45 +0200

    test(viewer): save document component fix

--- packages/sn-document-viewer-react/test/SaveDocumentComponent.test.tsx
+++ packages/sn-document-viewer-react/test/SaveDocumentComponent.test.tsx
@@ -1,7 +1,10 @@
 import { mount, shallow } from 'enzyme'
 import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
 import { SaveWidget } from '../src/components/document-widgets/SaveWidget'
 import { defaultViewerState, ViewerStateContext } from '../src/context/viewer-state'
+import { DocumentViewerApiSettingsContext } from '../src/context/api-settings'
+import { DocumentPermissionsContext } from '../src/context/document-permissions'
 
 describe('SaveDocumentComponent component', () => {
   it('Should render without crashing when has changes', () => {
@@ -21,11 +24,16 @@ describe('SaveDocumentComponent component', () => {
   it('Should trigger a save request when clicked', () => {
     const save = jest.fn()
     const wrapper = mount(
-      <ViewerStateContext.Provider value={{ ...defaultViewerState, hasChanges: true }}>
-        <SaveWidget />
-      </ViewerStateContext.Provider>,
+      <DocumentPermissionsContext.Provider value={{ canEdit: true } as any}>
+        <DocumentViewerApiSettingsContext.Provider value={{ saveChanges: save } as any}>
+          <ViewerStateContext.Provider value={{ ...defaultViewerState, hasChanges: true }}>
+            <SaveWidget />
+          </ViewerStateContext.Provider>
+        </DocumentViewerApiSettingsContext.Provider>
+      </DocumentPermissionsContext.Provider>,
     )
-    wrapper.find('#Save').simulate('click')
+
+    wrapper.find(IconButton).simulate('click')
     expect(save).toBeCalled()
   })
 })
