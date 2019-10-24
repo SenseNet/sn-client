3ba205e5fca81727b10ce4739c469259c90aeb6a
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-10-24 22:31:46 +0200

    chore(viewer): convert to own jest config

---
+++ packages/sn-document-viewer-react/jest.config.js
@@ -0,0 +1,20 @@
+const pack = require('./package')
+
+module.exports = {
+  preset: 'ts-jest',
+  displayName: pack.name,
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  transform: {
+    '\\.gif$': '<rootDir>/test/fileTransform.js',
+  },
+  coveragePathIgnorePatterns: ['<rootDir>/test/*'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.test.json',
+      diagnostics: {
+        warnOnly: !process.env.CI,
+      },
+    },
+  },
+}
