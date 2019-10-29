942439631ac294a1d756d0d6c9e64b894e38bad2
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-10-29 11:51:32 +0100

    chore: do not request actions multiple times

--- apps/sensenet/src/services/CommandProviders/CustomActionCommandProvider.ts
+++ apps/sensenet/src/services/CommandProviders/CustomActionCommandProvider.ts
@@ -1,4 +1,4 @@
-import { MetadataAction } from '@sensenet/client-core'
+import { MetadataAction, ODataResponse, Repository } from '@sensenet/client-core'
 import { Injectable, ObservableValue } from '@sensenet/client-utils'
 import { ActionModel, GenericContent } from '@sensenet/default-content-types'
 import { CommandProvider, SearchOptions } from '../CommandProviderManager'
@@ -24,6 +24,25 @@ export class CustomActionCommandProvider implements CommandProvider {
       ? true
       : false
   }
+
+  private contentWithActionsAndMetadata: ODataResponse<GenericContent> | undefined
+
+  private async getActions(id: number, repository: Repository) {
+    if (this.contentWithActionsAndMetadata && id === this.contentWithActionsAndMetadata.d.Id) {
+      return this.contentWithActionsAndMetadata
+    }
+    const result = await repository.load<GenericContent>({
+      idOrPath: id,
+      oDataOptions: {
+        metadata: 'full',
+        expand: ['Actions'],
+        select: ['Actions'],
+      },
+    })
+    this.contentWithActionsAndMetadata = result
+    return result
+  }
+
   public async getItems(options: SearchOptions) {
     const content = this.selectionService.activeContent.getValue()
     const localization = this.localization.currentValues.getValue().commandPalette.customAction
@@ -31,17 +50,10 @@ export class CustomActionCommandProvider implements CommandProvider {
     if (!content) {
       return []
     }
-    const result = await options.repository.load<GenericContent>({
-      idOrPath: content.Id,
-      oDataOptions: {
-        metadata: 'full',
-        expand: ['Actions'],
-        select: ['Actions'],
-      },
-    })
-    const actions = (result.d.Actions as ActionModel[]) || []
 
-    return actions
+    const { d: contentWithActions } = await this.getActions(content.Id, options.repository)
+
+    return (contentWithActions.Actions as ActionModel[])
       .filter(
         a =>
           (a.Name.toLowerCase().includes(filteredTerm) && a.IsODataAction) ||
@@ -49,14 +61,14 @@ export class CustomActionCommandProvider implements CommandProvider {
       )
       .map(a => {
         const actionMetadata =
-          result.d.__metadata &&
-          result.d.__metadata.actions &&
-          result.d.__metadata.actions.find(action => action.name === a.Name)
+          contentWithActions.__metadata &&
+          contentWithActions.__metadata.actions &&
+          contentWithActions.__metadata.actions.find(action => action.name === a.Name)
 
         const functionMetadata =
-          result.d.__metadata &&
-          result.d.__metadata.functions &&
-          result.d.__metadata.functions.find(fn => fn.name === a.Name)
+          contentWithActions.__metadata &&
+          contentWithActions.__metadata.functions &&
+          contentWithActions.__metadata.functions.find(fn => fn.name === a.Name)
 
         // merge custom parameters to function metadata
         const customActionMetadata = functionMetadata && {
