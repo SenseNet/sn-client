497aa44eab537aba69134bcecfc4b046225600d7
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-11-11 09:40:01 +0100

    Feature/execute custom action (#442)
    * fix(snapp): console log warning about missing key
    
    * fix(client-utils): getsegments
    
    do not fail on root/content/example(1).pdf
    
    * fix(snapp):  console errors on start and search
    
    * feature(snapp): custom actions for crud
    
    closes #382
    
    * fix(snapp): term can be undefined
    
    * refactor(snapp): do not mutate object
    
    * chore(snapp): execute action clear monaco
    
    * chore: rename delete actions parameter
    
    Co-Authored-By: Aniko Litvanyi <herflis33@gmail.com>
    
    * style: lint fix
    
    * chore: do not request actions multiple times

--- apps/sensenet/src/services/CommandProviders/NavigationCommandProvider.ts
+++ apps/sensenet/src/services/CommandProviders/NavigationCommandProvider.ts
@@ -62,9 +62,9 @@ export class NavigationCommandProvider implements CommandProvider {
   private localizationValues: ReturnType<LocalizationService['currentValues']['getValue']>['navigationCommandProvider']
 
   public shouldExec(options: SearchOptions) {
-    const termLowerCase = options.term.toLocaleLowerCase()
+    const termLowerCase = options.term && options.term.toLocaleLowerCase()
     return (
-      options.term.length > 0 &&
+      options.term != null &&
       this.getRoutes(options).find(
         r =>
           r.primaryText.toLocaleLowerCase().includes(termLowerCase) ||
