e8fd3a7c96c64685dc73b3b7b6db56a5fa8eae70
Aniko Litvanyi <herflis33@gmail.com>
2019-11-15 13:32:26 +0100

    Update packages/sn-controls-react/src/viewcontrols/EditView.tsx
    Co-Authored-By: Zoltán Bedi <zoltan.bedi@gmail.com>

--- packages/sn-controls-react/src/viewcontrols/EditView.tsx
+++ packages/sn-controls-react/src/viewcontrols/EditView.tsx
@@ -38,7 +38,7 @@ export const EditView: React.FC<EditViewProps> = props => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     props.onSubmit?.(props.content, saveableFields as any)
-    props.submitCallback && props.submitCallback()
+    props.submitCallback?.()
   }
 
   const handleInputChange = (field: string, value: unknown) => {
