e6abf728804a58ff96457fbc4d528c6b39b2d5a1
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-11-19 13:02:14 +0100

    refactor(hooks): useWopi to return functions
    useWopi now returns two functions to determine if the content
    is editable with wopi or not. Same for is read available

--- packages/sn-hooks-react/test/use-wopi.test.tsx
+++ packages/sn-hooks-react/test/use-wopi.test.tsx
@@ -1,47 +1,52 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { ActionModel, GenericContent } from '@sensenet/default-content-types'
+import { GenericContent } from '@sensenet/default-content-types'
 import { useWopi } from '../src/hooks'
 
-const WopiDump: React.FC<{ content: GenericContent }> = ({ content }) => {
-  const i = useWopi(content)
-  return <div>{JSON.stringify(i)}</div>
+const Wopi = ({ content }: { content: GenericContent }) => {
+  const { isReadAvailable, isWriteAvailable } = useWopi()
+  return (
+    <>
+      {isReadAvailable(content) ? <p>Read is available</p> : null}
+      {isWriteAvailable(content) ? <p>Write is available</p> : null}
+    </>
+  )
 }
 
-describe('Wopi', () => {
-  it('Should match the snapshot', () => {
-    expect(shallow(<WopiDump content={{ Id: 1, Path: '', Type: 'File', Name: 'aaa.txt' }} />)).toMatchSnapshot()
+describe('useWopi hook', () => {
+  it('should allow edit based on available action', () => {
+    const content = {
+      Type: 'File',
+      Actions: [{ Name: 'WopiOpenEdit' }],
+    }
+    const wrapper = shallow(<Wopi content={content as any} />)
+    expect(wrapper.find('p').length).toBe(2)
   })
 
-  it('Should allow edit based on available action', () => {
-    expect(
-      shallow(
-        <WopiDump
-          content={{
-            Id: 1,
-            Path: '',
-            Type: 'File',
-            Name: 'aaa.txt',
-            Actions: [{ Name: 'WopiOpenEdit' } as ActionModel],
-          }}
-        />,
-      ),
-    ).toMatchSnapshot()
+  it('should allow view based on available action', () => {
+    const content = {
+      Type: 'File',
+      Actions: [{ Name: 'WopiOpenView' }],
+    }
+    const wrapper = shallow(<Wopi content={content as any} />)
+    expect(wrapper.find('p').length).toBe(1)
   })
 
-  it('Should allow view based on available action', () => {
-    expect(
-      shallow(
-        <WopiDump
-          content={{
-            Id: 1,
-            Path: '',
-            Type: 'File',
-            Name: 'aaa.txt',
-            Actions: [{ Name: 'WopiOpenView' } as ActionModel],
-          }}
-        />,
-      ),
-    ).toMatchSnapshot()
+  it('should not show any wopiaction based on available action', () => {
+    const content = {
+      Type: 'File',
+      Actions: [{ Name: 'Browse' }],
+    }
+    const wrapper = shallow(<Wopi content={content as any} />)
+    expect(wrapper.find('p').length).toBe(0)
+  })
+
+  it('should not show any wopiaction when content type is not file', () => {
+    const content = {
+      Type: 'Task',
+      Actions: [{ Name: 'Browse' }],
+    }
+    const wrapper = shallow(<Wopi content={content as any} />)
+    expect(wrapper.find('p').length).toBe(0)
   })
 })
