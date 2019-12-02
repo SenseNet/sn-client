9042180eabce9da674b0e625c8e385593ac55122
Enikő Pusztai <eniko.pusztai@sensenet.com>
2019-12-02 15:19:45 +0100

    src and test folders are updated

--- examples/sn-react-imageupload/src/app.tsx
+++ examples/sn-react-imageupload/src/app.tsx
@@ -1,21 +1,90 @@
-import React from 'react'
-import { Button, CssBaseline, Tooltip, Typography } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { ConstantContent } from '@sensenet/client-core'
+import { Image } from '@sensenet/default-content-types'
+import { CssBaseline, IconButton, Snackbar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import CloseIcon from '@material-ui/icons/Close'
+import { useRepository } from '@sensenet/hooks-react'
 import snLogo from './assets/sensenet_logo_transparent.png'
-import { useCurrentUser } from './hooks/use-current-user'
-import { useRepository } from './hooks/use-repository'
+import { AdvancedGridList } from './components/AdvancedGridList'
+import { SimpleAppBar } from './components/SimpleAppBar'
+
+export const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'unset',
+    overflow: 'visible',
+    backgroundColor: theme.palette.background.paper,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  close: {
+    padding: theme.spacing(0.5),
+  },
+}))
 
 /**
  * The main entry point of your app. You can start h@cking from here ;)
  */
 export const App: React.FunctionComponent = () => {
-  const usr = useCurrentUser()
   const repo = useRepository()
+  const [data, setData] = useState<Image[]>([])
+  const classes = useStyles()
+  const [isNotificationShown, ShowNotification] = React.useState<boolean>(false)
+  const [reloadToken, setReloadToken] = useState(1)
+  /** Display the notificationbar about successful upload
+   * @param {boolean} switcher Should the notification shown or not
+   */
+  function DisplayNotification(switcher: boolean) {
+    ShowNotification(switcher)
+  }
+  /**
+   * Hide the notificationbar
+   */
+  function CloseNotfication() {
+    ShowNotification(false)
+  }
+  /**
+   * Hide the notificationbar
+   */
+  function setUploaddataFunction() {
+    setReloadToken(Math.random())
+  }
+
+  useEffect(() => {
+    /**
+     * Fetches the images from the repository.
+     */
+    async function loadImages(): Promise<void> {
+      const result = await repo.loadCollection<Image>({
+        path: `${ConstantContent.PORTAL_ROOT.Path}/Content/IT/ImageLibrary`,
+        oDataOptions: {
+          select: [
+            'Binary',
+            'DisplayName',
+            'Description',
+            'CreationDate',
+            'CreatedBy',
+            'Height',
+            'ModificationDate',
+            'Size',
+            'Width',
+          ],
+          expand: ['CreatedBy'],
+        },
+      })
+      setData(result.d.results)
+    }
+    loadImages()
+  }, [repo, reloadToken])
   return (
     <div
       style={{
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center',
         width: '100%',
         height: '100%',
         flexDirection: 'column',
@@ -24,21 +93,36 @@ export const App: React.FunctionComponent = () => {
         backgroundImage: `url(${snLogo})`,
         backgroundSize: 'auto',
       }}>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        open={isNotificationShown}
+        autoHideDuration={6000}
+        onClose={CloseNotfication}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">Successful Upload</span>}
+        action={[
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            className={classes.close}
+            onClick={CloseNotfication}>
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
       <CssBaseline />
-      <Typography variant="h3" gutterBottom>
-        Hello, {usr.Name}{' '}
-        <span role="img" aria-label="sunglass">
-          😎
-        </span>
-      </Typography>
-      <Tooltip title="Return to the Login screen and select another repository">
-        <Button variant="outlined" color="primary" onClick={() => repo.authentication.logout()}>
-          Log out
-          <span role="img" aria-label="sunglass">
-            🚪
-          </span>
-        </Button>
-      </Tooltip>
+      <SimpleAppBar uploadsetdata={setUploaddataFunction} notificationControll={DisplayNotification} />
+      <AdvancedGridList
+        imgList={data}
+        uploadsetdata={setUploaddataFunction}
+        notificationControll={DisplayNotification}
+      />
     </div>
   )
 }
---
+++ examples/sn-react-imageupload/src/components/AdvancedGridList.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import { Image, User } from '@sensenet/default-content-types'
+import { IconButton } from '@material-ui/core'
+import GridList from '@material-ui/core/GridList'
+import { makeStyles } from '@material-ui/core/styles'
+import GridListTile from '@material-ui/core/GridListTile'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+import { ConstantContent } from '@sensenet/client-core'
+import { useRepository } from '@sensenet/hooks-react'
+import { DropFileArea } from './DropFileArea'
+
+interface AdvancedGridprops {
+  imgList: Image[]
+  uploadsetdata: () => void
+  notificationControll: (IsOpen: boolean) => void
+}
+
+export const useStyles = makeStyles(theme => ({
+  root: {
+    width: '75%',
+    justifyContent: 'space-around',
+    overflow: 'visible',
+    backgroundColor: theme.palette.background.paper,
+    marginTop: '100px',
+  },
+  gridList: {
+    height: 'auto',
+    transform: 'translateZ(0)',
+  },
+  imgTile: {
+    cursor: 'default',
+  },
+  icon: {
+    color: 'white',
+  },
+  titleBar: {
+    background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+}))
+/**
+ * Determines which image should be full width
+ * @param {number} anumber Seletected number's index.
+ * @returns {int} 2 for full width, 1 for half size.
+ */
+export function pickTile(anumber: number) {
+  let tilenumber = anumber % 3
+  tilenumber = tilenumber === 0 ? 2 : 1
+  return tilenumber
+}
+/**
+ * Display Images from repository
+ */
+export const AdvancedGridList: React.FunctionComponent<AdvancedGridprops> = props => {
+  const [isDragOver, setDragOver] = useState(false)
+  const classes = useStyles()
+  const repo = useRepository()
+  /**
+   * Sets the isDragOver state true or false
+   * @param {boolean} isOpen Seletected number's index.
+   */
+  function DragSetter(isOpen: boolean) {
+    setDragOver(isOpen)
+  }
+
+  return (
+    <div className={classes.root}>
+      <DropFileArea
+        uploadPath={`${ConstantContent.PORTAL_ROOT.Path}/Content/IT/ImageLibrary`}
+        uploadsetdata={props.uploadsetdata}
+        notificationControll={props.notificationControll}
+        setDragOver={DragSetter}
+        isDragOver={isDragOver}>
+        <GridList cellHeight={200} spacing={1} style={{ opacity: isDragOver ? 0.5 : 1 }} className={classes.gridList}>
+          {props.imgList.map((tile, index) => (
+            <GridListTile key={tile.Id} cols={pickTile(index)} rows={pickTile(index)}>
+              <img
+                className={classes.imgTile}
+                src={repo.configuration.repositoryUrl + tile.Path}
+                alt={tile.Description}
+              />
+              <GridListTileBar
+                title={tile.DisplayName}
+                titlePosition="bottom"
+                subtitle={<span>by: {(tile.CreatedBy as User).FullName}</span>}
+                actionIcon={<IconButton aria-label={`star ${tile.DisplayName}`} className={classes.icon} />}
+                actionPosition="left"
+                className={classes.titleBar}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </DropFileArea>
+    </div>
+  )
+}
