f757d12830d956d48e7853edec7f9da7fffb2ca7
eniko.pusztai <eniko.pusztai@sensenet.com>
2019-12-02 16:22:07 +0100

    Update examples/sn-react-memoapp/test/full-screen-loader.test.tsx
    Co-Authored-By: Aniko Litvanyi <herflis33@gmail.com>

--- examples/sn-react-memoapp/test/full-screen-loader.test.tsx
+++ examples/sn-react-memoapp/test/full-screen-loader.test.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { FullScreenLoader } from '../src/components/full-screen-loader'
 
 describe('The full screen loader instance', () => {
-  it('should renders correctly', () => {
+  it('should be rendered correctly', () => {
     expect(shallow(<FullScreenLoader />)).toMatchSnapshot()
   })
 })
