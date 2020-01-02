df5e6223093eee5d8f882cd9dca53dbb84c3fdbd
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-01-02 15:56:46 +0100

    style(logger): remove empty newlines

--- packages/sn-client-utils/src/logger/console-logger.ts
+++ packages/sn-client-utils/src/logger/console-logger.ts
@@ -1,112 +1,73 @@
-import { Injectable } from '../index'
-
-import { AbstractLogger } from './abstract-logger'
-
-import { LeveledLogEntry, LogLevel } from './log-entries'
+import { AbstractLogger, LeveledLogEntry, LogLevel } from '.'
+import { Injectable } from '..'
 
 /**
-
  * Resets the console color
-
  */
-
 export const Reset = '\x1b[0m'
 
 /**
-
  * Black console foreground color
-
  */
 
 export const FgBlack = '\x1b[30m'
 
 /**
-
  * Red console foreground color
-
  */
-
 export const FgRed = '\x1b[31m'
 
 /**
-
  * Green console foreground color
-
  */
-
 export const FgGreen = '\x1b[32m'
 
 /**
-
  * Yellow console foreground color
-
  */
-
 export const FgYellow = '\x1b[33m'
 
 /**
-
  * Blue console foreground color
-
  */
-
 export const FgBlue = '\x1b[34m'
 
 /**
-
  * Magentaa console foreground color
-
  */
-
 export const FgMagenta = '\x1b[35m'
 
 /**
-
  * Cyan console foreground color
-
  */
-
 export const FgCyan = '\x1b[36m'
 
 /**
-
  * White console foreground color
-
  */
-
 export const FgWhite = '\x1b[37m'
 
 /**
-
  * Returns an associated color to a specific log level
-
- * @param level the log level
-
  */
-
 export const getLevelColor = (level: LogLevel) => {
   let color: string
   switch (level) {
     case LogLevel.Verbose:
       color = FgBlue
       break
-
     case LogLevel.Debug:
       color = FgBlue
       break
-
     case LogLevel.Information:
       color = FgGreen
       break
-
     case LogLevel.Warning:
       color = FgYellow
       break
-
     case LogLevel.Error:
       color = FgRed
       break
-
     default:
       color = FgRed
       break
@@ -116,27 +77,18 @@ export const getLevelColor = (level: LogLevel) => {
 }
 
 /**
-
  * The default formatter for the Console logger
-
  * @param entry the log entry to be formatted
-
  */
-
 export const defaultFormatter = <T>(entry: LeveledLogEntry<T>) => {
   const fontColor = getLevelColor(entry.level)
-
   return [`${fontColor}%s${Reset}`, entry.scope, entry.message]
 }
 
 /**
-
  * Formatter for a verbose message
-
  * @param entry the log entry
-
  */
-
 export const verboseFormatter = <T>(entry: LeveledLogEntry<T>) => {
   const fontColor = getLevelColor(entry.level)
 
@@ -146,11 +98,8 @@ export const verboseFormatter = <T>(entry: LeveledLogEntry<T>) => {
 }
 
 /**
-
  * A logger implementation that dumps log messages to the console
-
  */
-
 @Injectable({ lifetime: 'scoped' })
 export class ConsoleLogger extends AbstractLogger {
   public async addEntry<T>(entry: LeveledLogEntry<T>) {
