4f8b76f47aaba22bae7be810199336c372096c54
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-01-07 11:13:24 +0100

    snapp: use cheap-module-eval-source-map for faster builds

--- apps/sensenet/webpack.dev.js
+++ apps/sensenet/webpack.dev.js
@@ -5,7 +5,7 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'cheap-module-source-map',
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     historyApiFallback: true,
   },
