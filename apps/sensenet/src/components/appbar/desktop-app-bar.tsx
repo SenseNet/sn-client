6d76ce59221f8046488b729c536a32739830e5e3
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-02-19 22:09:24 +0100

    snapp: Remove repository selector from appbar

--- apps/sensenet/src/components/appbar/desktop-app-bar.tsx
+++ apps/sensenet/src/components/appbar/desktop-app-bar.tsx
@@ -1,45 +1,45 @@
-import AppBar from '@material-ui/core/AppBar'
-import IconButton from '@material-ui/core/IconButton'
-import Toolbar from '@material-ui/core/Toolbar'
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core'
 import Menu from '@material-ui/icons/Menu'
 import React, { useContext } from 'react'
-import { ResponsiveContext, ResponsivePersonalSetttings } from '../../context'
-import { useCommandPalette, useTheme } from '../../hooks'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/sensenet-icon-32.png'
+import { ResponsivePersonalSetttings } from '../../context'
+import { useCommandPalette, useRepoUrlFromLocalStorage } from '../../hooks'
 import { CommandPalette } from '../command-palette/CommandPalette'
-import { RepositorySelector } from '../RepositorySelector'
 import { DesktopNavMenu } from './desktop-nav-menu'
 
 export const DesktopAppBar: React.FunctionComponent<{ openDrawer?: () => void }> = props => {
-  const device = useContext(ResponsiveContext)
-  const theme = useTheme()
+  const { repoUrl } = useRepoUrlFromLocalStorage()
   const personalSettings = useContext(ResponsivePersonalSetttings)
-
   const commandPalette = useCommandPalette()
 
   return (
-    <AppBar
-      position="sticky"
-      style={{ backgroundColor: theme.palette.background.paper, position: 'relative', height: '64px' }}>
+    <AppBar position="sticky" color="inherit">
       <Toolbar style={{ position: 'static' }}>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            textDecoration: 'none',
-            overflow: 'hidden',
-            alignItems: 'center',
-            flexGrow: commandPalette.isOpened ? 0 : 1,
-          }}>
-          {personalSettings.drawer.type === 'temporary' ? (
+        {personalSettings.drawer.type === 'temporary' ? (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              textDecoration: 'none',
+              overflow: 'hidden',
+              alignItems: 'center',
+              flexGrow: commandPalette.isOpened ? 0 : 1,
+            }}>
             <IconButton
               onClick={() => {
-                props.openDrawer && props.openDrawer()
+                props.openDrawer?.()
               }}>
               <Menu />
             </IconButton>
-          ) : null}
-          {device !== 'desktop' && commandPalette.isOpened ? null : <RepositorySelector />}
-        </div>
+          </div>
+        ) : null}
+        <Typography variant="h5">
+          <Link to="/" style={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
+            <img src={logo} style={{ marginRight: '.5em', filter: 'drop-shadow(0px 1    px 1px black)' }} alt="logo" />
+            <p>{repoUrl}</p>
+          </Link>
+        </Typography>
 
         {personalSettings.commandPalette.enabled ? <CommandPalette {...commandPalette} /> : <div style={{ flex: 1 }} />}
         <DesktopNavMenu />
