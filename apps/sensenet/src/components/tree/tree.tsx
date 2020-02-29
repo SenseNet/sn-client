96a7936853a33f38eba91eeeb2672ec275f37e00
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-02-29 21:22:16 +0100

    snapp: Modify tree not to load all nodes

--- apps/sensenet/src/components/tree/tree.tsx
+++ apps/sensenet/src/components/tree/tree.tsx
@@ -1,16 +1,18 @@
 /* eslint-disable import/named */
 import { ListItem, ListItemIcon, ListItemText, List as MuiList } from '@material-ui/core'
+import { Add } from '@material-ui/icons'
 import { GenericContent } from '@sensenet/default-content-types'
 import { useRepository } from '@sensenet/hooks-react'
 import React, { ReactNode, useCallback, useEffect, useRef, useState } from 'react'
 import { AutoSizer, Index, IndexRange, InfiniteLoader, List, ListRowProps } from 'react-virtualized'
-import { Icon } from '../Icon'
-import { ContentContextMenu } from '../context-menu/content-context-menu'
 import { useSelectionService } from '../../hooks'
+import { ContentContextMenu } from '../context-menu/content-context-menu'
+import { Icon } from '../Icon'
 
 type ItemType = GenericContent & {
-  Children: ItemType[]
-  expanded: boolean
+  children: ItemType[]
+  expanded?: boolean
+  hasNextPage?: boolean
 }
 
 type TreeProps = {
@@ -21,18 +23,33 @@ type TreeProps = {
 const ROW_HEIGHT = 48
 
 export function Tree(props: TreeProps) {
-  const listRef = useRef<List>()
+  const listRef = useRef<List>(null)
   const selectionService = useSelectionService()
   const [contextMenuAnchor, setContextMenuAnchor] = useState<HTMLElement | null>(null)
   const repo = useRepository()
-  const [items, setItems] = useState<GenericContent[]>()
+  const [items, setItems] = useState<ItemType[]>()
   const [itemCount, setItemCount] = useState<number>()
 
   const rowHeight = ({ index }: Index) => {
     if (!items || !items[index]) {
       return ROW_HEIGHT
     }
-    return getExpandedItemCount(items[index] as any) * ROW_HEIGHT
+    return getExpandedItemCount(items[index]) * ROW_HEIGHT
+  }
+
+  const getExpandedItemCount = (item: ItemType) => {
+    let totalCount = 1
+
+    if (item.expanded) {
+      totalCount += item.children.map(getExpandedItemCount).reduce((total, count) => {
+        return total + count
+      }, 0)
+      if (item.hasNextPage) {
+        totalCount++
+      }
+    }
+
+    return totalCount
   }
 
   const isRowLoaded = ({ index }: Index) => {
@@ -40,25 +57,28 @@ export function Tree(props: TreeProps) {
   }
 
   const loadMoreRows = async ({ startIndex, stopIndex }: IndexRange) => {
-    const ac = new AbortController()
-    const result = await getContent(ac, props.parentPath, stopIndex, startIndex)
+    const result = await getContent(props.parentPath, stopIndex, startIndex)
     setItems(prevItems => {
       if (prevItems) {
-        return [...prevItems, ...result.d.results]
+        return [...prevItems, ...result.d.results.map(mapContentToItemType)]
       }
-      return result.d.results
+      return result.d.results.map(mapContentToItemType)
     })
   }
+
+  const mapContentToItemType = (content: GenericContent) => ({ children: [], ...content })
 
   function renderItem(item: ItemType, keyPrefix: string, paddingLeft: number) {
     const onClick = async (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
       event.stopPropagation()
       props.onItemClick(item)
-      if (!item.expanded && !item.Children) {
-        const ac = new AbortController()
-        // TODO: figure out how to load this lazily
-        const result = await getContent(ac, item.Path, 100, 0)
-        item.Children = result.d.results as any
+      if (item.Type === 'File') {
+        return
+      }
+      if (!item.expanded && !item.children.length) {
+        const result = await getContent(item.Path, 10, 0)
+        item.children = result.d.results.map(mapContentToItemType)
+        item.hasNextPage = result.d.__count > result.d.results.length
       }
       item.expanded = !item.expanded
       listRef.current?.recomputeRowHeights()
@@ -68,7 +88,7 @@ export function Tree(props: TreeProps) {
     let children: ReactNode[] = []
 
     if (item.expanded) {
-      children = item.Children.map((child, index) => {
+      children = item.children.map((child, index) => {
         return renderItem(child, `${keyPrefix}-${index}`, paddingLeft + 20)
       })
     }
@@ -84,49 +104,59 @@ export function Tree(props: TreeProps) {
         selected={selectionService.activeContent.getValue()?.Id === item.Id}
         key={keyPrefix}
         style={{ paddingLeft }}
-        button
-        className="item">
+        button>
         <ListItemIcon>
           <Icon item={item} />
         </ListItemIcon>
         <ListItemText primary={item.DisplayName} />
       </ListItem>
     )
+
+    if (item.hasNextPage && item.expanded) {
+      const loadMore = (
+        <ListItem
+          button
+          style={{ paddingLeft: paddingLeft + 20 }}
+          onClick={async e => {
+            e.stopPropagation()
+            const result = await getContent(item.Path, 10, item.children.length)
+            item.children = [...item.children, ...result.d.results.map(mapContentToItemType)]
+            item.hasNextPage = result.d.results.length >= 10
+            listRef.current?.recomputeRowHeights()
+            listRef.current?.forceUpdate()
+          }}>
+          <ListItemIcon>
+            <Add />
+          </ListItemIcon>
+          <ListItemText primary="Load more" />
+        </ListItem>
+      )
+      children.push(loadMore)
+    }
     children.unshift(nodeItem)
 
     return children
   }
 
   const rowRenderer = ({ key, style, index }: ListRowProps) => {
-    if (!items) {
+    if (!items || !items[index]) {
       return (
         <p style={style} key={key}>
           Loading
         </p>
       )
     }
-    const node = renderItem(items[index] as any, index.toString(), 0)
+
     return (
       <MuiList key={key} style={style}>
-        {node}
+        {renderItem(items[index], index.toString(), 10)}
       </MuiList>
     )
   }
 
-  const getExpandedItemCount = (item: ItemType) => {
-    let totalCount = 1
-
-    if (item.expanded) {
-      totalCount += item.Children.map(getExpandedItemCount).reduce((total, count) => {
-        return total + count
-      }, 0)
-    }
-
-    return totalCount
-  }
-
   const getContent = useCallback(
-    async (ac: AbortController, path: string, top: number, skip: number) => {
+    async (path: string, top: number, skip: number) => {
+      const ac = new AbortController()
       return await repo.loadCollection<GenericContent>({
         path,
         requestInit: {
@@ -177,29 +207,27 @@ export function Tree(props: TreeProps) {
         flexGrow: 2,
         flexShrink: 0,
         borderRight: '1px solid rgba(128,128,128,.2)',
-        overflow: 'auto',
       }}>
       <InfiniteLoader isRowLoaded={isRowLoaded} loadMoreRows={loadMoreRows} rowCount={itemCount}>
-        {({ onRowsRendered, registerChild }) => (
-          <AutoSizer>
-            {({ height, width }) => (
-              <List
-                height={height}
-                onRowsRendered={onRowsRendered}
-                overscanRowCount={10}
-                ref={ref => {
-                  registerChild(ref)
-                  // ref type is List | null but listref.Current is List | undefined
-                  listRef.current = ref as any
-                }}
-                rowHeight={rowHeight}
-                rowRenderer={rowRenderer}
-                rowCount={itemCount}
-                width={width}
-              />
-            )}
-          </AutoSizer>
-        )}
+        {({ onRowsRendered, registerChild }) => {
+          registerChild(listRef.current)
+          return (
+            <AutoSizer>
+              {({ width, height }) => (
+                <List
+                  height={height}
+                  onRowsRendered={onRowsRendered}
+                  overscanRowCount={10}
+                  ref={listRef}
+                  rowHeight={rowHeight}
+                  rowRenderer={rowRenderer}
+                  rowCount={itemCount}
+                  width={width}
+                />
+              )}
+            </AutoSizer>
+          )
+        }}
       </InfiniteLoader>
       {selectionService.activeContent.getValue() ? (
         <ContentContextMenu
