83b6f60489038d24be5ac8f3f4b38cf442e8fa03
Anikó Litványi <herflis33@gmail.com>
2020-03-03 16:11:15 +0100

    Add a new reducer for sharing stuff

---
+++ packages/sn-redux/src/reducers/sharing.ts
@@ -0,0 +1,37 @@
+import { combineReducers, Reducer } from 'redux'
+import { SharingLevel, SharingMode } from '@sensenet/client-core/src'
+
+export interface SharingEntry {
+  Id: string
+  Token: string
+  Identity: 0
+  Mode: SharingMode
+  Level: SharingLevel
+  CreatorId: number
+  ShareDate: string
+}
+
+export const sharingEntries: Reducer<SharingEntry[]> = (state = [], action) => {
+  switch (action.type) {
+    case 'GET_SHARING_ENTRIES_SUCCESS':
+      console.log(action.result.d.results)
+      console.log(action)
+      for (const entry of action.result.d.results) {
+        state[entry.Id] = entry
+      }
+      return {
+        ...state,
+      }
+    default:
+      return state
+  }
+}
+
+/**
+ * Reducer combining sharing related reducers in one.
+ */
+export const sharing = combineReducers<{
+  sharingEntries: ReturnType<typeof sharingEntries>
+}>({
+  sharingEntries,
+})
