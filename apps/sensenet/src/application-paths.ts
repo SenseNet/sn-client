fc2800d005cd218a2e983591aada6c7a14285df3
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-03-19 15:43:41 +0100

    Remove unused path from applicationPaths

--- apps/sensenet/src/application-paths.ts
+++ apps/sensenet/src/application-paths.ts
@@ -1,7 +1,6 @@
 const authPrefix = '/authentication'
 
 export const applicationPaths = {
-  login: `${authPrefix}/login`,
   loginCallback: `${authPrefix}/login-callback`,
   logOutCallback: `${authPrefix}/logout-callback`,
 }
