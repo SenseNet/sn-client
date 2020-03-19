593a3e8e8f774d436cc4341cf324e7f84e287422
eniko.pusztai <eniko.pusztai@sensenet.com>
2020-03-19 16:12:15 +0100

    fix frontend crash after deleting from tree (#591)
    * fix frontend crash after deleting from tree
    
    * create test for delete
    
    * create test for modify
    
    * delete tree/index.tsx
    
    * fix delete on root bug

--- packages/sn-hooks-react/test/current-content.test.tsx
+++ packages/sn-hooks-react/test/current-content.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { mount, shallow } from 'enzyme'
-import { act } from 'react-dom/test-utils'
 import { Repository } from '@sensenet/client-core'
-import { CurrentContentProvider } from '../src/context/current-content'
+import { mount, shallow } from 'enzyme'
+import React from 'react'
+import { act } from 'react-dom/test-utils'
 import { RepositoryContext } from '../src/context'
+import { CurrentContentContext, CurrentContentProvider } from '../src/context/current-content'
 
 describe('CurrentContent', () => {
   it('matches snapshot', () => {
@@ -32,5 +32,72 @@ describe('CurrentContent', () => {
         </RepositoryContext.Provider>,
       )
     })
+  })
+
+  it('should load the parent when deleted', async () => {
+    const mockContent = { Id: 1, Name: 'Teszt1', Path: '/Root/Content/IT' }
+    let currentRepoLoadArgs: any
+    const repo = new Repository()
+    repo.load = args => {
+      currentRepoLoadArgs = args
+      return { d: mockContent } as any
+    }
+    repo.delete = () => {
+      return { d: { results: [mockContent], errors: [] } } as any
+    }
+    await act(async () => {
+      mount(
+        <RepositoryContext.Provider value={repo}>
+          <CurrentContentProvider idOrPath={mockContent.Path} />
+        </RepositoryContext.Provider>,
+      )
+    })
+    expect(currentRepoLoadArgs.idOrPath).toBe(mockContent.Path)
+    await act(async () => {
+      repo.delete({ idOrPath: mockContent.Path })
+    })
+    expect(currentRepoLoadArgs.idOrPath).toBe('Root/Content')
+  })
+
+  it('should reload on modify', async () => {
+    const mockContent = { Id: 1, Name: 'Teszt1', Path: '/Root/Content/IT' }
+
+    const repo = new Repository()
+    repo.load = () => {
+      return { d: { ...mockContent } } as any
+    }
+    repo.patch = (patchOptions: any) => {
+      mockContent.Name = patchOptions.content.Name
+      return { d: mockContent, errors: [] } as any
+    }
+    let wrapper: any
+    await act(async () => {
+      wrapper = mount(
+        <RepositoryContext.Provider value={repo}>
+          <CurrentContentProvider idOrPath={mockContent.Path} onContentLoaded={jest.fn}>
+            <CurrentContentContext.Consumer>{value => <div>Name: {value.Name}</div>}</CurrentContentContext.Consumer>
+          </CurrentContentProvider>
+        </RepositoryContext.Provider>,
+      )
+    })
+    expect(
+      wrapper
+        .update()
+        .find('div')
+        .text(),
+    ).toBe('Name: Teszt1')
+    await act(async () => {
+      repo.patch({
+        idOrPath: mockContent.Path,
+        content: { Name: 'Teszt2', Id: 1 },
+      })
+    })
+
+    expect(
+      wrapper
+        .update()
+        .find('div')
+        .text(),
+    ).toBe('Name: Teszt2')
   })
 })
