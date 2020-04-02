97d06822a83ac7fdc9504dc2597cee9416ed186d
Zoltán Bedi <zoltan.bedi@gmail.com>
2020-04-02 16:04:08 +0200

    Add oidc auth to calendar example

--- examples/sn-react-calendar/src/components/navbar.tsx
+++ examples/sn-react-calendar/src/components/navbar.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import { AppBar, Button, Toolbar, Tooltip, Typography } from '@material-ui/core'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import { useRepository } from '@sensenet/hooks-react'
+import { useOidcAuthentication } from '@sensenet/authentication-oidc-react'
+import React from 'react'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,18 +20,18 @@ const useStyles = makeStyles((theme: Theme) =>
  * Navbar component
  */
 export const NavBarComponent: React.FunctionComponent = () => {
-  const repo = useRepository()
+  const { logout } = useOidcAuthentication()
   const classes = useStyles()
 
   return (
     <div className={classes.root}>
-      <AppBar>
+      <AppBar position="relative">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            Calendar
+            Document Browser
           </Typography>
           <Tooltip title="Return to the Login screen and select another repository">
-            <Button color="inherit" onClick={() => repo.authentication.logout()}>
+            <Button color="inherit" onClick={logout}>
               Log out
             </Button>
           </Tooltip>
