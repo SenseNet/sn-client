bd1df440657dd172180151016ca3cc1530278509
eniko.pusztai <eniko.pusztai@sensenet.com>
2020-04-03 12:28:36 +0200

    Update apps/sensenet/src/components/view-controls/edit-view.tsx
    Co-Authored-By: Aniko Litvanyi <herflis33@gmail.com>

--- apps/sensenet/src/components/view-controls/edit-view.tsx
+++ apps/sensenet/src/components/view-controls/edit-view.tsx
@@ -156,7 +156,7 @@ export const EditView: React.FC<EditViewProps> = props => {
           onSubmit={handleSubmit}>
           <Grid container={true} spacing={2}>
             {schema.fieldMappings
-              .sort((item1, item2) => item2.fieldSettings.DefaultOrder! - item1.fieldSettings.DefaultOrder!)
+              .sort((item1, item2) => item2.fieldSettings.FieldIndex! - item1.fieldSettings.FieldIndex!)
               .map(field => {
                 const fieldControl = createElement(
                   controlMapper.getControlForContentField(
