b8b21cab37c64c0f22b1ac63a34baaa9acb0e417
Enikő Pusztai <eniko.pusztai@sensenet.com>
2020-04-06 19:36:08 +0200

    install cypress-xpath, create login and logout functions

--- cypress/integration/open_admin_ui.spec.js
+++ cypress/integration/open_admin_ui.spec.js
@@ -1,37 +1,18 @@
-describe('Open admin ui', () => {
+const userName = 'businesscat'
+const password = 'businesscat'
+const repository = 'https://dev.demo.sensenet.com'
+const welcomeMessage = 'Welcome back, Business Cat'
+
+describe('Test forms', () => {
   beforeEach(() => {
-    cy.viewport(1602, 947)
-    cy.visit('/')
+    cy.login(userName, password, repository)
   })
 
-  it('reaches the admin landing page', () => {
-    cy.contains('Welcome!')
-    cy.url().should('include', '/login')
+  afterEach(() => {
+    cy.logout()
   })
 
-  it('accepts input for username', () => {
-    const userName = 'businesscat'
-    const password = 'businesscat'
-    const repository = 'https://dev.demo.sensenet.com'
-    const welcomeMessage = 'Welcome back, Business Cat'
-
-    cy.get('#username')
-      .type(userName)
-      .should('have.value', userName)
-
-    cy.get('#password')
-      .type(password)
-      .should('have.value', password)
-
-    cy.get('#repository')
-      .type(repository)
-      .should('have.value', repository)
-
-    cy.get('button[aria-label="Login"]').click()
-
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(3000)
-
+  it('edit', () => {
     cy.contains(welcomeMessage)
   })
 })
