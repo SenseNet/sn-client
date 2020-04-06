23e8c9a11bcf8ed7670c1fbdf54a4bdb23e05da9
Enikő Pusztai <eniko.pusztai@sensenet.com>
2020-04-06 19:36:08 +0200

    install cypress-xpath, create login and logout functions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,37 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (userName, password, repository) => {
+  cy.viewport(1602, 947)
+
+  cy.visit('/')
+
+  cy.url().should('include', '/login')
+
+  cy.get('#username')
+    .type(userName)
+    .should('have.value', userName)
+
+  cy.get('#password')
+    .type(password)
+    .should('have.value', password)
+
+  cy.get('#repository')
+    .type(repository)
+    .should('have.value', repository)
+
+  cy.get('button[aria-label="Login"]').click()
+
+  cy.location('pathname', { timeout: 20000 }).should('eq', '/')
+})
+
+Cypress.Commands.add('logout', () => {
+  cy.xpath('/html/body/div[1]/div/header/div/div[3]/button/span[1]').click()
+
+  cy.xpath('/html/body/div[1]/div/header/div/div[3]/div[2]/div/ul/li[2]').as('logoutButton')
+
+  cy.get('@logoutButton')
+    .contains('Log out')
+    .click()
+
+  cy.xpath('/html/body/div[2]/div[3]/div/div/div[3]/button[2]').click()
+
+  cy.url().should('include', '/login')
+})
