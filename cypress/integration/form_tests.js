864587fc53eb2ed1dec62c5215c0cb465d168fc6
Enikő Pusztai <eniko.pusztai@sensenet.com>
2020-04-07 19:10:38 +0200

    create new user

--- cypress/integration/form_tests.js
+++ cypress/integration/form_tests.js
@@ -2,6 +2,11 @@ const userName = 'builtin\\admin'
 const password = 'admin'
 const repository = 'https://dev.demo.sensenet.com'
 
+const loginNameText = 'testuser'
+const fullNameText = 'Test User'
+const emailText = 'test@test.com'
+const passwordText = 'testpassword'
+
 describe('Test forms', () => {
   beforeEach(() => {
     cy.login(userName, password, repository)
@@ -16,8 +21,11 @@ describe('Test forms', () => {
     const user = 'User'
 
     cy.xpath('/html/body/div[1]/div/div/div[1]/div/ul/div/a[3]/div').click()
+
     cy.xpath('/html/body/div[1]/div/div/div[2]/div/div[1]/span').as('usersAndGroupsTitle')
+
     cy.get('@usersAndGroupsTitle').contains(usersAndGroupsTitle)
+
     cy.xpath('/html/body/div[1]/div/div/div[1]/div/ul/div/div[2]/div/span/button').click()
 
     cy.xpath('/html/body/div[2]/div[3]/ul')
@@ -34,5 +42,34 @@ describe('Test forms', () => {
       .contains(user)
 
     cy.get('@newUserButton').click()
+
+    cy.get('#LoginName')
+      .type(loginNameText)
+      .should('have.value', loginNameText)
+
+    cy.get('#FullName')
+      .type(fullNameText)
+      .should('have.value', fullNameText)
+
+    cy.get('#Email')
+      .type(emailText)
+      .should('have.value', emailText)
+
+    cy.get('#Password').type(passwordText)
+
+    cy.contains('Submit').click()
+
+    cy.get('.ReactVirtualized__Grid__innerScrollContainer')
+      .children()
+      .last()
+      .as('newUser')
+
+    cy.get('@newUser')
+      .children()
+      .eq(1)
+      .children('div')
+      .children('div')
+      .children('div')
+      .contains(fullNameText)
   })
 })
