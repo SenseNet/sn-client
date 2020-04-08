4dbad1bef1f44d405e2de3d2d2c563c1b3117649
Enikő Pusztai <eniko.pusztai@sensenet.com>
2020-04-08 11:00:00 +0200

    create edit user test

--- cypress/integration/form_tests.js
+++ cypress/integration/form_tests.js
@@ -3,9 +3,13 @@ const password = 'admin'
 const repository = 'https://dev.demo.sensenet.com'
 
 const loginNameText = 'testuser'
-const fullNameText = 'Test User'
+const fullNameText = 'Test Test'
 const emailText = 'test@test.com'
 const passwordText = 'testpassword'
+
+const usersAndGroupsTitle = 'Users and groups'
+
+const modifiedFullNameText = 'Test User'
 
 describe('Test forms', () => {
   beforeEach(() => {
@@ -13,11 +17,10 @@ describe('Test forms', () => {
   })
 
   afterEach(() => {
-    //cy.logout()
+    cy.logout()
   })
 
   it('creates a new user', () => {
-    const usersAndGroupsTitle = 'Users and groups'
     const user = 'User'
 
     cy.xpath('/html/body/div[1]/div/div/div[1]/div/ul/div/a[3]/div').click()
@@ -71,5 +74,52 @@ describe('Test forms', () => {
       .children('div')
       .children('div')
       .contains(fullNameText)
+  })
+
+  it('edit the name of new user', () => {
+    cy.xpath('/html/body/div[1]/div/div/div[1]/div/ul/div/a[3]/div').click()
+
+    cy.xpath('/html/body/div[1]/div/div/div[2]/div/div[1]/span').as('usersAndGroupsTitle')
+
+    cy.get('@usersAndGroupsTitle').contains(usersAndGroupsTitle)
+
+    cy.get('.ReactVirtualized__Grid__innerScrollContainer')
+      .children()
+      .last()
+      .as('newUser')
+
+    cy.get('@newUser').rightclick()
+
+    cy.get('.MuiPopover-root div ul')
+      .children()
+      .should('have.length', 8)
+
+    cy.get('.MuiPopover-root div ul')
+      .children('li')
+      .children('div')
+      .contains('Edit')
+      .click()
+
+    cy.get('#FullName')
+      .clear()
+      .type(modifiedFullNameText)
+      .should('have.value', modifiedFullNameText)
+
+    cy.contains('Submit').click()
+
+    cy.contains('Cancel').click()
+
+    cy.get('.ReactVirtualized__Grid__innerScrollContainer')
+      .children()
+      .last()
+      .as('newUser')
+
+    cy.get('@newUser')
+      .children()
+      .eq(1)
+      .children('div')
+      .children('div')
+      .children('div')
+      .contains(modifiedFullNameText)
   })
 })
