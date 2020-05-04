2ec657bc10bb5c31feee0424788d4b3796101d9b
eniko.pusztai <eniko.pusztai@sensenet.com>
2020-05-04 10:19:47 +0200

    Update packages/sn-document-viewer-react/src/components/document-widgets/ToggleThumbnails.tsx
    Co-authored-by: Zoltán Bedi <zoltan.bedi@gmail.com>

--- packages/sn-document-viewer-react/src/components/document-widgets/ToggleThumbnails.tsx
+++ packages/sn-document-viewer-react/src/components/document-widgets/ToggleThumbnails.tsx
@@ -1,5 +1,4 @@
 import React, { CSSProperties } from 'react'
-//import { styles } from '../styles'
 import { useLocalization, useViewerState } from '../../hooks'
 import { ToggleBase } from './ToggleBase'
 
