33dd8b6b0aac9b0de2f8fcbaac136cf87d0447c3
Enikő Pusztai <eniko.pusztai@sensenet.com>
2020-09-11 14:57:25 +0200

    fix email field style

--- apps/sensenet/src/components/content-list/email-field.tsx
+++ apps/sensenet/src/components/content-list/email-field.tsx
@@ -1,14 +1,34 @@
-import { TableCell } from '@material-ui/core'
+import { createStyles, makeStyles, TableCell, Tooltip } from '@material-ui/core'
 import clsx from 'clsx'
 import React from 'react'
 import { useGlobalStyles } from '../../globalStyles'
 
+const useStyles = makeStyles(() => {
+  return createStyles({
+    anchor: {
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+    },
+    cell: {
+      padding: '0 3px',
+    },
+  })
+})
+
 export const EmailField: React.FC<{ mail: string }> = ({ mail }) => {
   const globalClasses = useGlobalStyles()
+  const classes = useStyles()
 
   return (
-    <TableCell className={clsx(globalClasses.centeredLeft, globalClasses.virtualizedCellStyle)} component="div">
-      <a href={`mailto:${mail}`}>{mail}</a>
+    <TableCell
+      className={clsx(globalClasses.centeredLeft, globalClasses.virtualizedCellStyle, classes.cell)}
+      component="div">
+      <Tooltip placement="top" title={mail}>
+        <a href={`mailto:${mail}`} className={classes.anchor}>
+          {mail}
+        </a>
+      </Tooltip>
     </TableCell>
   )
 }
