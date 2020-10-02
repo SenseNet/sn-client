638508eed73a53c66eef33e9f2502b1edfa82393
Pieter Vanhoutteghem <piv@ocit9006-2019.be.eocgroup.com>
2020-10-02 11:35:45 +0200

    E2E Logo Navigation Test

---
+++ apps/sensenet/cypress/integration/ui/logonavigation.ts
@@ -0,0 +1,19 @@
+import { PATHS } from '../../../src/application-paths'
+import { pathWithQueryParams } from '../../../src/services/query-string-builder'
+
+describe('Logo Navigation Test', () => {
+  beforeEach(() => {
+    cy.login('admin')
+    cy.visit(pathWithQueryParams({ path: '/', newParams: { repoUrl: Cypress.env('repoUrl') } }))
+  })
+
+  it('should go to Root on click of Logo', () => {
+    cy.get('a[href="/content/explorer/"]').click()
+    cy.get('[alt="logo"]')
+      .click()
+      .location()
+      .should((loc) => {
+        expect(loc.pathname).to.eq('/')
+      })
+  })
+})
