840678ae561f7a3fa7e5a032d32fa21984ff9692
Daniel Shuy <daniel_shuy@hotmail.com>
2020-10-09 15:05:42 +0800

    Add beforeEach()

--- apps/sensenet/cypress/integration/drawer/drawer.spec.ts
+++ apps/sensenet/cypress/integration/drawer/drawer.spec.ts
@@ -1,18 +1,18 @@
 import { pathWithQueryParams } from '../../../src/services/query-string-builder'
 
 describe('Drawer', () => {
-  it('clicking on the hamburger menu icon should open the drawer', () => {
+  beforeEach(() => {
     cy.login()
     cy.visit(pathWithQueryParams({ path: '/', newParams: { repoUrl: Cypress.env('repoUrl') } }))
+  })
 
+  it('clicking on the hamburger menu icon should open the drawer', () => {
     cy.get('[data-test="drawer-expandcollapse-button"]').click()
 
     cy.get('[data-test="drawer"]').should('have.css', 'width', '250px')
   })
 
   it('clicking on the X icon should close the extended view of the drawer', () => {
-    cy.login()
-    cy.visit(pathWithQueryParams({ path: '/', newParams: { repoUrl: Cypress.env('repoUrl') } }))
     cy.get('[data-test="drawer-expandcollapse-button"]').click()
 
     cy.get('[data-test="drawer-expandcollapse-button"]').click()
