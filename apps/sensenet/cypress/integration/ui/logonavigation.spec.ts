52c5db3f6c16f259d50cc324536692ee8d08dab6
Pieter Vanhoutteghem <pieter.vanh@gmail.com>
2020-10-10 13:06:56 +0200

    make logo selector test better

--- apps/sensenet/cypress/integration/ui/logonavigation.spec.ts
+++ apps/sensenet/cypress/integration/ui/logonavigation.spec.ts
@@ -2,13 +2,13 @@ import { pathWithQueryParams } from '../../../src/services/query-string-builder'
 
 describe('Logo Navigation Test', () => {
   beforeEach(() => {
-    cy.login('admin')
+    cy.login()
     cy.visit(pathWithQueryParams({ path: '/', newParams: { repoUrl: Cypress.env('repoUrl') } }))
   })
 
   it('should go to Root on click of Logo', () => {
     cy.get('a[href="/content/explorer/"]').click()
-    cy.get('[alt="logo"]')
+    cy.get('[data-test="sensenet-logo"]')
       .click()
       .location()
       .should((loc) => {
