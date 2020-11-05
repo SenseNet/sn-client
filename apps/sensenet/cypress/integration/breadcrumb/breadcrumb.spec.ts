b6bd8e43251c84d8026014da420d8518564d45be
Nilesh Patel <nilesh.nvs@hotmail.com>
2020-11-05 16:58:15 +0530

    changes done

--- apps/sensenet/cypress/integration/breadcrumb/breadcrumb.spec.ts
+++ apps/sensenet/cypress/integration/breadcrumb/breadcrumb.spec.ts
@@ -1,41 +1,56 @@
 import { pathWithQueryParams } from '../../../src/services/query-string-builder'
 
+const expectedBreadcrumbItems = ['Content', '/', 'IT Workspace', '/', 'Document library']
+
 describe('Breadcrumb', () => {
   before(() => {
     cy.login()
     cy.visit(pathWithQueryParams({ path: '/', newParams: { repoUrl: Cypress.env('repoUrl') } }))
   })
   it('breadcrumb after navigating to IT Workspace, breadcrumb should be displayed as it should.', () => {
-    const expectedBreadcrumbItems = ['Content', '/', 'IT Workspace', '/', 'Document Library']
     cy.get('[data-test="drawer-menu-item-Content"]').click()
     cy.get('[data-test="IT Workspace"]')
       .click()
       .then(() => {
-        cy.get('[data-test="Calendar"]').should('be.visible')
         cy.get('[data-test="table-cell-Calendar"]').should('be.visible')
       })
 
-    cy.get('[data-test="Document Library"]').click({ force: true })
-    cy.get('nav[aria-label="breadcrumb"] li').each(($el) => {
-      expect(expectedBreadcrumbItems).to.include($el.text())
-    })
+    cy.get('[data-test="Document library"]').click({ force: true })
+    cy.get('nav[aria-label="breadcrumb"] li')
+      .should('have.length', expectedBreadcrumbItems.length)
+      .each(($el) => {
+        expect(expectedBreadcrumbItems).to.include($el.text())
+      })
   })
   it('clicking on parent item in the breadcrumb should open the chosen container', () => {
-    const expectedBreadcrumbItems = ['Content', '/', 'IT Workspace', '/', 'Document Library']
     cy.get('[data-test="drawer-menu-item-Content"]').click()
-    cy.get('[data-test="IT Workspace"]').click()
-    cy.get('[data-test="Document Library"]').click({ force: true })
-    cy.get('nav[aria-label="breadcrumb"] li').each(($el) => {
-      expect(expectedBreadcrumbItems).to.include($el.text())
-    })
-    cy.get('[data-test="Document Library"]').should('be.visible')
-    cy.get('[data-test="table-cell-Document Library"]').should('be.visible')
+    cy.get('[data-test="IT Workspace"]')
+      .click()
+      .then(() => {
+        cy.get('[data-test="table-cell-Calendar"]').should('be.visible')
+      })
+    cy.get('[data-test="Document library"]')
+      .click({ force: true })
+      .then(() => {
+        cy.get('[data-test="table-cell-Calendar"]').should('not.be.visible')
+      })
+    cy.get('nav[aria-label="breadcrumb"] li')
+      .find('button[aria-label="IT Workspace"]')
+      .click()
+      .then(() => {
+        cy.get('[data-test="table-cell-Calendar"]').should('be.visible')
+      })
   })
   it('right click on a breadcrumb item should open its actionmenu.', () => {
     cy.get('[data-test="drawer-menu-item-Content"]').click()
-    cy.get('[data-test="IT Workspace"]').click()
-    cy.get('[data-test="Document Library"]').click({ force: true })
+    cy.get('[data-test="IT Workspace"]')
+      .click()
+      .then(() => {
+        cy.get('[data-test="table-cell-Calendar"]').should('be.visible')
+      })
+    cy.get('[data-test="Document library"]').click({ force: true })
     cy.get('nav[aria-label="breadcrumb"] li')
+      .should('have.length', expectedBreadcrumbItems.length)
       .find('button[aria-label="IT Workspace"]')
       .rightclick()
       .then(() => {
