00f309862e0d51caccf724d5e3c9b3fda22b0408
Nilesh Patel <nilesh.nvs@hotmail.com>
2020-11-05 09:36:12 +0530

    changes done as requested

--- apps/sensenet/cypress/integration/breadcrumb/breadcrumb.spec.ts
+++ apps/sensenet/cypress/integration/breadcrumb/breadcrumb.spec.ts
@@ -6,9 +6,16 @@ describe('Breadcrumb', () => {
     cy.visit(pathWithQueryParams({ path: '/', newParams: { repoUrl: Cypress.env('repoUrl') } }))
   })
   it('breadcrumb after navigating to IT Workspace, breadcrumb should be displayed as it should.', () => {
-    const expectedBreadcrumbItems = ['Content', '/', 'IT Workspace']
+    const expectedBreadcrumbItems = ['Content', '/', 'IT Workspace', '/', 'Document Library']
     cy.get('[data-test="drawer-menu-item-Content"]').click()
-    cy.get('[data-test="IT Workspace"]').click()
+    cy.get('[data-test="IT Workspace"]')
+      .click()
+      .then(() => {
+        cy.get('[data-test="Calendar"]').should('be.visible')
+        cy.get('[data-test="table-cell-Calendar"]').should('be.visible')
+      })
+
+    cy.get('[data-test="Document Library"]').click({ force: true })
     cy.get('nav[aria-label="breadcrumb"] li').each(($el) => {
       expect(expectedBreadcrumbItems).to.include($el.text())
     })
@@ -17,15 +24,17 @@ describe('Breadcrumb', () => {
     const expectedBreadcrumbItems = ['Content', '/', 'IT Workspace', '/', 'Document Library']
     cy.get('[data-test="drawer-menu-item-Content"]').click()
     cy.get('[data-test="IT Workspace"]').click()
-    cy.get('[data-test="Document Library"]').click()
+    cy.get('[data-test="Document Library"]').click({ force: true })
     cy.get('nav[aria-label="breadcrumb"] li').each(($el) => {
       expect(expectedBreadcrumbItems).to.include($el.text())
     })
+    cy.get('[data-test="Document Library"]').should('be.visible')
+    cy.get('[data-test="table-cell-Document Library"]').should('be.visible')
   })
   it('right click on a breadcrumb item should open its actionmenu.', () => {
     cy.get('[data-test="drawer-menu-item-Content"]').click()
     cy.get('[data-test="IT Workspace"]').click()
-    cy.get('[data-test="Document Library"]').click()
+    cy.get('[data-test="Document Library"]').click({ force: true })
     cy.get('nav[aria-label="breadcrumb"] li')
       .find('button[aria-label="IT Workspace"]')
       .rightclick()
