d7396b312ef83c606a2371c77042329bf87686d3
pusztaienike <eniko.pusztai@sensenet.com>
2021-03-08 14:57:27 +0100

    implement header

---
+++ packages/sn-universal-header/src/components/header.tsx
@@ -0,0 +1,53 @@
+import { AppBar, createStyles, makeStyles, Toolbar } from '@material-ui/core'
+import React from 'react'
+import { HamburgerMenu, HamburgerMenuClassKey } from './hamburger-menu'
+import { ThreeDotMenu, ThreeDotMenuClassKey } from './three-dot-menu'
+
+export const HEADER_HEIGHT = '60px'
+
+const useStyles = makeStyles(() => {
+  return createStyles({
+    appBar: {
+      height: HEADER_HEIGHT,
+      backgroundColor: '#342cac',
+      boxShadow: 'none',
+    },
+    toolBar: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      height: '100%',
+      paddingRight: 0,
+      paddingLeft: '20px',
+    },
+    hamburgerAndTitleContainer: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+  })
+})
+
+type UniversalHeaderClassKey = Partial<ReturnType<typeof useStyles>>
+
+interface UniversalHeaderProps {
+  title: string
+  threeDotMenuItems: React.ReactNode
+  headerClasses?: UniversalHeaderClassKey
+  hamburgerMenuClasses?: HamburgerMenuClassKey
+  threeDotMenuClasses?: ThreeDotMenuClassKey
+}
+
+export const UniversalHeader: React.FunctionComponent<UniversalHeaderProps> = (props) => {
+  const classes = useStyles(props)
+
+  return (
+    <AppBar position="sticky" className={classes.appBar}>
+      <Toolbar className={classes.toolBar}>
+        <div className={classes.hamburgerAndTitleContainer}>
+          <HamburgerMenu classes={props.hamburgerMenuClasses} />
+          <div>{props.title}</div>
+        </div>
+        <ThreeDotMenu classes={props.threeDotMenuClasses} menuItems={props.threeDotMenuItems} />
+      </Toolbar>
+    </AppBar>
+  )
+}
