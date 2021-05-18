9a3c07ecf5496b2b380e0366876858bf2870ab32
pusztaienike <eniko.pusztai@sensenet.com>
2021-05-18 19:33:04 +0200

    clean config

--- examples/sn-blog-gatsby/configuration.js
+++ examples/sn-blog-gatsby/configuration.js
@@ -1,8 +1,7 @@
 exports.repositoryUrl = 'https://dev.demo.sensenet.com'
 
 exports.configuration = {
-  clientId: 'KxkgOTAxLLCicUx7', //process.env.REACT_APP_CLIENT_ID ?? '', // businesscat clientId for dev.demo.sensenet.com
-  clientSecret: 'secret', //process.env.REACT_APP_CLIENT_SECRET ?? '',
+  clientId: process.env.GATSBY_REACT_APP_CLIENT_ID || '', // businesscat clientId for dev.demo.sensenet.com
+  clientSecret: process.env.GATSBY_REACT_APP_CLIENT_SECRET || '',
   identityServerUrl: 'https://is.demo.sensenet.com',
-  //appId: '7cYLChuhJxyGb7BS', //externalSPA clientId for dev.demo.sensenet.com
 }
