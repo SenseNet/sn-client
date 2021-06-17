35e7401e56c3a2912dad5f1c2bcbf0b9143f4a83
Zoltan Takacs <zoltandavid.takacs@sensenet.com>
2021-06-17 16:44:16 +0200

    lazy load editor (#1355)
    * lazy load editor
    
    * remove comment
    
    * fix unit tests

--- packages/sn-controls-react/test/rich-text-editor.test.tsx
+++ packages/sn-controls-react/test/rich-text-editor.test.tsx
@@ -1,10 +1,55 @@
+/* eslint-disable no-irregular-whitespace */
+
 import { Editor } from '@sensenet/editor-react'
 import FormHelperText from '@material-ui/core/FormHelperText'
 import InputLabel from '@material-ui/core/InputLabel'
 import Typography from '@material-ui/core/Typography'
 import { mount, shallow } from 'enzyme'
-import React from 'react'
+import React, { FC, useEffect, useState } from 'react'
+import { act } from 'react-dom/test-utils'
 import { defaultLocalization, RichTextEditor } from '../src/fieldcontrols'
+
+// The test must wait for React.lazy to resolve
+const waitForComponentToPaint = async (wrapper) => {
+  await act(async () => {
+    await new Promise((resolve) => setTimeout(resolve))
+    wrapper.update()
+  })
+}
+
+beforeAll(() => {
+  // React lazy and Suspense are not supported by server-side rendering, so it must be mocked
+  // https://stackoverflow.com/a/65751177
+  jest.mock('react', () => {
+    const react = jest.requireActual('react')
+    const Suspense = ({ children }) => {
+      return children
+    }
+
+    const lazy = jest.fn().mockImplementation((fn) => {
+      const Component: FC = (props) => {
+        type DefaultExportedFC = { default: FC }
+        const [C, setC] = useState<DefaultExportedFC>()
+
+        useEffect(() => {
+          fn().then((v: DefaultExportedFC) => {
+            setC(v)
+          })
+        }, [])
+
+        return C ? <C.default {...props} /> : null
+      }
+
+      return Component
+    })
+
+    return {
+      ...react,
+      lazy,
+      Suspense,
+    }
+  })
+})
 
 describe('Rich text editor field control', () => {
   const defaultSettings = {
@@ -27,9 +72,9 @@ describe('Rich text editor field control', () => {
     })
   })
   describe('in edit/new view', () => {
-    it('should set all the props', () => {
+    it('should set all the props', async () => {
       const value = 'Hello World'
-      const wrapper = shallow(
+      const wrapper = mount(
         <RichTextEditor
           fieldValue={value}
           actionName="edit"
@@ -41,18 +86,18 @@ describe('Rich text editor field control', () => {
           }}
         />,
       )
+      await waitForComponentToPaint(wrapper)
 
       expect(wrapper.find(Editor).prop('content')).toBe(value)
       expect(wrapper.find(Editor).prop('placeholder')).toBe(defaultSettings.DisplayName)
       expect(wrapper.find(Editor).prop('readOnly')).toBeTruthy()
-      expect(wrapper.find(InputLabel).text()).toBe(defaultSettings.DisplayName)
+      expect(wrapper.find(InputLabel).text()).toBe(`${defaultSettings.DisplayName} *`)
       expect(wrapper.find(InputLabel).prop('required')).toBeTruthy()
       expect(wrapper.find(FormHelperText).text()).toBe('description')
-      expect(wrapper).toMatchSnapshot()
     })
 
-    it('should set default value', () => {
-      const wrapper = shallow(
+    it('should set default value', async () => {
+      const wrapper = mount(
         <RichTextEditor
           actionName="new"
           settings={{
@@ -61,6 +106,8 @@ describe('Rich text editor field control', () => {
           }}
         />,
       )
+      await waitForComponentToPaint(wrapper)
+
       expect(wrapper.find(Editor).prop('content')).toBe('defaultValue')
     })
   })
