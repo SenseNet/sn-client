dfe0813367848528a73b089e457c7b41185481c1
Zoltán Takács <zoltandavid.takacs@sensenet.com>
2021-06-21 08:51:10 +0200

    fix RichTextEditor types

--- packages/sn-editor-react/src/components/editor.tsx
+++ packages/sn-editor-react/src/components/editor.tsx
@@ -62,7 +62,7 @@ const useStyles = makeStyles((theme) => {
   })
 })
 
-interface EditorProps {
+export interface EditorProps {
   autofocus?: EditorOptions['autofocus']
   content?: EditorOptions['content']
   onChange?: EditorOptions['onUpdate']
