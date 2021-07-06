faad6693845bcc471bf73be7cdc37c71f9c0356b
pusztaie <eniko.pusztai@sensenet.com>
2021-07-06 16:00:07 +0200

    add connection to backend

--- apps/sensenet/src/components/dialogs/webhook-log/webhook-log.tsx
+++ apps/sensenet/src/components/dialogs/webhook-log/webhook-log.tsx
@@ -1,136 +1,13 @@
 import { WebhookSubscription } from '@sensenet/default-content-types'
+import { useLogger, useRepository } from '@sensenet/hooks-react'
 import { Button, DialogActions, DialogContent } from '@material-ui/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGlobalStyles } from '../../../globalStyles'
 import { useLocalization } from '../../../hooks'
 import { useDialog } from '../dialog-provider'
 import { DialogTitle } from '../dialog-title'
 import { CollapsibleTable } from './collapsible-table'
-
-const exampleData = {
-  data: [
-    {
-      CreationTime: '2021-06-13T18:31:08.7658781Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1010,
-      ResponseStatusCode: 200,
-      Url: 'POST https://example.com/hook/2',
-      WebHookId: 1242,
-      ContentId: 10001,
-      EventName: 'Event2',
-      ErrorMessage: null,
-    },
-    {
-      CreationTime: '2021-06-13T12:31:08.7658771Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1020,
-      ResponseStatusCode: 200,
-      Url: 'POST https://example.com/hook/3',
-      WebHookId: 1242,
-      ContentId: 10002,
-      EventName: 'Event3',
-      ErrorMessage: null,
-    },
-    {
-      CreationTime: '2021-06-13T06:31:08.7658762Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1030,
-      ResponseStatusCode: 200,
-      Url: 'POST https://example.com/hook/4',
-      WebHookId: 1242,
-      ContentId: 10003,
-      EventName: 'Event4',
-      ErrorMessage: null,
-    },
-    {
-      CreationTime: '2021-06-13T00:31:08.7658751Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1040,
-      ResponseStatusCode: 400,
-      Url: 'POST https://example.com/hook/5',
-      WebHookId: 1242,
-      ContentId: 10004,
-      EventName: 'Event1',
-      ErrorMessage: 'Warning message',
-    },
-    {
-      CreationTime: '2021-06-12T18:31:08.7658742Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1050,
-      ResponseStatusCode: 200,
-      Url: 'POST https://example.com/hook/1',
-      WebHookId: 1242,
-      ContentId: 10005,
-      EventName: 'Event2',
-      ErrorMessage: null,
-    },
-    {
-      CreationTime: '2021-06-12T12:31:08.7658732Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1060,
-      ResponseStatusCode: 500,
-      Url: 'POST https://example.com/hook/2',
-      WebHookId: 1242,
-      ContentId: 10006,
-      EventName: 'Event3',
-      ErrorMessage: 'Error message',
-    },
-    {
-      CreationTime: '2021-06-12T06:31:08.7658722Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1070,
-      ResponseStatusCode: 200,
-      Url: 'POST https://example.com/hook/3',
-      WebHookId: 1242,
-      ContentId: 10007,
-      EventName: 'Event4',
-      ErrorMessage: null,
-    },
-    {
-      CreationTime: '2021-06-12T00:31:08.7658713Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1080,
-      ResponseStatusCode: 200,
-      Url: 'POST https://example.com/hook/4',
-      WebHookId: 1242,
-      ContentId: 10008,
-      EventName: 'Event1',
-      ErrorMessage: null,
-    },
-    {
-      CreationTime: '2021-06-11T18:31:08.7658703Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1090,
-      ResponseStatusCode: 400,
-      Url: 'POST https://example.com/hook/5',
-      WebHookId: 1242,
-      ContentId: 10009,
-      EventName: 'Event2',
-      ErrorMessage: 'Warning message',
-    },
-    {
-      CreationTime: '2021-06-11T12:31:08.7658685Z',
-      Duration: '00:00:00.9000000',
-      RequestLength: 101,
-      ResponseLength: 1100,
-      ResponseStatusCode: 200,
-      Url: 'POST https://example.com/hook/1',
-      WebHookId: 1242,
-      ContentId: 10010,
-      EventName: 'Event3',
-      ErrorMessage: null,
-    },
-  ],
-}
+import { WebhookStatInput } from '.'
 
 export type WebhookLogDialogProps = {
   content: WebhookSubscription
@@ -140,6 +17,30 @@ export const WebhookLogDialog: React.FunctionComponent<WebhookLogDialogProps> = 
   const { closeLastDialog } = useDialog()
   const globalClasses = useGlobalStyles()
   const localization = useLocalization()
+  const logger = useLogger('WebhookLog')
+  const repo = useRepository()
+  const [data, setData] = useState<WebhookStatInput[]>([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await repo.executeAction<any, WebhookStatInput[]>({
+          idOrPath: props.content.Path,
+          name: `GetWebHookUsageList`,
+          method: 'POST',
+          body: {},
+        })
+        console.log(response)
+        setData(response)
+      } catch (error) {
+        logger.error({
+          message: error.message,
+        })
+      }
+    }
+
+    getData()
+  }, [logger, props.content.Path, repo])
 
   return (
     <>
@@ -148,10 +49,13 @@ export const WebhookLogDialog: React.FunctionComponent<WebhookLogDialogProps> = 
       </DialogTitle>
       <>
         <DialogContent>
-          <CollapsibleTable webhooks={exampleData.data} />
+          <CollapsibleTable webhooks={data} />
         </DialogContent>
         <DialogActions>
-          <Button aria-label="Cancel button" className={globalClasses.cancelButton} onClick={closeLastDialog}>
+          <Button
+            aria-label={localization.forms.cancel}
+            className={globalClasses.cancelButton}
+            onClick={closeLastDialog}>
             {localization.forms.cancel}
           </Button>
         </DialogActions>
