183ed99766c02e6d2a3d72891da862ae13ea8b57
Adam Hassan <hassana@sensenet.com>
2023-01-26 10:14:04 +0100

    Correct Date Time Format (#1453)
    * correct Date Time Format
    
    * picker change
    
    * test fix
    
    * DateTimeFormat prop test
    
    * date format test correction
    
    * snapshot update
    
    * test update
    
    * remove console.log
    
    * Min Date and Format settings + tests
    
    * null value mask
    
    * initState opt
    
    * initState changes
    
    * initState changes typescript
    
    * variable name change
    
    * refactor
    
    * fieldValue Update
    
    * coverage for min Date

--- packages/sn-controls-react/src/fieldcontrols/date-picker.tsx
+++ packages/sn-controls-react/src/fieldcontrols/date-picker.tsx
@@ -13,72 +13,98 @@ import { changeTemplatedValue } from '../helpers'
 import { ReactClientFieldSetting } from './client-field-setting'
 import { defaultLocalization } from './localization'
 
+const minDatePickerDate = new Date('0001-01-01')
+
+const initialValueState = ({
+  fieldValue,
+  actionName,
+  settings,
+}: Pick<ReactClientFieldSetting<DateTimeFieldSetting>, 'fieldValue' | 'actionName' | 'settings'>) => {
+  if (fieldValue === '0001-01-01T00:00:00Z') {
+    return null
+  }
+
+  if (fieldValue) {
+    return fieldValue
+  }
+
+  if (actionName !== 'new') {
+    return null
+  }
+
+  const secureCheckedDateInput = changeTemplatedValue(settings.DefaultValue, settings.EvaluatedDefaultValue)
+
+  return secureCheckedDateInput
+}
 /**
  * Field control that represents a Date field. Available values will be populated from the FieldSettings.
  */
 export const DatePicker: React.FC<ReactClientFieldSetting<DateTimeFieldSetting>> = (props) => {
-  const localization = deepMerge(defaultLocalization.datePicker, props.localization?.datePicker)
+  const { settings, actionName, fieldValue, locale, localization, hideDescription, fieldOnChange } = props
 
-  const initialState =
-    props.fieldValue ||
-    (props.actionName === 'new' &&
-      changeTemplatedValue(props.settings.DefaultValue, props.settings.EvaluatedDefaultValue)) ||
-    null
-  const [value, setValue] = useState(initialState)
+  const localizationMerged = deepMerge(defaultLocalization.datePicker, localization?.datePicker)
+
+  const [value, setValue] = useState(initialValueState({ fieldValue, actionName, settings }))
 
   const handleDateChange = (date: MaterialUiPickersDate) => {
     if (!date) {
       return
     }
     setValue(new Date(date).toISOString())
-    props.fieldOnChange?.(props.settings.Name, new Date(date).toISOString())
+    fieldOnChange?.(settings.Name, new Date(date).toISOString())
   }
 
-  switch (props.actionName) {
+  switch (actionName) {
     case 'edit':
     case 'new':
       return (
-        <MuiPickersUtilsProvider utils={DateFnsUtils} locale={props.locale}>
-          {props.settings.DateTimeMode === DateTimeMode.Date ? (
-            <MUIDatePicker
-              value={value}
-              onChange={handleDateChange}
-              name={props.settings.Name}
-              label={props.settings.DisplayName}
-              id={props.settings.Name}
-              disabled={props.settings.ReadOnly}
-              placeholder={props.settings.DisplayName}
-              required={props.settings.Compulsory}
-              fullWidth={true}
-            />
-          ) : (
-            <DateTimePicker
-              value={value}
-              onChange={handleDateChange}
-              label={props.settings.DisplayName}
-              name={props.settings.Name}
-              id={props.settings.Name}
-              disabled={props.settings.ReadOnly}
-              placeholder={props.settings.DisplayName}
-              required={props.settings.Compulsory}
-              fullWidth={true}
-            />
-          )}
-          {!props.hideDescription && <FormHelperText>{props.settings.Description}</FormHelperText>}
+        <MuiPickersUtilsProvider utils={DateFnsUtils} locale={locale}>
+          <>
+            {settings.DateTimeMode === DateTimeMode.Date ? (
+              <MUIDatePicker
+                value={value}
+                minDate={minDatePickerDate}
+                onChange={handleDateChange}
+                name={settings.Name}
+                label={settings.DisplayName}
+                id={settings.Name}
+                disabled={settings.ReadOnly}
+                placeholder={settings.DisplayName}
+                required={settings.Compulsory}
+                fullWidth={true}
+                format="yyyy MMMM dd"
+              />
+            ) : (
+              <DateTimePicker
+                minDate={minDatePickerDate}
+                value={value}
+                onChange={handleDateChange}
+                label={settings.DisplayName}
+                name={settings.Name}
+                id={settings.Name}
+                disabled={settings.ReadOnly}
+                placeholder={settings.DisplayName}
+                required={settings.Compulsory}
+                fullWidth={true}
+                format="yyyy MMMM do hh:mm aaaa"
+              />
+            )}
+            {!hideDescription && <FormHelperText>{settings.Description}</FormHelperText>}
+          </>
         </MuiPickersUtilsProvider>
       )
     default:
       return (
         <div>
           <Typography variant="caption" gutterBottom={true}>
-            {props.settings.DisplayName}
+            {settings.DisplayName}
           </Typography>
           <Typography variant="body1" gutterBottom={true}>
-            {props.fieldValue
-              ? props.settings.DateTimeMode === DateTimeMode.Date
-                ? format(new Date(props.fieldValue), 'PPP', { locale: props.locale }).toLocaleString()
-                : format(new Date(props.fieldValue), 'PPPppp', { locale: props.locale }).toLocaleString()
-              : localization.noValue}
+            {fieldValue
+              ? settings.DateTimeMode === DateTimeMode.Date
+                ? format(new Date(fieldValue), 'PPP', { locale }).toLocaleString()
+                : format(new Date(fieldValue), 'PPPppp', { locale }).toLocaleString()
+              : localizationMerged.noValue}
           </Typography>
         </div>
       )
