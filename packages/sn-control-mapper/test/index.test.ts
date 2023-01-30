46a74850c3f0685c3658ae5d0501f8a7b7dafa0c
Ádám Hassan <hassana@sensenet.com>
2023-01-30 19:31:14 +0100

    test case hope it will work

--- packages/sn-control-mapper/test/index.test.ts
+++ packages/sn-control-mapper/test/index.test.ts
@@ -1,5 +1,6 @@
 import { Repository } from '@sensenet/client-core'
 import { ChoiceFieldSetting, FieldVisibility, IntegerFieldSetting } from '@sensenet/default-content-types'
+import { PageCount } from '../../sn-controls-react/src/fieldcontrols'
 import { ControlMapper } from '../src'
 
 class ExampleControlBase {}
@@ -27,22 +28,22 @@ export const controlMapperTests = describe('ControlMapper', () => {
     expect(controlType).toBe(ExampleDefaultFieldControl)
   })
 
-  // it('Should return correct explicit defined Control for PageCount', () => {
-  //   mapper.setupFieldSettingDefault('IntegerFieldSetting', (setting) => {
-  //     if (setting.Name === 'PageCount') {
-  //       return ExampleModifiedControl
-  //     }
-  //     return ExampleDefaultFieldControl
-  //   })
+  it('Should return correct explicit defined Control for PageCount', () => {
+    mapper.setupFieldSettingDefault('IntegerFieldSetting', (setting) => {
+      if (setting.Name === 'PageCount') {
+        return PageCount
+      }
+      return ExampleDefaultFieldControl
+    })
 
-  //   const fs = { Name: 'PageCount', Type: 'IntegerFieldSetting' } as IntegerFieldSetting
-  //   const controlType = mapper.getControlForFieldSetting(fs)
-  //   expect(controlType).toBe(ExampleModifiedControl)
+    const fs = { Name: 'PageCount', Type: 'IntegerFieldSetting' } as IntegerFieldSetting
+    const controlType = mapper.getControlForFieldSetting(fs)
+    expect(controlType).toBe(PageCount)
 
-  //   const fs2 = { Name: 'test', Type: 'IntegerFieldSetting' } as IntegerFieldSetting
-  //   const controlType2 = mapper.getControlForFieldSetting(fs2)
-  //   expect(controlType2).toBe(ExampleDefaultFieldControl)
-  // })
+    const fs2 = { Name: 'test', Type: 'IntegerFieldSetting' } as IntegerFieldSetting
+    const controlType2 = mapper.getControlForFieldSetting(fs2)
+    expect(controlType2).toBe(ExampleDefaultFieldControl)
+  })
 
   it('Should be constructed', () => {
     expect(mapper).toBeInstanceOf(ControlMapper)
