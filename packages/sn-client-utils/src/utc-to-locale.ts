a621d6a39326d5ebd51b1c6d5e80bdf59a46fb5c
Ádám Hassan <hassana@sensenet.com>
2023-03-23 09:32:51 +0100

    unit test for date converter

--- packages/sn-client-utils/src/utc-to-locale.ts
+++ packages/sn-client-utils/src/utc-to-locale.ts
@@ -5,17 +5,16 @@
  * @example '2022-03-21T15:30:00.000Z'-> 2023-03-21 15:30:00.
  */
 export function convertUtcToLocale(utcDateString: string) {
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  } as const
   try {
     const date = new Date(utcDateString)
-
-    const options = {
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-    } as const
 
     return new Intl.DateTimeFormat(window.navigator.language, options).format(date)
   } catch (error) {
