7276d508d47ac926f3063777e3074377ad7d6133
vargajoe <varga.yosh@gmail.com>
2023-10-26 13:48:56 +0200

    add action bar edit/details icons

--- apps/sensenet/src/components/view-controls/browse-view.tsx
+++ apps/sensenet/src/components/view-controls/browse-view.tsx
@@ -63,7 +63,7 @@ export const BrowseView: React.FC<BrowseViewProps> = (props) => {
         ...classes,
         cancel: globalClasses.cancelButton,
       }}
-      renderTitle={() => <ViewTitle titleBold={content?.DisplayName} content={content} />}
+      renderTitle={() => <ViewTitle actionName="browse" titleBold={content?.DisplayName} content={content} />}
     />
   )
 }
