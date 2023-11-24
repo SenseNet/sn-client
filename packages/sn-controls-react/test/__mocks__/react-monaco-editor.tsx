a2eae55bc15846b78bce6cdff541c27ed537b365
Ádám Hassan <hassana@sensenet.com>
2023-11-24 14:33:49 +0100

    mock react monaco editor

---
+++ packages/sn-controls-react/test/__mocks__/react-monaco-editor.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+/**
+ * This is a mock MonacoEditor component for testing purposes.
+ * It simply renders an empty div.
+ *
+ * @returns {JSX.Element} An empty div.
+ */
+export default function MonacoEditor(): JSX.Element {
+  return <div />
+}
