d6e961dc4fc35ac31ef62da67fd12a7345b3062e
Marosvölgyi Zoltán <zoltan.marosvolgyi@sensenet.com>
2025-02-13 14:28:15 +0100

    fix tree

--- apps/sensenet/src/components/tree/Contexts/ExpandedItemsProvider.tsx
+++ apps/sensenet/src/components/tree/Contexts/ExpandedItemsProvider.tsx
@@ -1,13 +1,82 @@
-import React, { createContext, ReactNode, useState } from 'react'
+import { ODataCollectionResponse } from '@sensenet/client-core'
+import { GenericContent } from '@sensenet/default-content-types'
+import { useRepository } from '@sensenet/hooks-react'
+import React, { createContext, ReactNode, useEffect, useRef, useState } from 'react'
 // Meghatározzuk a Context típusát: egy string tömb és egy setter függvény
-type ExpandItemsContextType = [Set<string>, React.Dispatch<React.SetStateAction<Set<string>>>]
+type ExpandItemsContextType = [
+  Set<string>,
+  React.Dispatch<React.SetStateAction<Set<string>>>,
+  (path: string) => Promise<GenericContent[] | undefined>,
+  React.Dispatch<React.SetStateAction<number>>,
+]
 
 // Létrehozzuk a Context-et alapértelmezett értékekkel
 export const ExpandItemsContext = createContext<ExpandItemsContextType | undefined>(undefined)
 
 const ExpandedItemsProvider = ({ children }: { children: ReactNode }) => {
   const [expandItems, setExpandItems] = useState<Set<string>>(new Set())
-  return <ExpandItemsContext.Provider value={[expandItems, setExpandItems]}>{children}</ExpandItemsContext.Provider>
+  const cache = useRef<{ [key: string]: { data: GenericContent[] | undefined; timestamp: number } }>({}) // Globális cache objektum
+  const [cacheTime, setCacheTime] = useState<number>(6000)
+  const repo = useRepository()
+  const loadChildren = async (path: string): Promise<GenericContent[] | undefined> => {
+    //letölti a connteneteket a megadott path alapján
+    const loadCollection = function loadCollection(
+      contentPath: string,
+    ): Promise<ODataCollectionResponse<GenericContent>> {
+      return repo.loadCollection<GenericContent>({
+        path: contentPath,
+        oDataOptions: {
+          select: ['Id', 'Path', 'Name', 'DisplayName', 'Type', 'Actions', 'Icon', 'ParentId'],
+          onlyselectList: true,
+        },
+      })
+    }
+    //cache logika
+    if (path === undefined || path === '') return undefined
+    const now = Date.now()
+    //ha a cachben nincs elem akkor tovább megyünk
+    if (cache.current[path] !== undefined) {
+      //ha a cacheben van elem és undefined akkor várunk
+      let i = 0
+      while (cache.current[path].data === undefined) {
+        setTimeout(() => {}, 200)
+        console.log('#globalfetch: waiting for response', path)
+        i++
+        if (i > 10) {
+          console.log('#globalfetch: max iteration exceeded', path)
+          return undefined
+        }
+      }
+
+      //ha a cacheben van elem és nem undefined akkor visszadjuk azt
+      if (now - cache.current[path].timestamp < cacheTime) {
+        console.log('#globalfetch: from cache', path)
+        return (
+          cache.current[path] as {
+            data: GenericContent[] | undefined
+            timestamp: number
+          }
+        )?.data
+      }
+    }
+    try {
+      const response = await loadCollection(path)
+      //itt megkéne várni, esetleg egy await async meoldaná a problémát
+      const result = response?.d.results
+      cache.current[path] = { data: result, timestamp: now }
+      console.log('#globalfetch: miss cache', path)
+      return result
+      //ezt nem várja meg
+    } catch (error) {
+      console.error('#globalfetch: Fetch error:', error)
+    }
+  }
+
+  return (
+    <ExpandItemsContext.Provider value={[expandItems, setExpandItems, loadChildren, setCacheTime]}>
+      {children}
+    </ExpandItemsContext.Provider>
+  )
 }
 
 export default ExpandedItemsProvider
--- apps/sensenet/src/components/tree/StyledTreeItem.tsx
+++ apps/sensenet/src/components/tree/StyledTreeItem.tsx
@@ -1,13 +1,9 @@
-import { Collapse, ListItemIcon, ListItemText } from '@material-ui/core'
+import { ListItemIcon, ListItemText } from '@material-ui/core'
 // @ts-ignore
 import { createStyles, Theme, withStyles } from '@material-ui/core/styles'
 import TreeItem from '@material-ui/lab/TreeItem'
-import { ODataCollectionResponse } from '@sensenet/client-core'
 import { GenericContent } from '@sensenet/default-content-types'
-import { useRepository } from '@sensenet/hooks-react'
-//import { useQueryClient } from 'react-query'
 import React, { useCallback, useContext, useEffect, useState } from 'react'
-import { useGlobalCacheFetch } from '../contentprovider/GlobalContentCollection'
 import { Icon } from '../Icon'
 import { ExpandItemsContext } from './Contexts/ExpandedItemsProvider'
 import StyledTreeItemProps from './Props/StyledTreeItemProps'
@@ -26,41 +22,18 @@ export const StyledTreeItem = withStyles((theme: Theme) =>
     },
   }),
 )((props: StyledTreeItemProps) => {
-  const repo = useRepository()
-  //const queryClient = useQueryClient()
-  const [innerElements, setInnerElements] = useState<[]>()
-  const [childrenLoaded, setChildrenLoaded] = useState<boolean>(false)
+  const [hasChildren, setHasChildren] = useState<boolean>(true)
+  const [innerElements, setInnerElements] = useState<React.JSX.Element[]>()
   const expContext = useContext(ExpandItemsContext)
   if (!expContext) {
     throw new Error('MyComponent must be used within a ExpandItemsProvider')
   }
-  // const fetchCollection = (contentPath: string): Promise<ODataCollectionResponse<GenericContent>> => {
-  //   return repo.loadCollection<GenericContent>({
-  //     path: contentPath,
-  //     oDataOptions: {
-  //       select: ['Id', 'Path', 'Name', 'DisplayName', 'Type', 'Actions', 'Icon', 'ParentId'],
-  //       onlyselectList: true,
-  //     },
-  //   })
-  // }
 
-  const { data } = useGlobalCacheFetch('https://jsonplaceholder.typicode.com/todos', 60000)
-  const [expandItems, setExpandItems] = expContext
+  const [expandItems, setExpandItems, loadChildren] = expContext
   const loadCollectionCB = useCallback(() => {
-    function loadCollection(contentPath: string): Promise<ODataCollectionResponse<GenericContent>> {
-      return repo.loadCollection<GenericContent>({
-        path: contentPath,
-        oDataOptions: {
-          select: ['Id', 'Path', 'Name', 'DisplayName', 'Type', 'Actions', 'Icon', 'ParentId'],
-          onlyselectList: true,
-        },
-      })
-    }
-
-    setChildrenLoaded(true)
-    const respRequest = loadCollection(props.contentvalue.Path)
-    respRequest.then((result: any) => {
-      const elements = result?.d.results.map((innerChild: GenericContent) => {
+    const children = loadChildren(props.contentvalue.Path)
+    children.then((result: GenericContent[] | undefined) => {
+      const elements = result?.map((innerChild: GenericContent) => {
         return (
           <StyledTreeItem
             id="1"
@@ -73,57 +46,123 @@ export const StyledTreeItem = withStyles((theme: Theme) =>
             isOpen={true}
             parentisopen={true}
             onNavigate={props.onNavigate}
-            // addItemToExpanded={props.addItemToExpanded}
-            // expandedItems={props.expandedItems}
-            // getExpandedItems={props.getExpandedItems}
           />
         )
       })
-      setInnerElements(elements)
-      //}
+      if (elements !== undefined) {
+        setHasChildren(elements.length > 0)
+        setInnerElements(elements)
+      }
     })
-  }, [props.activeItemPath, props.contentvalue.Path, props.onNavigate, repo])
-
+  }, [loadChildren, props.activeItemPath, props.contentvalue.Path, props.onNavigate])
   useEffect(() => {
-    console.log('#exptree: 2nd 0cb', props.contentvalue.Id, props.activeItemPath, props.contentvalue.Path)
-    //első gyerekeke letöltése
-    if (!childrenLoaded && props.id !== undefined && props.id === '0') {
-      console.log('#exptree: 1st lcb', props.contentvalue.Id)
+    if (props.id === '0') {
       loadCollectionCB()
     }
-    //A currentpath-t tartalmazó elemeket kinyitjuk
-    if (props.activeItemPath.startsWith(props.contentvalue.Path)) {
-      console.log('#exptree: 2nd lcb', props.contentvalue.Id, props.activeItemPath, props.contentvalue.Path)
-      loadCollectionCB()
-      // const { data, fetchData, loading } = useGlobalCacheFetch('https://jsonplaceholder.typicode.com/todos', 60000)
-      // fetchData().then(() => {
-      //   console.log('fetchData result', data)
-      // })
-      setExpandItems((eItems) => eItems.add(props.contentvalue.Id.toString()))
-    }
+  }, [props.id, loadCollectionCB])
+  // const loadCollectionCB = useCallback(() => {
+  //   function loadCollection(contentPath: string): Promise<ODataCollectionResponse<GenericContent>> {
+  //     return repo.loadCollection<GenericContent>({
+  //       path: contentPath,
+  //       oDataOptions: {
+  //         select: ['Id', 'Path', 'Name', 'DisplayName', 'Type', 'Actions', 'Icon', 'ParentId'],
+  //         onlyselectList: true,
+  //       },
+  //     })
+  //   }
 
-    //Első betöltésre: Ha a parent parentje closed akkor nem töltök be gyerek elemeket, amikor a treeitem nincs nyitva, de a parent igen
-    console.log('#exptree: 3rd lcb', props.contentvalue, expandItems)
-    let parentIdString = ''
-    const parentId = props.contentvalue.ParentId?.toString()
-    if (parentId !== undefined) {
-      parentIdString = parentId
-    }
-    if (expandItems.has(parentIdString)) {
-      //loadCollectionCB()
-      console.log('#exptree: parent opened', props.contentvalue, expandItems)
-    }
-  }, [
-    props,
-    childrenLoaded,
-    loadCollectionCB,
-    expandItems,
-    setExpandItems,
-    props.contentvalue.Path,
-    props.isOpen,
-    props.parentisopen,
-    repo,
-  ])
+  //   setChildrenLoaded(true)
+  //   const respRequest = loadCollection(props.contentvalue.Path)
+  //   respRequest.then((result: any) => {
+  //     const elements = result?.d.results.map((innerChild: GenericContent) => {
+  //       return (
+  //         <StyledTreeItem
+  //           id="1"
+  //           key={innerChild.Id}
+  //           activeItemPath={props.activeItemPath}
+  //           itemID={innerChild.Id.toString()}
+  //           nodeId={innerChild.Id.toString()}
+  //           label={`${innerChild.Name} ${innerChild.Id}`}
+  //           contentvalue={innerChild}
+  //           isOpen={true}
+  //           parentisopen={true}
+  //           onNavigate={props.onNavigate}
+  //           // addItemToExpanded={props.addItemToExpanded}
+  //           // expandedItems={props.expandedItems}
+  //           // getExpandedItems={props.getExpandedItems}
+  //         />
+  //       )
+  //     })
+  //     setInnerElements(elements)
+  //     //}
+  //   })
+  // }, [props.activeItemPath, props.contentvalue.Path, props.onNavigate, repo])
+
+  // useEffect(() => {
+  //   const childs = loadChildren(props.contentvalue.Path)
+  //   const elements = childs?.map((innerChild: GenericContent) => {
+  //     return (
+  //       <StyledTreeItem
+  //         id="1"
+  //         key={innerChild.Id}
+  //         activeItemPath={props.activeItemPath}
+  //         itemID={innerChild.Id.toString()}
+  //         nodeId={innerChild.Id.toString()}
+  //         label={`${innerChild.Name} ${innerChild.Id}`}
+  //         contentvalue={innerChild}
+  //         isOpen={true}
+  //         parentisopen={true}
+  //         onNavigate={props.onNavigate}
+  //         // addItemToExpanded={props.addItemToExpanded}
+  //         // expandedItems={props.expandedItems}
+  //         // getExpandedItems={props.getExpandedItems}
+  //       />
+  //     )
+  //   })
+  //   if (elements !== undefined) {
+  //     setInnerElements(elements)
+  //   }
+  // }, [props.activeItemPath, props.contentvalue.Path, props.onNavigate, loadChildren])
+  // useEffect(() => {
+  //   console.log('#exptree: 2nd 0cb', props.contentvalue.Id, props.activeItemPath, props.contentvalue.Path)
+  //   //első gyerekeke letöltése
+  //   if (!childrenLoaded && props.id !== undefined && props.id === '0') {
+  //     console.log('#exptree: 1st lcb', props.contentvalue.Id)
+  //     loadCollectionCB()
+  //   }
+  //   //A currentpath-t tartalmazó elemeket kinyitjuk
+  //   if (props.activeItemPath.startsWith(props.contentvalue.Path)) {
+  //     console.log('#exptree: 2nd lcb', props.contentvalue.Id, props.activeItemPath, props.contentvalue.Path)
+  //     // loadCollectionCB()
+  //     // const { data, fetchData, loading } = useGlobalCacheFetch('https://jsonplaceholder.typicode.com/todos', 60000)
+  //     // fetchData().then(() => {
+  //     //   console.log('fetchData result', data)
+  //     // })
+  //     setExpandItems((eItems) => eItems.add(props.contentvalue.Id.toString()))
+  //   }
+
+  //   //Első betöltésre: Ha a parent parentje closed akkor nem töltök be gyerek elemeket, amikor a treeitem nincs nyitva, de a parent igen
+  //   console.log('#exptree: 3rd lcb', props.contentvalue, expandItems)
+  //   let parentIdString = ''
+  //   const parentId = props.contentvalue.ParentId?.toString()
+  //   if (parentId !== undefined) {
+  //     parentIdString = parentId
+  //   }
+  //   if (expandItems.has(parentIdString)) {
+  //     //loadCollectionCB()
+  //     console.log('#exptree: parent opened', props.contentvalue, expandItems)
+  //   }
+  // }, [
+  //   props,
+  //   childrenLoaded,
+  //   loadCollectionCB,
+  //   expandItems,
+  //   setExpandItems,
+  //   props.contentvalue.Path,
+  //   props.isOpen,
+  //   props.parentisopen,
+  //   repo,
+  // ])
   //console.log('#exptree on render', expandItems, props.contentvalue.Id)
   if (props.activeItemPath.startsWith(props.contentvalue.Path)) {
     console.log('#exptree: treeitem root render', props.contentvalue.Id, props.activeItemPath, props.contentvalue.Path)
@@ -138,33 +177,31 @@ export const StyledTreeItem = withStyles((theme: Theme) =>
           <ListItemIcon key={props.contentvalue.Id}>
             <Icon item={props.contentvalue} />
           </ListItemIcon>
-          <ListItemText
-            style={{ fontSize: '11px!important' }}
-            primary={`${props.contentvalue.Name} ${props.contentvalue.Id} `}
-          />
+          <ListItemText style={{ fontSize: '11px!important' }} primary={`${props.contentvalue.Name}`} />
         </>
       }
-      // id="1"
-
+      id={props.contentvalue.Id.toString()}
       onIconClick={() => {
         //Ha a lenyílóra kattintasz akkor hozzáadjuk hogy őt ki kell nyitni
         if (expandItems.has(props.contentvalue.Id.toString())) {
           const deleteResult = expandItems.delete(props.contentvalue.Id.toString())
           if (deleteResult) {
-            // setExpandItems((eItems) => eItems)
+            setExpandItems((eItems) => eItems)
             console.log('#exptree: close', props.contentvalue.Id, expandItems)
           }
-          loadCollectionCB()
         } else {
-          //setExpandItems((eItems) => eItems.add(props.contentvalue.Id.toString()))
+          loadCollectionCB()
+          setExpandItems((eItems) => eItems.add(props.contentvalue.Id.toString()))
           console.log('#exptree: open', props.contentvalue.Id, expandItems)
         }
       }}
       onLabelClick={() => {
         //On click-re csak contextust kell válltani
         props.onNavigate(props.contentvalue)
-      }}>
+      }}
+      collapseIcon={!hasChildren && <></>}>
       {innerElements}
+      <></>
     </TreeItem>
   )
 })
