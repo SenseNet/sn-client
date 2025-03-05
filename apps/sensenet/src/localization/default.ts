11d3153bf88905c0c0db2e2a8ba9c678227f5cf8
Hassan Adam <hassanad@Hassan-Mac-mini.local>
2025-03-05 12:36:30 +0100

    feature: show toeast massage

--- apps/sensenet/src/localization/default.ts
+++ apps/sensenet/src/localization/default.ts
@@ -689,7 +689,7 @@ const values = {
     title: 'Action Framkework Demo',
     submit: 'Upload',
     cancel: 'Cancel',
-    success: 'Succesfull Operation!',
+    success: 'Succesfull Operation:',
   },
 }
 
