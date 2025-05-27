bc8d3dcb1e9823d17e015ee6136cb3aeadfb3baf
NoelKova <Noel.Kovacs@sensenet.com>
2025-05-27 14:59:00 +0200

    auth redirect on logout fix

--- apps/sensenet/src/services/auth-config.ts
+++ apps/sensenet/src/services/auth-config.ts
@@ -15,10 +15,7 @@ export const getAuthConfig = async (repoUrl: string) => {
     redirect_uri: window.location.origin + PATHS.loginCallback.appPath,
     response_type: 'code',
     scope: 'openid profile sensenet',
-    post_logout_redirect_uri: pathWithQueryParams({
-      path: window.location.origin,
-      newParams: { repoUrl },
-    }),
+    post_logout_redirect_uri: window.location.origin,
     silent_redirect_uri: window.location.origin + PATHS.silentCallback.appPath,
     extraQueryParams: { snrepo: trimmedRepoUrl },
   }
